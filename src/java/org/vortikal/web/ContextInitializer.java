b0d47a1b91610b5e0f7fcb0bb672b6a38b8fd1dc
Gorm Paulsen <gormap@usit.uio.no>
2005-02-11 16:23:11 +0000

    Removed javadoc comment

--- src/java/org/vortikal/web/ContextInitializer.java
+++ src/java/org/vortikal/web/ContextInitializer.java
@@ -28,10 +28,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * Created on 15.jul.2004
- *
- */
 package org.vortikal.web;
 
 import javax.servlet.http.HttpServletRequest;

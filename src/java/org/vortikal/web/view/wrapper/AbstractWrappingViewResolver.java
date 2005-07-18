6c95f5cd9e289b0d29b811cd1f3ada931fe8c0a7
Gorm Paulsen <gormap@usit.uio.no>
2005-07-18 17:40:31 +0000

    Code cleanup

--- src/java/org/vortikal/web/view/wrapper/AbstractWrappingViewResolver.java
+++ src/java/org/vortikal/web/view/wrapper/AbstractWrappingViewResolver.java
@@ -1,3 +1,33 @@
+/* Copyright (c) 2005, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.vortikal.web.view.wrapper;
 
 import java.util.ArrayList;
@@ -22,61 +52,59 @@ public abstract class AbstractWrappingViewResolver implements ViewResolver {
     private Log logger = LogFactory.getLog(this.getClass());
     
     private ViewWrapper wrappingView;
-    private ReferenceDataProvider[] providers;
+    private ReferenceDataProvider[] referenceDataProviders;
 
     public View resolveViewName(String viewName, Locale locale) throws Exception {
         View view = resolveViewNameInternal(viewName, locale);
         
-        if (view != null && wrappingView != null) {
-            return new ProxyView(view, this.providers, wrappingView);
+//         if (view != null && this.wrappingView != null) {
+        if (view != null) {
+            view = new ProxyView(view, this.referenceDataProviders, this.wrappingView);
         }
+        if (logger.isDebugEnabled()) {
+            logger.debug("Returning view '" + view + "' for view name '"
+                         + viewName + "', locale '" + locale + "'");
+        }
+
         return view;
     }
 
     /**
      * Actually resolves the view. Must be implemented by subclasses.
      * 
-     * @param viewName
-     *            a the name of the view.
+     * @param viewName the name of the view.
      * @return the resolved view.
      */
     protected abstract View resolveViewNameInternal(String viewName, Locale locale);
 
 
-    /**
-     * @param providers
-     *            The providers to set.
-     */
-    public void setProviders(ReferenceDataProvider[] providers) {
-        this.providers = providers;
+    public void setReferenceDataProviders(ReferenceDataProvider[] referenceDataProviders) {
+        this.referenceDataProviders = referenceDataProviders;
     }
 
     
-    /**
-     * @param wrappingView The wrappingView to set.
-     */
     public void setWrappingView(ViewWrapper wrappingView) {
         this.wrappingView = wrappingView;
     }
 
 
     /**
-     * Wrapper class for the resolved view, running the <code>providers</code>
+     * Wrapper class for the resolved view, running the <code>referenceDataProviders</code>
      * before the wrapped view is run (and the necessary model is available)
      */
     private class ProxyView implements View {
 
-        private ReferenceDataProvider[] providers;
+        private ReferenceDataProvider[] referenceDataProviders;
         private View view;
         private ViewWrapper viewWrapper;
         
         /**
-         * @param view -
-         *            the view to eventually run
-         * @param providers -
-         *            the set of reference data providers for this view
+         * @param view - the view to eventually run
+         * @param referenceDataProviders - the set of reference data
+         * providers for this view
          */
-        public ProxyView(View view, ReferenceDataProvider[] resolverProviders, ViewWrapper viewWrapper) {
+        public ProxyView(View view, ReferenceDataProvider[] resolverProviders,
+                         ViewWrapper viewWrapper) {
 
             if (view == null)
                 throw new IllegalArgumentException(
@@ -91,20 +119,20 @@ public abstract class AbstractWrappingViewResolver implements ViewResolver {
                 providerList.addAll(Arrays.asList(resolverProviders));
             }
 
-            if (viewWrapper != null && (viewWrapper instanceof ReferenceDataProviding)) {
+            if (this.viewWrapper != null && (this.viewWrapper instanceof ReferenceDataProviding)) {
                 ReferenceDataProvider[] wrapperProviders = null;
 
-                wrapperProviders = ((ReferenceDataProviding) viewWrapper)
+                wrapperProviders = ((ReferenceDataProviding) this.viewWrapper)
                         .getReferenceDataProviders();
                 if (wrapperProviders != null) {
                     providerList.addAll(Arrays.asList(wrapperProviders));
                 }
             }
             
-            if (view instanceof ReferenceDataProviding) {
+            if (this.view instanceof ReferenceDataProviding) {
                 ReferenceDataProvider[] viewProviders = null;
 
-                viewProviders = ((ReferenceDataProviding) view)
+                viewProviders = ((ReferenceDataProviding) this.view)
                         .getReferenceDataProviders();
                 if (viewProviders != null) {
                     providerList.addAll(Arrays.asList(viewProviders));
@@ -114,22 +142,24 @@ public abstract class AbstractWrappingViewResolver implements ViewResolver {
             if (providerList.size() > 0) {
                 if (logger.isDebugEnabled()) {
                     logger.debug("Found reference data providers for view "
-                            + view + ": " + providerList);
+                            + this.view + ": " + providerList);
                 }
 
-                providers = (ReferenceDataProvider[])providerList.toArray(new ReferenceDataProvider[providerList.size()]);
+                this.referenceDataProviders = (ReferenceDataProvider[])
+                    providerList.toArray(new ReferenceDataProvider[providerList.size()]);
             }        
         }
 
         public void render(Map model, HttpServletRequest request,
                 HttpServletResponse response) throws Exception {
 
-            if (this.providers != null && this.providers.length > 0) {
-                if (model == null)
+            if (this.referenceDataProviders != null && this.referenceDataProviders.length > 0) {
+                if (model == null) {
                     model = new HashMap();
-
-                for (int i = 0; i < this.providers.length; i++) {
-                    ReferenceDataProvider provider = this.providers[i];
+                }
+                
+                for (int i = 0; i < this.referenceDataProviders.length; i++) {
+                    ReferenceDataProvider provider = this.referenceDataProviders[i];
                     if (logger.isDebugEnabled())
                         logger.debug("Invoking reference data provider '"
                                 + provider + "'");
@@ -137,17 +167,19 @@ public abstract class AbstractWrappingViewResolver implements ViewResolver {
                 }
             }
             
-            if (viewWrapper != null)
-                viewWrapper.renderView(view, model, request, response);
-            else
-                view.render(model, request, response);
+            if (viewWrapper != null) {
+                this.viewWrapper.renderView(this.view, model, request, response);
+            } else {
+                this.view.render(model, request, response);
+            }
+            
         }
 
         public String toString() {
             StringBuffer sb = new StringBuffer();
-            sb.append(this.getClass().toString()).append(":");
-            sb.append("view = ").append(this.view.toString());
-            sb.append("viewWrapper = ").append(this.viewWrapper.toString());
+            sb.append(this.getClass().getName()).append(":");
+            sb.append("view = ").append(this.view);
+            sb.append("viewWrapper = ").append(this.viewWrapper);
             return sb.toString();
         }
 

ef39c3507038a7238f97bd76d8eec723ec873c54
Gorm Paulsen <gormap@usit.uio.no>
2005-08-10 20:39:39 +0000

    Added copyright header and logging

--- src/java/org/vortikal/web/view/freemarker/MultiTemplateLocationsFreeMarkerConfigurer.java
+++ src/java/org/vortikal/web/view/freemarker/MultiTemplateLocationsFreeMarkerConfigurer.java
@@ -1,3 +1,33 @@
+/* Copyright (c) 2004, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.vortikal.web.view.freemarker;
 
 import java.io.IOException;
@@ -6,6 +36,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.MultiTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanCreationException;
@@ -22,28 +61,30 @@ import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-import freemarker.cache.ClassTemplateLoader;
-import freemarker.cache.MultiTemplateLoader;
-import freemarker.cache.TemplateLoader;
-import freemarker.template.Configuration;
-import freemarker.template.TemplateException;
-
-/** JavaBean to configure FreeMarker for web usage, mirroring Spring's 
- *  <code>FreeMarkerConfigurer</code>, differing only in the way template paths are resolved.
+/**
+ *  JavaBean to configure FreeMarker for web usage, mirroring Spring's
+ *  <code>FreeMarkerConfigurer</code>, differing only in the way
+ *  template paths are resolved.
  *  
- *  This bean look up beans implementing the <code>FreeMarkerTemplateLocation</code> interface,
- *  making a <code>MultiTemplateLoader</code> containing all the found template locations.
+ *  This bean look up beans implementing the
+ *  <code>FreeMarkerTemplateLocation</code> interface, making a
+ *  <code>MultiTemplateLoader</code> containing all the found template
+ *  locations.
  *  
- *  Delegates the actual configuration creation to Spring's <code>FreemarkerConfigurationFactory</code> 
- *  (the super class of <code>FreeMarkerConfigurer</code>) 
+ *  Delegates the actual configuration creation to Spring's
+ *  <code>FreemarkerConfigurationFactory</code> (the super class of
+ *  <code>FreeMarkerConfigurer</code>)
  *  
  *  @see org.springframework.ui.freemarker.FreeMarkerConfigurer
  *  @see org.springframework.ui.freemarker.FreeMarkerConfigurationFactory
  *  @see freemarker.cache.MultiTemplateLoader
  *  @see FreeMarkerTemplateLocation
  */
-public class MultiTemplateLocationsFreeMarkerConfigurer implements FreeMarkerConfig, 
-    InitializingBean, ApplicationContextAware {
+public class MultiTemplateLocationsFreeMarkerConfigurer
+  implements FreeMarkerConfig, InitializingBean, ApplicationContextAware {
+
+    private Log logger = LogFactory.getLog(this.getClass());
+    
 
     private ApplicationContext applicationContext;
     
@@ -84,6 +125,10 @@ public class MultiTemplateLocationsFreeMarkerConfigurer implements FreeMarkerCon
             Map templateLocations = 
                     BeanFactoryUtils.beansOfTypeIncludingAncestors(
                             applicationContext, FreeMarkerTemplateLocation.class, true, false);
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Found template locations: " + templateLocations);
+            }
 
             if (templateLocations.isEmpty()) 
                 throw new BeanCreationException("Couldn't find any beans implementing " 

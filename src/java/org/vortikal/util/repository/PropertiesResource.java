bfc234986afcfaf717471dbe82429075b67bc8bb
Gorm Paulsen <gormap@usit.uio.no>
2005-12-07 12:13:03 +0000

    Added a java.util.Properties implementation that reads its entries from a repository

---
+++ src/java/org/vortikal/util/repository/PropertiesResource.java
@@ -0,0 +1,171 @@
+/* Copyright (c) 2005, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.util.repository;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.vortikal.repository.Repository;
+import org.springframework.context.ApplicationEvent;
+
+
+/**
+ * 
+ */
+public class PropertiesResource extends Properties implements InitializingBean,
+                                                              ApplicationListener {
+
+    private Log logger = LogFactory.getLog(this.getClass());
+
+    private Repository repository;
+    private String uri;
+    private String token;
+    private boolean demandResourceAvailability = false;
+    private boolean lazyInit = false;
+    
+
+    public PropertiesResource() {
+        super();
+    }
+    
+    public PropertiesResource(Properties defaults) {
+        super(defaults);
+    }
+    
+    public PropertiesResource(Repository repository, String uri, String token,
+                              boolean demandResourceAvailability, boolean lazyInit) {
+        this.repository = repository;
+        this.uri = uri;
+        this.token = token;
+        this.demandResourceAvailability = demandResourceAvailability;
+    }
+    
+
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
+
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+
+    public void setDemandResourceAvailability(boolean demandResourceAvailability) {
+        this.demandResourceAvailability = demandResourceAvailability;
+    }
+
+
+    public void setLazyInit(boolean lazyInit) {
+        this.lazyInit = lazyInit;
+    }
+    
+
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (!this.lazyInit) {
+            return;
+        }
+
+        if (event instanceof ContextRefreshedEvent) {
+            try {
+                this.load();
+            } catch (Exception e) {
+                logger.warn(e);
+            }
+        }
+    }
+    
+
+    public void afterPropertiesSet() throws Exception {
+        if (this.repository == null) {
+            throw new BeanInitializationException(
+                "JavaBean property 'repository' not specified");
+        }
+        if (this.uri == null) {
+            throw new BeanInitializationException(
+                "JavaBean property 'uri' not specified");
+        }
+        if (!this.lazyInit) {
+            this.load();
+        }
+    }
+
+
+    public void load(Repository repository, String uri, String token) throws IOException {
+        this.load(repository, uri, token, false);
+    }
+    
+
+    public void load(Repository repository, String uri, String token,
+                     boolean demandResourceAvailability) throws IOException {
+        if (demandResourceAvailability) {
+            this.loadInternal(repository, uri, token);
+        } else {
+            try {
+                this.loadInternal(repository, uri, token);
+            } catch (Exception e) {
+                logger.warn("Unable to load properties from uri '"
+                            + uri + "', repository '" + repository
+                            + "', token '" + token + "'", e);
+            }
+        }
+    }
+    
+    public void load() throws IOException {
+
+        if (this.repository == null || this.uri == null) {
+            throw new IllegalStateException(
+                "JavaBean properties 'repository' and 'uri' must be specified");
+        }
+        this.clear();
+        this.load(this.repository, this.uri, this.token, this.demandResourceAvailability);
+
+    }
+    
+
+    private void loadInternal(Repository repository, String uri, String token)
+        throws IOException {
+        InputStream inputStream = repository.getInputStream(token, uri, false);
+        super.load(inputStream);
+    }
+
+}

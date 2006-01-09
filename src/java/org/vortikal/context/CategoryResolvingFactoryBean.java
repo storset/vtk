ef82e0533a91b9230bf285ef7855b11f979660a7
Gorm Paulsen <gormap@usit.uio.no>
2006-01-09 14:55:10 +0000

    Added option to specify comparator for sorting resolved category sets.

--- src/java/org/vortikal/context/CategoryResolvingFactoryBean.java
+++ src/java/org/vortikal/context/CategoryResolvingFactoryBean.java
@@ -32,6 +32,7 @@ package org.vortikal.context;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -45,28 +46,36 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.OrderComparator;
 import org.springframework.core.Ordered;
+
 import org.vortikal.web.service.Service;
 
-
-
 /**
- * Factory bean looking up groups of objects based on the Categorizable interface
- * and Class type in the context.
+ * Factory bean looking up groups of objects based on the
+ * Categorizable interface and Class type in the context.
  * 
- * Configurable bean properties:
+ * Configurable JavaBean properties:
  * 
  * <ul>
- * <li><code>category</code> - required tring describing the category of the services
- * <li><code>class</code> - required bean type. Must implement Categorizable.
- *  If the class implements ordered, the returned set will be sorted.
+ * 
+ *   <li><code>category</code> - required {@link String} describing
+ *   the category of the services
+ *   <li><code>class</code> - required bean {@link Class type}. Must
+ *    implement {@link Categorizable}.  If the class implements {@link
+ *    Ordered}, the returned set will be sorted.
+ *   <li><code>category</code> - an optional {@link Comparator},
+ *   which, if specified, is used for sorting the result set instead
+ *   of the default {@link OrderComparator}.
+ * </ul>
  */
-public class CategoryResolvingFactoryBean implements ApplicationContextAware, FactoryBean, InitializingBean {
+public class CategoryResolvingFactoryBean
+  implements ApplicationContextAware, FactoryBean, InitializingBean {
 
     private ApplicationContext applicationContext;
     private String category;
     private Class clazz;
     private boolean ordered;
-
+    private Comparator comparator;
+    
 
     /**
      * Gets a list of {@link Service services} declared to belong to a
@@ -80,24 +89,27 @@ public class CategoryResolvingFactoryBean implements ApplicationContextAware, Fa
     private List getObjectsOfCategory(String category) {
         // find all services, and sort out those of category 'category';
         Map matchingBeans = BeanFactoryUtils.beansOfTypeIncludingAncestors(
-            applicationContext, clazz, true, false);
+            this.applicationContext, this.clazz, true, false);
     
         List objects = new ArrayList(matchingBeans.values());
         List list = new ArrayList(objects);
         for (Iterator iter = list.iterator(); iter.hasNext();) {
             Categorizable categorizable = (Categorizable) iter.next();
             if (categorizable.getCategories() == null 
-                || !categorizable.getCategories().contains(category)) 
+                || !categorizable.getCategories().contains(this.category)) 
                 objects.remove(categorizable);
         }
         
-        if (ordered)
+        if (comparator != null) {
+            Collections.sort(objects, this.comparator);
+        } else if (this.ordered) {
             Collections.sort(objects, new OrderComparator());
+        }
         
         return objects;
     }
 
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
@@ -121,15 +133,21 @@ public class CategoryResolvingFactoryBean implements ApplicationContextAware, Fa
         this.clazz = clazz;
     }
 
+    public void setComparator(Comparator comparator) {
+        this.comparator = comparator;
+    }
+    
+
     public void afterPropertiesSet() throws Exception {
         if (this.category == null) 
             throw new BeanInitializationException("Property 'category' must be specified");
         if (this.clazz == null)
             throw new BeanInitializationException("Property 'clazz' must be specified");
-        if (! Categorizable.class.isAssignableFrom(clazz))
-            throw new BeanInitializationException("Property 'clazz' must be a class implementing Categorizable");
+        if (! Categorizable.class.isAssignableFrom(this.clazz))
+            throw new BeanInitializationException(
+                "Property 'clazz' must be a class implementing Categorizable");
 
-        ordered = Ordered.class.isAssignableFrom(this.clazz);
+        this.ordered = Ordered.class.isAssignableFrom(this.clazz);
         
     }
 }

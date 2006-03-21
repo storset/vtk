0fc6b432c07c1c38c80ebf66936ac19d84cab9f5
Kaj Hejer <kajh@usit.uio.no>
2006-03-21 16:22:39 +0000

    the beginning of a testcase for VTK-260

---
+++ test/java/org/vortikal/web/controller/DisplayXmlResourceControllerTestCase.java
@@ -0,0 +1,106 @@
+package org.vortikal.web.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+import org.jmock.cglib.MockObjectTestCase;
+import org.jmock.Mock;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.vortikal.repository.Property;
+import org.vortikal.repository.Repository;
+import org.vortikal.repository.Resource;
+import org.vortikal.security.SecurityContext;
+import org.vortikal.web.RequestContext;
+
+public class DisplayXmlResourceControllerTestCase extends MockObjectTestCase {
+
+    private static Log logger = LogFactory.getLog(DisplayXmlResourceControllerTestCase.class);
+
+    private String schemaPropertyName = "schema";
+
+    private String schemaNamespace = " http://www.uio.no/vortex/custom-properties";
+
+    private String faqSchema = "http://www.uio.no/xsd/uio/faq/v001/faq.xsd";
+
+    private Mock mockRepository;
+
+    private HttpServletRequest request;
+
+    private DisplayXmlResourceController controller;
+
+    private String uri = "/hest.xml";
+
+    private String token;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        BasicConfigurator.configure();
+        request = new MockHttpServletRequest();
+        controller = new DisplayXmlResourceController();
+        RequestContext requestContext = new RequestContext(request, null, uri);
+        RequestContext.setRequestContext(requestContext);
+        token = "";
+        SecurityContext securityContext = new SecurityContext(token, null);
+        SecurityContext.setSecurityContext(securityContext);
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testLastModified() {
+
+        long lastModified;
+
+        Property schemaProperty = new Property();
+        schemaProperty.setNamespace(schemaPropertyName);
+        schemaProperty.setName(schemaPropertyName);
+        schemaProperty.setValue(faqSchema);
+
+        Date lastModifiedExpected = new Date();
+        Mock mockResource = mock(Resource.class, "mockResource");
+        mockResource.expects(atLeastOnce()).method("isCollection").withNoArguments().will(
+                returnValue(false));
+        mockResource.expects(atLeastOnce()).method("getLastModified").withNoArguments().will(
+                returnValue(lastModifiedExpected));
+        mockResource.expects(atLeastOnce()).method("getProperty").with(eq(schemaNamespace),
+                eq(schemaPropertyName)).will(returnValue(schemaProperty));
+        mockResource.expects(atLeastOnce()).method("getName").withNoArguments().will(
+                returnValue("test resource"));
+        Resource resource = (Resource) mockResource.proxy();
+
+        mockRepository = mock(Repository.class);
+        mockRepository.expects(atLeastOnce()).method("retrieve").with(eq(token), eq(uri), eq(true))
+                .will(returnValue(resource));
+
+        assertNotNull(mockRepository);
+        controller.setRepository((Repository) mockRepository.proxy());
+
+        lastModified = controller.getLastModified(request);
+        assertEquals(-1, lastModified);
+
+        controller.setHandleLastModified(true);
+        lastModified = controller.getLastModified(request);
+        assertEquals(lastModifiedExpected.getTime(), lastModified);
+
+        List schemaList = new ArrayList();
+        schemaList.add(faqSchema);
+//        controller.setSchemasForHandleLastModified(schemaList);
+//        controller.setHandleLastModifiedForSchemasInList(true);
+//        lastModified = controller.getLastModified(request);
+//        assertEquals(lastModifiedExpected.getTime(), lastModified);
+//        
+//        controller.setSchemasForHandleLastModified(schemaList);
+//        controller.setHandleLastModifiedForSchemasInList(false);
+//        lastModified = controller.getLastModified(request);
+//        assertEquals(-1, lastModified);
+
+    }
+}
+

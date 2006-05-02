36c9f80bcaedd87570065465c7286e6f3c39ea0b
Øyvind Stegard <oyviste@usit.uio.no>
2006-05-02 10:27:23 +0000

    Oops.

--- src/java/org/vortikal/repositoryimpl/query/Reindexer.java
+++ src/java/org/vortikal/repositoryimpl/query/Reindexer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.InitializingBean;
 import org.vortikal.repository.PropertySet;
 import org.vortikal.repository.query.PropertySetIndex;
 import org.vortikal.repositoryimpl.dao.IndexDataAccessor;
@@ -17,13 +19,31 @@ import org.vortikal.repositoryimpl.dao.ResultSetIterator;
  * @author oyviste
  *
  */
-public class Reindexer {
+public class Reindexer implements InitializingBean {
 
    private IndexDataAccessor indexDataAccessor;
    private PropertySetIndex index;
    
    Log logger = LogFactory.getLog(Reindexer.class);
 
+   public void afterPropertiesSet() {
+       
+       if (indexDataAccessor == null) {
+           throw new BeanInitializationException("Property 'indexDataAccessor' not set.");
+       } else if (index == null) {
+           throw new BeanInitializationException("Property 'index' not set.");
+       }
+       
+       new Thread(new Runnable() {
+           public void run() {
+               try {
+                   Thread.sleep(6000);
+               } catch (InterruptedException ie) {}
+               Reindexer.this.run();
+           }
+       }).start();
+   }
+   
    public synchronized void run() throws IndexException {
     
        if (!index.lock()) {
@@ -41,8 +61,10 @@ public class Reindexer {
                    logger.debug("Adding property set at URI '" 
                            + propertySet.getURI() + "' to index.");
                }
-               index.addPropertySet((PropertySet)i.next());
+               index.addPropertySet(propertySet);
            }
+           
+           index.commit();
        } catch (IOException io) {
            throw new IndexException(io);
        } finally {
@@ -64,5 +86,5 @@ public class Reindexer {
    public void setIndex(PropertySetIndex index) {
        this.index = index;
    }
-    
+   
 }

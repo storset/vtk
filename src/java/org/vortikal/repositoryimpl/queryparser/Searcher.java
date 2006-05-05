41228604189aee7bb67a62a26d5cac553c1c6ac6
Øyvind Stegard <oyviste@usit.uio.no>
2006-05-05 14:58:19 +0000

    Copied some JavaDoc from RepositorySearcher interface, which is not in use, but otherwise equal.

--- src/java/org/vortikal/repositoryimpl/queryparser/Searcher.java
+++ src/java/org/vortikal/repositoryimpl/queryparser/Searcher.java
@@ -30,21 +30,78 @@
  */
 package org.vortikal.repositoryimpl.queryparser;
 
+import org.vortikal.repository.query.QueryException;
 import org.vortikal.repositoryimpl.query.query.Query;
 
 /**
  * Simple search interface
  *
- * Created: Wed May  5 21:39:46 2004
  *
  */
 public interface Searcher {
 
+    /**
+     * Executes a query on repository resources.
+     * 
+     * @param token The security token associated with the principal 
+     *              executing the query.
+     * @param query The <code>Query</code> object, containing the query
+     *              conditions.
+     * @return      A <code>ResultSet</code> containing the results.
+     * 
+     * @see org.vortikal.repository.query.Query
+     * @see org.vortikal.repository.query.ResultSet
+     *              
+     * @throws QueryException If the query could not be executed.
+     */
     public ResultSet execute(String token, Query query) throws QueryException;
     
+    /**
+     * Perform a query on repository resources with a hard limit on how
+     * many results that should be returned.
+     * 
+     * @param token The security token associated with the principal 
+     *              executing the query.
+     * @param query The <code>Query</code> object, containing the query
+     *              conditions.
+     * @return      A <code>ResultSet</code> containing the results.
+     * 
+     * @param maxResults Maximum number of desired results in the returned
+     *                   result set. If the query produces more hits than
+     *                   this limit, the overflowing results are discarded.
+     * 
+     * @return      A <code>ResultSet</code> containing the results.
+     *
+     * @throws QueryException If the query could not be executed.
+     */
     public ResultSet execute(String token, Query query, int maxResults) throws
         QueryException;
 
+
+    /**
+     * Executes a query on repository resources with a hard limit on how
+     * many results that should be returned, in addition to a cursor. 
+     * 
+     * At any given time, the <code>Query</code> alone will produce a complete result
+     * set. The <code>cursor</code> and <code>maxResults</code> parameters
+     * can be used to fetch subsets of this result set. Useful for implementing
+     * paging when browsing large result sets.
+     * 
+     * The implementation must take into consideration what happens
+     * when the complete result set changes between queries with 
+     * cursor/maxResults. 
+     * 
+     * @param token The security token associated with the principal 
+     *              executing the query.
+     * @param query The <code>Query</code> object, containing the query
+     *              conditions.
+     * @param maxResults Number of results to include from cursor position.
+     * @param cursor     Positition to start in the query result set.
+     * 
+     * @return      A <code>ResultSet</code> containing a subset of the results.
+     * 
+     * @throws QueryException If the query could not be executed.
+     */
     public ResultSet execute(String token, Query query, int maxResults,
                           int cursor) throws QueryException;
 

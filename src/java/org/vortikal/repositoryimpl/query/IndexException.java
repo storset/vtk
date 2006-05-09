3d2e210751345e047102dfae5e287b5718b46e56
Øyvind Stegard <oyviste@usit.uio.no>
2006-05-09 10:37:06 +0000

    Fix compilation error

---
+++ src/java/org/vortikal/repositoryimpl/query/IndexException.java
@@ -0,0 +1,25 @@
+package org.vortikal.repositoryimpl.query;
+
+public class IndexException extends QueryException {
+
+    public IndexException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public IndexException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public IndexException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public IndexException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}

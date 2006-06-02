dc611aec9d2be14160475a94456cffab306ca642
Kaj Hejer <kajh@usit.uio.no>
2006-06-02 11:34:03 +0000

    removed old code

--- src/java/org/vortikal/webdav/ifheader/IfHeader.java
+++ src/java/org/vortikal/webdav/ifheader/IfHeader.java
@@ -24,9 +24,7 @@ import org.vortikal.repository.Resource;
 public interface IfHeader {
 
     public boolean matches(Resource resource, boolean shouldMatchOnNoIfHeader);
-    
-    //public boolean matchesEtags(Resource resource, boolean shouldMatchOnNoIfHeader);
-    
+        
     public Iterator getAllTokens();
 
     public Iterator getAllNotTokens();

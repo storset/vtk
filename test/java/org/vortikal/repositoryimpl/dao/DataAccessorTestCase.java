6434d3a07ef12448452f10f3e32fdd52c348bd1b
Gorm Paulsen <gormap@usit.uio.no>
2006-07-21 13:43:49 +0000

    Add some (ACL-related) test cases for the DataAccessor interface.

---
+++ test/java/org/vortikal/repositoryimpl/dao/DataAccessorTestCase.java
@@ -0,0 +1,118 @@
+/* Copyright (c) 2006, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.repositoryimpl.dao;
+
+import java.io.IOException;
+import junit.framework.TestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.vortikal.repository.AbstractRepositoryTestCase;
+import org.vortikal.repository.Acl;
+import org.vortikal.repository.Repository;
+import org.vortikal.repositoryimpl.PropertyManager;
+import org.vortikal.repositoryimpl.PropertyManagerImpl;
+import org.vortikal.repositoryimpl.ResourceImpl;
+import org.vortikal.security.Principal;
+import org.vortikal.security.PrincipalManager;
+import org.vortikal.security.token.TokenManager;
+
+
+public class DataAccessorTestCase extends AbstractRepositoryTestCase {
+
+
+    protected void setUp() throws Exception {
+        System.setProperty("org.apache.commons.logging.Log",
+                           "org.apache.commons.logging.impl.Log4JLogger");
+        System.setProperty("log4j.configuration",
+                           "log4j.test.xml");
+        super.setUp();
+    }
+
+
+    private PropertyManagerImpl getPropertyManager() {
+        return (PropertyManagerImpl) getApplicationContext().getBean("propertyManager");
+    }
+    
+
+    private DataAccessor getDataAccessor() {
+        return (DataAccessor) getApplicationContext().getBean("repository.backend");
+    }
+    
+
+    public void testChangeAclInheritance() throws Exception {
+        PropertyManagerImpl propertyManager = getPropertyManager();
+        DataAccessor dao = getDataAccessor();
+        Principal rootPrincipal = getPrincipalManager().getUserPrincipal("root@localhost");
+
+        ResourceImpl root = dao.load("/");
+
+        // Create /parent with inherited ACL:
+        String parentURI = "/parent";
+        ResourceImpl parent = propertyManager.create(rootPrincipal, parentURI, true);
+        parent.setACL((Acl) root.getAcl().clone());
+        parent.setAclInheritedFrom(root.getID());
+        parent.getAcl().setInherited(true);
+        dao.store(parent);
+        parent = dao.load(parentURI);        
+        assertEquals(root.getID(), parent.getAclInheritedFrom());
+        
+        // Create /parent/child with inherited ACL:
+        String childURI = parentURI + "/child";
+        ResourceImpl child = propertyManager.create(rootPrincipal, childURI, false);
+        child.setACL((Acl) root.getAcl().clone());
+        child.setAclInheritedFrom(root.getID());
+        child.getAcl().setInherited(true);
+        dao.store(child);
+        child = dao.load(childURI);
+        assertEquals(root.getID(), child.getAclInheritedFrom());
+
+        // Set parent's ACL inherited = false
+        parent.getAcl().setInherited(false);
+        dao.store(parent);
+        parent = dao.load(parentURI);
+        child = dao.load(childURI);
+        assertEquals(parent.getID(), child.getAclInheritedFrom());
+
+        // Create /parent/second-child with inherited ACL:
+        String secondChildURI = parentURI + "/second-child";
+        ResourceImpl secondChild = propertyManager.create(rootPrincipal, secondChildURI, false);
+        secondChild.setACL((Acl) parent.getAcl().clone());
+        secondChild.setAclInheritedFrom(parent.getID());
+        secondChild.getAcl().setInherited(true);
+        dao.store(secondChild);
+        secondChild = dao.load(secondChildURI);
+        assertEquals(parent.getID(), secondChild.getAclInheritedFrom());
+
+    }
+    
+    
+}
+

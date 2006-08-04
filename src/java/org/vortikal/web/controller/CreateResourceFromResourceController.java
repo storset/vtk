92ca3f11c3337b20aa256a1f5f509ad5ca888367
Gorm Paulsen <gormap@usit.uio.no>
2006-08-04 15:09:07 +0000

    Generalization of CreateResourceFromResource controller

--- src/java/org/vortikal/web/controller/CreateResourceFromResourceController.java
+++ src/java/org/vortikal/web/controller/CreateResourceFromResourceController.java
@@ -34,6 +34,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -49,35 +50,42 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-import org.vortikal.repository.Namespace;
 import org.vortikal.repository.Property;
 import org.vortikal.repository.Repository;
 import org.vortikal.repository.Resource;
+import org.vortikal.repository.resourcetype.PropertyTypeDefinition;
+import org.vortikal.repository.resourcetype.Value;
 import org.vortikal.security.SecurityContext;
 import org.vortikal.web.RequestContext;
 import org.vortikal.xml.TransformerManager;
 
 
 /**
- * Controller that creates a resource from another resource.
- * TODO: Make this generic
+ * Controller that creates a resource from another resource using
+ * XSLT transformation.
  * 
  * <p>Configurable properties:
  * <ul>
- *   <li><code>repository</code> - the repository is required</li>
- *   <li><code>resourceName</code> - Create new resource with this name</li>
- * 	 <li><code>transformerManager</code> - required
- * 	 <li><code>stylesheetIdentifier</code> - An identifier for the XSL Stylesheet 
- *   to use - required
- * 	 <li><code>successView</code> - default is 'redirect'
- * 	 <li><code>errorView</code> - default is 'admin'
+ *   <li><code>repository</code> - the {@link Repository} (required)
+ *   <li><code>resourceName</code> - Create the new resource with this name
+ * 	 <li><code>transformerManager</code> - a {@link TransformerManager} (required)
+ * 	 <li><code>stylesheetIdentifier</code> - An identifier for the
+ * 	 XSL Stylesheet to use (required)
+ * 	 <li><code>successView</code> - the success view name (required)
+ * 	 <li><code>errorView</code> - the error view (required)
+ * 	 <li><code>initialResourceProperties</code> - a map from
+ * 	 {@link PropertyTypeDefinition} to {@link Value} objects, listing
+ * 	 initial properties that are set on the created resource.
+ * 	 <li><code>resourceAlreadyExistsMessageKey</code> - the
+ * 	 localization key to put in the model when a resource of the
+ * 	 target name already exists.
  * </ul>
  * </p>
  *
  * <p>Model data provided:
  * <ul>
  *   <li><code>resource</code> - the resource to redirect to on success
- * 	 <li><code>error</code> - error message on error
+ *   <li><code>error</code> - error message on error
  * </ul>
  */
 public class CreateResourceFromResourceController implements Controller,
@@ -91,87 +99,9 @@ public class CreateResourceFromResourceController implements Controller,
     private String stylesheetIdentifier;
     private String errorView;
     private String successView;
+    private Map initialResourceProperties = new HashMap();
+    private String resourceAlreadyExistsMessageKey = "manage.create.document.exists";
     
-    public ModelAndView handleRequest(HttpServletRequest req,
-            HttpServletResponse resp) throws Exception {
-
-        Map model = new HashMap();
-
-        String uri = RequestContext.getRequestContext().getResourceURI();
-        String token = SecurityContext.getSecurityContext().getToken();
-
-        Resource resource = this.repository.retrieve(token, uri, false);
-        String newResourceUri = uri.substring(0, uri.lastIndexOf("/") + 1)
-                + this.resourceName;
-
-        boolean exists = this.repository.exists(token, newResourceUri);
-        if (exists) {
-            model.put("createErrorMessage", "minutes.exists");
-            return new ModelAndView(this.errorView, model);
-        }
-
-        // repository.lock(token,newResourceUri,Lock.LOCKTYPE_EXCLUSIVE_WRITE,"ownerInfo","0",5);
-
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        InputStream inStream = this.repository.getInputStream(token, uri, true);
-
-        Transformer transformer = 
-            this.transformerManager.getTransformer(this.stylesheetIdentifier);
-
-        transformer.transform(new StreamSource(inStream), 
-                new StreamResult(outStream));
-
-        InputStream in = new ByteArrayInputStream(outStream.toByteArray());
-
-        Resource newResource = this.repository.createDocument(token, newResourceUri);
-
-        Namespace namespace = Namespace.CUSTOM_NAMESPACE;
-
-        // Setting DAV-properties for webedit and transform-view to yes
-        Property p = newResource.createProperty(namespace, "web-edit");
-        p.setStringValue("yes");
-
-        p = newResource.createProperty(namespace, "transform-view");
-        p.setStringValue("yes");
-
-        p = newResource.createProperty(namespace, "visual-profile");
-        p.setStringValue("yes");
-
-        this.repository.store(token, newResource);
-        this.repository.storeContent(token, newResourceUri, in);
-
-        Resource parent = null;
-        parent = this.repository.retrieve(token, resource.getParent(), false);
-        model.put("resource", parent);
-
-        return new ModelAndView(this.successView, model);
-    }
-
-    public void afterPropertiesSet() throws Exception {
-        if (this.repository == null) 
-            throw new BeanInitializationException("Property 'repository' required");
-        
-        if (this.resourceName == null)
-            throw new BeanInitializationException("Property 'resourceName' required");
-    
-        if (this.transformerManager == null)
-            throw new BeanInitializationException("Property 'transformerManager' required");
-        
-        if (this.stylesheetIdentifier == null)
-            throw new BeanInitializationException("Property 'stylesheetIdentifier' required");
-
-        if (this.successView == null)
-            throw new BeanInitializationException("Property 'successView' required");
-
-        if (this.errorView == null)
-            throw new BeanInitializationException("Property 'errorView' required");
-
-        try {
-            this.transformerManager.getTransformer(this.stylesheetIdentifier);
-        } catch (Exception e) {
-            this.logger.warn("Error trying to compile stylesheet '" + this.stylesheetIdentifier + "'", e);
-        }
-    }
 
     public void setErrorView(String errorView) {
         this.errorView = errorView;
@@ -196,5 +126,106 @@ public class CreateResourceFromResourceController implements Controller,
     public void setTransformerManager(TransformerManager transformerManager) {
         this.transformerManager = transformerManager;
     }
+
+    public void setResourceAlreadyExistsMessageKey(String resourceAlreadyExistsMessageKey) {
+        this.resourceAlreadyExistsMessageKey = resourceAlreadyExistsMessageKey;
+    }
+    
+
+    public void setInitialResourceProperties(Map initialResourceProperties) {
+        if (initialResourceProperties == null) return;
+
+        for (Iterator i = initialResourceProperties.keySet().iterator(); i.hasNext();) {
+            Object key = i.next();
+            if (!(key instanceof PropertyTypeDefinition)) 
+                throw new IllegalArgumentException("All keys must be of class "
+                                                   + PropertyTypeDefinition.class.getName());
+            Object value = initialResourceProperties.get(key);
+            if (!(value instanceof Value)) 
+                throw new IllegalArgumentException("All values must be of class "
+                                                   + Value.class.getName());
+        }
+        this.initialResourceProperties = initialResourceProperties;
+    }
+    
+
+    public void afterPropertiesSet() throws Exception {
+        if (this.repository == null) 
+            throw new BeanInitializationException("Property 'repository' required");
+        
+        if (this.resourceName == null)
+            throw new BeanInitializationException("Property 'resourceName' required");
+    
+        if (this.transformerManager == null)
+            throw new BeanInitializationException("Property 'transformerManager' required");
+        
+        if (this.stylesheetIdentifier == null)
+            throw new BeanInitializationException("Property 'stylesheetIdentifier' required");
+
+        if (this.successView == null)
+            throw new BeanInitializationException("Property 'successView' required");
+
+        if (this.errorView == null)
+            throw new BeanInitializationException("Property 'errorView' required");
+
+        try {
+            this.transformerManager.getTransformer(this.stylesheetIdentifier);
+        } catch (Exception e) {
+            this.logger.warn("Error trying to compile stylesheet '" +
+                             this.stylesheetIdentifier + "'", e);
+        }
+    }
+
+
+    public ModelAndView handleRequest(HttpServletRequest req,
+            HttpServletResponse resp) throws Exception {
+
+        Map model = new HashMap();
+
+        String uri = RequestContext.getRequestContext().getResourceURI();
+        String token = SecurityContext.getSecurityContext().getToken();
+
+        Resource resource = this.repository.retrieve(token, uri, false);
+        String newResourceUri = uri.substring(0, uri.lastIndexOf("/") + 1)
+                + this.resourceName;
+
+        boolean exists = this.repository.exists(token, newResourceUri);
+        if (exists) {
+            model.put("createErrorMessage", this.resourceAlreadyExistsMessageKey);
+            return new ModelAndView(this.errorView, model);
+        }
+
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        InputStream inStream = this.repository.getInputStream(token, uri, true);
+
+        Transformer transformer = 
+            this.transformerManager.getTransformer(this.stylesheetIdentifier);
+
+        transformer.transform(new StreamSource(inStream), 
+                new StreamResult(outStream));
+
+        InputStream in = new ByteArrayInputStream(outStream.toByteArray());
+
+        Resource newResource = this.repository.createDocument(token, newResourceUri);
+        this.repository.storeContent(token, newResourceUri, in);
+
+        if (this.initialResourceProperties != null) {
+            newResource = this.repository.retrieve(token, newResourceUri, true);
+            for (Iterator i = this.initialResourceProperties.keySet().iterator(); i.hasNext();) {
+                PropertyTypeDefinition def = (PropertyTypeDefinition) i.next();
+                Value value = (Value) this.initialResourceProperties.get(def);
+                Property prop = newResource.createProperty(def.getNamespace(), def.getName());
+                prop.setValue(value);
+            }
+            this.repository.store(token, newResource);
+        }
+
+        Resource parent = null;
+        parent = this.repository.retrieve(token, resource.getParent(), false);
+        model.put("resource", parent);
+
+        return new ModelAndView(this.successView, model);
+    }
+
 
 }

cda98664420aff40fac6866565f50b170f6ea31e
Gorm Paulsen <gormap@usit.uio.no>
2006-09-29 09:43:55 +0000

    Build a DOM tree of the document before writing, replace or add Content-Type meta element

--- src/java/org/vortikal/web/controller/repository/tidy/JTidyTransformer.java
+++ src/java/org/vortikal/web/controller/repository/tidy/JTidyTransformer.java
@@ -39,8 +39,17 @@ import java.nio.charset.Charset;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.vortikal.repository.Resource;
+import org.vortikal.util.io.StreamUtil;
+import org.vortikal.util.text.TextUtils;
 import org.vortikal.web.controller.repository.copy.Filter;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.w3c.tidy.Configuration;
 import org.w3c.tidy.Tidy;
 
@@ -56,7 +65,9 @@ public class JTidyTransformer implements Filter {
     private static boolean xhtml = true;
     private static String doctype = "transitional"; // "loose" will also be equivalent
     
-    public InputStream transform(InputStream inStream, Resource resource) {
+
+
+    public InputStream transformOLD(InputStream inStream, Resource resource) {
         
         try {
             Tidy tidy = new Tidy();
@@ -91,7 +102,6 @@ public class JTidyTransformer implements Filter {
             ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
             
             tidy.parse(inStream, outBuffer);
-            
             byte[] byteArrayBuffer = outBuffer.toByteArray();
             ByteArrayInputStream bais = new ByteArrayInputStream(byteArrayBuffer);
             
@@ -105,4 +115,85 @@ public class JTidyTransformer implements Filter {
             return new ByteArrayInputStream(null);
         }
     }
+
+    public InputStream transform(InputStream inStream, Resource resource) {
+        
+        try {
+            Tidy tidy = new Tidy();
+                        
+            // Setting up Tidy (default) output
+            tidy.setInputStreamName(inStream.getClass().getName());
+            tidy.setTidyMark(tidyMark);
+            tidy.setMakeClean(makeClean);
+            tidy.setShowWarnings(showWarnings);
+            // tidy.setOnlyErrors(onlyErrors); // If set TRUE, then only error
+                                               // messages are written to the
+                                               // OutputStream (i.e. no file
+                                               // content is written)
+            tidy.setQuiet(quiet);
+            tidy.setXHTML(xhtml);
+            tidy.setDocType(doctype); 
+            tidy.setCharEncoding(Configuration.UTF8);
+
+            byte[] buffer = StreamUtil.readInputStream(inStream);
+            String s = new String(buffer, resource.getCharacterEncoding());
+            InputStream newStream = StreamUtil.stringToStream(s, "utf-8");
+
+            Document document = tidy.parseDOM(newStream, null);
+            alterContentTypeMetaElement(document);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            tidy.pprint(document, outputStream);
+            
+            byte[] byteArrayBuffer = outputStream.toByteArray();
+            ByteArrayInputStream bais = new ByteArrayInputStream(byteArrayBuffer);
+            
+            outputStream.close();
+            bais.reset(); // must reset buffer pointer to [0]
+
+            return bais;
+
+        } catch (IOException e) {
+            logger.error("Caught exception", e);
+            return new ByteArrayInputStream(null);
+        }
+    }
+
+    
+
+
+    private void alterContentTypeMetaElement(Document doc) {
+        String encoding = null;
+
+        NodeList headNodes = doc.getElementsByTagName("head");
+        Node head = headNodes.item(0);
+        Node remove = null;
+
+        NodeList metaElements = doc.getElementsByTagName("meta");
+        for (int i = 0; i < metaElements.getLength(); i++) {
+            Node meta = metaElements.item(i);
+
+            if (meta.hasAttributes()) {
+                NamedNodeMap attrMap = meta.getAttributes();
+                Node httpEquivAttr = attrMap.getNamedItem("http-equiv");
+
+                if (httpEquivAttr != null) {
+                    String httpEquiv = httpEquivAttr.getNodeValue();
+
+                    if (httpEquiv!= null && "Content-Type".toLowerCase().equals(httpEquiv.toLowerCase())) {
+                        remove = doc.removeChild(meta);
+                        break;
+                    }
+                }
+            }
+        }
+        if (remove != null) {
+            remove.getParentNode().removeChild(remove);
+        }
+        Element meta = doc.createElement("meta");
+        meta.setAttribute("http-equiv", "Content-Type;charset=utf-8");
+        head.appendChild(meta);
+    }
+
+
 }

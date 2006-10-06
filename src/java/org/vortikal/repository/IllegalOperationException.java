53f151d53ac1be411dc20bf78ae401ba3ababe25
Gorm Paulsen <gormap@usit.uio.no>
2006-10-06 09:48:52 +0000

    Add constructor

--- src/java/org/vortikal/repository/IllegalOperationException.java
+++ src/java/org/vortikal/repository/IllegalOperationException.java
@@ -41,4 +41,8 @@ public class IllegalOperationException extends RepositoryException {
     public IllegalOperationException(String message) {
         super(message);
     }
+
+    public IllegalOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

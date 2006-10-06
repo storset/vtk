721e64d655f5844d77ed57ed78d232485a76116d
Øyvind Stegard <oyviste@usit.uio.no>
2006-10-06 12:18:57 +0000

    remove TODOs

--- src/java/org/vortikal/repositoryimpl/query/SortBuilderException.java
+++ src/java/org/vortikal/repositoryimpl/query/SortBuilderException.java
@@ -38,22 +38,18 @@ public class SortBuilderException extends QueryException {
 
     public SortBuilderException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public SortBuilderException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 
     public SortBuilderException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public SortBuilderException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
 }

c52c5babd01a35c7da704134dae006fd0837d292
Eirik Meland <eirikme@usit.uio.no>
2006-10-12 06:33:35 +0000

    added check for RESIN_HOME

--- resin-start.sh
+++ resin-start.sh
@@ -2,4 +2,10 @@
 
 CONF=target/vortikal-0.3.2-SNAPSHOT/WEB-INF/classes/resin.conf
 
-$RESIN_HOME/bin/httpd.sh -conf $CONF
+# check if RESIN_HOME is set
+if test -z $RESIN_HOME; then
+    echo "\$RESIN_HOME must be set to use this script";
+    echo "(set it in .bashrc)";
+else
+    $RESIN_HOME/bin/httpd.sh -conf $CONF;
+fi

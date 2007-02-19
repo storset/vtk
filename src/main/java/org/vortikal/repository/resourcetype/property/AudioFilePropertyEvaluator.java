06fcd972de6dc004ea78d220130b7047b6011258
Gorm Paulsen <gormap@usit.uio.no>
2007-02-19 17:26:28 +0000

    Add audio file properties (with optional support for MP3)

---
+++ src/main/java/org/vortikal/repository/resourcetype/property/AudioFilePropertyEvaluator.java
@@ -0,0 +1,85 @@
+/* Copyright (c) 2007, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.repository.resourcetype.property;
+
+import java.util.Date;
+import javax.sound.sampled.AudioFileFormat;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.vortikal.repository.Property;
+import org.vortikal.repository.PropertySet;
+import org.vortikal.repository.resourcetype.Content;
+import org.vortikal.repository.resourcetype.ContentModificationPropertyEvaluator;
+import org.vortikal.security.Principal;
+
+
+public class AudioFilePropertyEvaluator implements ContentModificationPropertyEvaluator {
+
+    private static Log logger = LogFactory.getLog(AudioFilePropertyEvaluator.class);
+
+    private String audioFileProperty;
+
+    public void setAudioFileProperty(String audioFileProperty) {
+        this.audioFileProperty = audioFileProperty;
+    }
+    
+
+    public boolean contentModification(Principal principal, Property property,
+            PropertySet ancestorPropertySet, Content content, Date time)
+            throws PropertyEvaluationException {
+
+        AudioFileFormat audioFileFormat = null;
+
+        try {
+            audioFileFormat = (AudioFileFormat) content.getContentRepresentation(
+                AudioFileFormat.class);
+        } catch (Exception e) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Unable to get AudioFileFormat representation of content", e);
+            }
+            return false;
+        }
+
+        String propertyValue = (String) audioFileFormat.properties()
+            .get(this.audioFileProperty);
+
+        if (propertyValue == null) {
+            return false;
+        }
+        property.setStringValue(propertyValue);
+        return true;
+    }
+
+    
+}
+

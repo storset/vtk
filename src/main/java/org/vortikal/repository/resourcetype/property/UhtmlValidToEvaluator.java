673a665693edaf00a9d3c1aa477dda7aed423972
Jo Størset <storset@usit.uio.no>
2007-02-19 15:35:00 +0000

    Tmp. html extracting

---
+++ src/main/java/org/vortikal/repository/resourcetype/property/UhtmlValidToEvaluator.java
@@ -0,0 +1,132 @@
+/* Copyright (c) 2006, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.repository.resourcetype.property;
+
+import java.util.Date;
+
+import org.vortikal.repository.Property;
+import org.vortikal.repository.PropertySet;
+import org.vortikal.repository.resourcetype.Content;
+import org.vortikal.repository.resourcetype.ContentModificationPropertyEvaluator;
+import org.vortikal.repository.resourcetype.Value;
+import org.vortikal.repository.resourcetype.ValueFactory;
+import org.vortikal.security.Principal;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+
+public class UhtmlValidToEvaluator implements ContentModificationPropertyEvaluator {
+
+    
+    private ValueFactory valueFactory;
+
+    public boolean contentModification(Principal principal, Property property,
+            PropertySet ancestorPropertySet, Content content, Date time)
+            throws PropertyEvaluationException {
+        
+        Document document = null;
+        
+        try {
+            document = (Document) content.getContentRepresentation(Document.class);
+        } catch (Exception e) {
+            throw new PropertyEvaluationException(
+                    "Unable to get DOM representation of content", e);
+        }
+
+        String validTo = getMetadata(document.getDocumentElement());
+        if (validTo == null) {
+            return false;
+        }                
+        
+        Value value = null;
+
+        if (this.valueFactory != null) {
+            int type = property.getDefinition().getType();
+            try {
+                value = this.valueFactory.createValue(validTo, type);
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            value = new Value(validTo);
+        }
+
+        property.setValue(value);
+        return true;
+    }
+
+
+    private String getMetadata(Node node) {
+        if (node == null || node.getNodeType() != Node.ELEMENT_NODE) {
+            return null;
+        }
+        
+        String nodeName = node.getNodeName();
+
+        if (nodeName.equals("meta")) {
+            if (!node.hasAttributes())
+                return null;
+            
+            NamedNodeMap nnm = node.getAttributes();
+            Node name = nnm.getNamedItem("name");
+            Node value = nnm.getNamedItem("value");
+            
+            if (name == null || value == null || !"dato.holdbarhet".equals(name.getNodeValue()))
+                return null;
+
+            return value.getNodeValue();
+        } else if (!"body".equals(nodeName) && !"link".equals(nodeName) && !"title".equals(nodeName)) {
+            // Invoke the recursive method on the children (if any).
+            NodeList children = node.getChildNodes();
+            if (children != null) {
+                int len = children.getLength();
+                for (int i = 0; i < len; i++) {
+                    String s = getMetadata(children.item(i));
+                    if (s != null)
+                        return s;
+                }
+            }
+        }
+        
+        return null;
+
+    }
+
+
+    public void setValueFactory(ValueFactory valueFactory) {
+        this.valueFactory = valueFactory;
+    }
+
+    
+}

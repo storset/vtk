e99323660dbd4b8fc899f33890f131e5b277ad51
Gorm Paulsen <gormap@usit.uio.no>
2007-02-19 17:35:10 +0000

    Add simple caching to RSS component

--- src/main/java/org/vortikal/web/view/decorating/components/RssFeedComponent.java
+++ src/main/java/org/vortikal/web/view/decorating/components/RssFeedComponent.java
@@ -37,7 +37,9 @@ import com.sun.syndication.io.XmlReader;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -53,22 +55,54 @@ import org.vortikal.web.servlet.BufferedResponse;
 import org.vortikal.web.view.decorating.DecoratorRequest;
 import org.vortikal.web.view.decorating.DecoratorResponse;
 
+// XXX: this class currently depends on the thread safety of the
+// SyndFeed implementation: if it turns out that it is not thread
+// safe, its data has to be extracted data to a custom bean after
+// fetching a feed.
+// 
 public class RssFeedComponent extends AbstractDecoratorComponent {
 
     private static Log logger = LogFactory.getLog(RssFeedComponent.class);
     
-    private String identifier = "feed";
+    private int readTimeout = -1;
+    private int connectTimeout = -1;
+
+    private Map cache = new HashMap();
+    private long cacheTimeout = 5 * 60 * 1000;
+
+    private String identifier = "Anonymous Feed Fetcher";
+
     private View view;
 
+    private String defaultDateFormat = "yyyy-MM-dd HH:mm";
 
     public void setView(View view) {
         this.view = view;
     }
     
-    public String getIdentifier() {
-        return this.identifier;
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
     }
 
+    public void setConnectTimeoutSeconds(int connectTimeout) {
+        this.connectTimeout = connectTimeout * 1000;
+    }
+
+    public void setReadTimeoutSeconds(int readTimeout) {
+        this.readTimeout = readTimeout * 1000;
+    }
+    
+
+    public void setCacheSeconds(int cacheSeconds) {
+        this.cacheTimeout = cacheSeconds * 1000;
+    }
+    
+    public void setDefaultDateFormat(String defaultDateFormat) {
+        if (defaultDateFormat == null) {
+            throw new IllegalArgumentException("Date format not valid");
+        }
+        this.defaultDateFormat = defaultDateFormat;
+    }
 
     public void render(DecoratorRequest request, DecoratorResponse response)
         throws Exception {
@@ -88,6 +122,11 @@ public class RssFeedComponent extends AbstractDecoratorComponent {
         boolean includeUpdatedDate = "true".equals(request.getParameter(
                                                        "includeUpdatedDate"));
 
+        String dateFormat = request.getStringParameter("dateFormat");
+        if (dateFormat == null) {
+            dateFormat = this.defaultDateFormat;
+        }
+
         Integer maxMsgs = new Integer(5);
         String numStr = request.getStringParameter("maxMsgs");
         if (numStr != null) {
@@ -96,10 +135,8 @@ public class RssFeedComponent extends AbstractDecoratorComponent {
             } catch (Exception e) { }
         }
 
-        XmlReader xmlReader = new XmlReader(new URL(address));
-        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = getFeed(address);
 
-        SyndFeed feed = input.build(xmlReader);
         Map conf = new HashMap();
         conf.put("includeLogo", new Boolean(includeLogo));
         conf.put("includeTitle", new Boolean(includeTitle));
@@ -111,7 +148,7 @@ public class RssFeedComponent extends AbstractDecoratorComponent {
         Map model = new HashMap();
 
         model.put("feed", feed);
-        model.put("dateFormatter", new DateFormatter());
+        model.put("dateFormatter", new DateFormatter(dateFormat));
         model.put("conf", conf);
         BufferedResponse tmpResponse = new BufferedResponse();
 
@@ -132,24 +169,138 @@ public class RssFeedComponent extends AbstractDecoratorComponent {
         out.close();
     }
 
+    public SyndFeed getFeed(String address) throws Exception {
 
+        if (this.cacheTimeout <= 0) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Returning uncached feed: '" + address + "'");
+            }
+            return fetchFeed(address);
+        }
+
+        FeedItem feed = (FeedItem) this.cache.get(address);
+        if (feed == null) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Caching feed: '" + address + "'");
+            }
+            cacheFeed(address);
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("Returning feed '" + address + "' from cache");
+        }
+
+        feed = (FeedItem) this.cache.get(address);
+        if (feed.getTimestamp().getTime() + this.cacheTimeout <= System.currentTimeMillis()) {
+            triggerFeedRefresh(address);
+        }
+
+
+        return feed.getFeed();
+    }
+
+    private void triggerFeedRefresh(final String address) {
+        Runnable fetcher = new Runnable() {
+           public void run() {
+              try {
+                 cacheFeed(address);
+              } catch (Exception e) {
+                 logger.info("Error refreshing feed '" + address + "'", e);
+              }
+           }
+        };
+        new Thread(fetcher).start();
+    }
+    
+
+
+    private synchronized void cacheFeed(String address) throws Exception {
+
+        FeedItem item = (FeedItem) this.cache.get(address);
+        long now = new Date().getTime();
+
+        if (item == null ||
+            (item.getTimestamp().getTime() + this.cacheTimeout <= now)) {
+            SyndFeed feed = fetchFeed(address);
+            this.cache.put(address, new FeedItem(feed));
+            logger.info("Cached feed '" + address + "'");
+        }
+    }
+
+
+
+    private SyndFeed fetchFeed(String address) throws Exception {
+
+        URLConnection connection = new URL(address).openConnection();
+        setTimeouts(connection);
+
+        connection.setRequestProperty("User-Agent", this.identifier);
+        connection.setUseCaches(true);
+
+        XmlReader xmlReader = new XmlReader(connection.getInputStream());
+        SyndFeedInput input = new SyndFeedInput();
+
+        SyndFeed feed = input.build(xmlReader);
+        return feed;
+    }
+    
+
+    private void setTimeouts(URLConnection connection) {
+        // XXX: In Java 1.5 timeouts can (and should be) be specified
+        // on a URLConnection. Coding these using reflection until we
+        // are officially on 1.5:
+
+        try {
+            java.lang.reflect.Method setConnectTimeout = connection.getClass().getMethod(
+                "setConnectTimeout", new Class[]{int.class});
+            java.lang.reflect.Method setReadTimeout = connection.getClass().getMethod(
+                "setReadTimeout", new Class[]{int.class});
+            if (this.connectTimeout > 0) {
+                setConnectTimeout.invoke(connection, new java.lang.Object[]{
+                        new Integer(this.connectTimeout)});
+            }
+            if (this.readTimeout > 0) {
+                setReadTimeout.invoke(connection, new java.lang.Object[]{
+                        new Integer(this.readTimeout)});
+            }
+            
+        } catch (Throwable t) {
+            // Connection timeouts not available
+        }
+    }
+    
+    private class FeedItem {
+        private SyndFeed feed;
+        private Date timestamp;
+
+        public FeedItem(SyndFeed feed) {
+            this.feed = feed;
+            this.timestamp = new Date();
+        }
+
+        public SyndFeed getFeed() {
+            return this.feed;
+        }
+
+        public Date getTimestamp() {
+            return this.timestamp;
+        }
+    }
 
     private class DateFormatter {
-        private String DATE_FORMAT_STRING = "yyyy-MM-dd HH:mm";
-        private final SimpleDateFormat DATE_FORMAT = new
-            SimpleDateFormat(DATE_FORMAT_STRING);
+        private SimpleDateFormat dateFormat; 
 
-        /**
-         * Formats the provided date
-         * @param pDate date to format
-         * @return formatted date
-         */
-        public String formatDate(Date pDate) {
+        public DateFormatter(String dateFormat) {
+            this.dateFormat = new SimpleDateFormat(dateFormat);
+        }
+        
+        public String formatDate(Date date) {
             String formattedDate = "no date present";
-            if (pDate != null) {
-                formattedDate = DATE_FORMAT.format(pDate);
+            if (date != null) {
+                formattedDate = this.dateFormat.format(date);
             }
             return formattedDate;
         }
     }
+
+
 }

c8f9b92e00215b00214d40f5d63777683c7638f6
Gorm Paulsen <gormap@usit.uio.no>
2007-02-27 14:51:30 +0000

    Add capability to parse template component syntax in HtmlTextNodes (in user documents)

--- src/main/java/org/vortikal/web/view/decorating/ssi/SsiNodeFilter.java
+++ src/main/java/org/vortikal/web/view/decorating/ssi/SsiNodeFilter.java
@@ -37,11 +37,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.InitializingBean;
+
 import org.vortikal.web.RequestContext;
+import org.vortikal.web.view.decorating.ComponentInvocation;
 import org.vortikal.web.view.decorating.DecoratorComponent;
 import org.vortikal.web.view.decorating.DecoratorRequest;
 import org.vortikal.web.view.decorating.DecoratorRequestImpl;
 import org.vortikal.web.view.decorating.DecoratorResponseImpl;
+import org.vortikal.web.view.decorating.TemplateParser;
+import org.vortikal.web.view.decorating.TemplateSource;
 import org.vortikal.web.view.decorating.html.HtmlComment;
 import org.vortikal.web.view.decorating.html.HtmlContent;
 import org.vortikal.web.view.decorating.html.HtmlElement;
@@ -50,7 +56,7 @@ import org.vortikal.web.view.decorating.html.HtmlPage;
 import org.vortikal.web.view.decorating.html.HtmlText;
 
 
-public class SsiNodeFilter implements HtmlNodeFilter {
+public class SsiNodeFilter implements HtmlNodeFilter, InitializingBean {
     
     private static final Pattern SSI_DIRECTIVE_REGEXP = Pattern.compile(
             "#([a-zA-Z]+)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
@@ -61,51 +67,97 @@ public class SsiNodeFilter implements HtmlNodeFilter {
 
     private Map directiveComponentMap;
     
+    private TemplateParser contentComponentParser;
+    
 
     public void setDirectiveComponentMap(Map directiveComponentMap) {
         this.directiveComponentMap = directiveComponentMap;
     }
     
+    public void setContentComponentParser(TemplateParser contentComponentParser) {
+        this.contentComponentParser = contentComponentParser;
+    }
+    
+    public void afterPropertiesSet() {
+        if (this.directiveComponentMap == null) {
+            throw new BeanInitializationException(
+                "JavaBean property 'directiveComponentMap' not specified");
+        }
+    }
+    
 
     public HtmlContent filterNode(HtmlContent node) {
+
         if (node instanceof HtmlComment) {
-            String content = node.getContent();
-            if (content == null || "".equals(content.trim())) {
+            ComponentInvocation ssiInvocation = buildSsiComponentInvocation(node);
+            if (ssiInvocation == null) {
                 return node;
             }
-            
-            String directive = null;
-            Matcher directiveMatcher = SSI_DIRECTIVE_REGEXP.matcher(content);
-            if (!directiveMatcher.find()) {
-                return node;
-            }
-            directive = directiveMatcher.group(1);
-            
-            Matcher paramMatcher = SSI_PARAMETERS_REGEXP.matcher(
-                content.substring(directiveMatcher.end()));
-
-            Map parameters = new HashMap();
-
-            while (paramMatcher.find()) {
-                String name = paramMatcher.group(1);
-                String value = paramMatcher.group(2);
-                if (name != null && value != null) {
-                    parameters.put(name, value);
-                }
-            }
-            HtmlContent filteredNode = invokeComponent(node, directive, parameters);
+            HtmlContent filteredNode = invokeComponents(new ComponentInvocation[] {ssiInvocation});
             return filteredNode;
+
+        } else if (node instanceof HtmlText) {
+            if (this.contentComponentParser == null) {
+                return node;
+            }
+            String content = ((HtmlText) node).getContent();
+            try {
+                ComponentInvocation[] parsedContent =
+                    this.contentComponentParser.parseTemplate(new java.io.StringReader(content));
+                HtmlContent filteredNode = invokeComponents(parsedContent);
+                return filteredNode;
+            } catch (Exception e) {
+                return node;
+            }
         }
         return node;
     }
+
     
-    private HtmlContent invokeComponent(HtmlContent node, String directive, Map parameters) {
-        DecoratorComponent component = (DecoratorComponent)
-            this.directiveComponentMap.get(directive);
-        if (component == null) {
-            return node;
+    private ComponentInvocation buildSsiComponentInvocation(HtmlContent node) {
+        String content = node.getContent();
+        if (content == null || "".equals(content.trim())) {
+            return null;
+        }
+            
+        String directive = null;
+        Matcher directiveMatcher = SSI_DIRECTIVE_REGEXP.matcher(content);
+        if (!directiveMatcher.find()) {
+            return null;
+        }
+        directive = directiveMatcher.group(1);
+            
+        Matcher paramMatcher = SSI_PARAMETERS_REGEXP.matcher(
+            content.substring(directiveMatcher.end()));
+
+        Map parameters = new HashMap();
+
+        while (paramMatcher.find()) {
+            String name = paramMatcher.group(1);
+            String value = paramMatcher.group(2);
+            if (name != null && value != null) {
+                parameters.put(name, value);
+            }
         }
 
+        final DecoratorComponent component = (DecoratorComponent)
+            this.directiveComponentMap.get(directive);
+        if (component == null) {
+            return null;
+        }
+        final Map invocationParams = parameters;
+        return new ComponentInvocation() {
+            public Map getParameters() {
+                return invocationParams;
+            }
+            public DecoratorComponent getComponent() {
+                return component;
+            }
+        };
+    }
+    
+
+    private HtmlContent invokeComponents(ComponentInvocation[] components) {
         HttpServletRequest servletRequest = RequestContext.getRequestContext().getServletRequest();
         
         org.springframework.web.servlet.support.RequestContext ctx =
@@ -114,19 +166,23 @@ public class SsiNodeFilter implements HtmlNodeFilter {
 
         final String doctype = "";
         
-        DecoratorRequest decoratorRequest = new DecoratorRequestImpl(
-            new HashMap(), null, servletRequest, parameters, doctype, locale);
-
-        DecoratorResponseImpl response = new DecoratorResponseImpl(doctype, locale, "utf-8");
-        
-        String result = null;
-        try {
-            component.render(decoratorRequest, response);
-            result = response.getContentAsString();
-        } catch (Throwable t) {
-            result = "SSI: " + t.getMessage();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < components.length; i++) {
+            Map parameters = components[i].getParameters();
+            DecoratorRequest decoratorRequest = new DecoratorRequestImpl(
+                new HashMap(), null, servletRequest, parameters, doctype, locale);
+            DecoratorResponseImpl response = new DecoratorResponseImpl(doctype, locale, "utf-8");
+            String result = null;
+            try {
+                DecoratorComponent component = components[i].getComponent();
+                component.render(decoratorRequest, response);
+                result = response.getContentAsString();
+            } catch (Throwable t) {
+                result = "Error: " + t.getMessage();
+            }
+            sb.append(result);
         }
-        final String text = result;
+        final String text = sb.toString();
         return new HtmlText() {
             public String getContent() {
                 return text;

acaf2b913984c9c0aa04d290cd12bad68df07acf
Øyvind Stegard <oyviste@usit.uio.no>
2007-03-08 14:34:58 +0000

    Refactoring

--- src/test/java/org/vortikal/repositoryimpl/index/FieldValueEncoderTestCase.java
+++ src/test/java/org/vortikal/repositoryimpl/index/FieldValueEncoderTestCase.java
@@ -28,7 +28,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.vortikal.repositoryimpl.query;
+package org.vortikal.repositoryimpl.index;
 
 import java.util.Calendar;
 import java.util.Date;

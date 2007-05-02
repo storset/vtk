3567ddd729400fd7fa1d671d2e07e807a4c0d9e3
Gorm Paulsen <gormap@usit.uio.no>
2007-05-02 10:15:50 +0000

    Allow percentage ('%') in resource names

--- src/main/java/org/vortikal/repositoryimpl/URIValidator.java
+++ src/main/java/org/vortikal/repositoryimpl/URIValidator.java
@@ -45,15 +45,15 @@ public class URIValidator {
             return false;
         }
 
+        if (uri.length() >= MAX_URI_LENGTH) {
+            return false;
+        }
+
         if (!uri.startsWith("/")) {
             return false;
         }
 
         if (uri.indexOf("//") != -1) {
-            return false;
-        }
-
-        if (uri.length() >= MAX_URI_LENGTH) {
             return false;
         }
 
@@ -68,24 +68,6 @@ public class URIValidator {
         if (uri.endsWith("/.")) {
             return false;
         }
-
-        if (uri.indexOf("%") != -1) {
-            return false;
-        }
-
-        //         /* SQL Strings: */
-        //         if (uri.indexOf("'") != -1) {
-        //             return false;
-        //         }
-        //         /* SQL comments: */
-        //         if (uri.indexOf("--") != -1) {
-        //             return false;
-        //         }
-        //         /* SQL (C-style) comments: */
-        //         if (uri.indexOf("/*") != -1 || uri.indexOf("*/") != -1) {
-        //             return false;
-        //         }
-        
         
         // XXX: new?
         

e163a38e235ed7d236b1f9d94b271dca676f00cf
Gorm Paulsen <gormap@usit.uio.no>
2007-07-19 11:44:27 +0000

    Add cascading delete for comments table

--- src/main/sql/upgrades/0.3.10-oracle.sql
+++ src/main/sql/upgrades/0.3.10-oracle.sql
@@ -20,7 +20,7 @@ alter table vortex_comment
 
 alter table vortex_comment
       add constraint vortex_comment_fk foreign key (resource_id)
-          references vortex_resource (resource_id);
+          references vortex_resource (resource_id) on delete cascade;
 
 
 -- add 'add-comment' privilege

9606f46228bf9e99f748b3603c7d887b54af7569
Øyvind Stegard <oyviste@usit.uio.no>
2007-11-15 13:31:33 +0000

    fixup javadoc

--- src/main/java/org/vortikal/repository/resourcetype/TypeLocalizationProvider.java
+++ src/main/java/org/vortikal/repository/resourcetype/TypeLocalizationProvider.java
@@ -41,19 +41,24 @@ public interface TypeLocalizationProvider {
      * @param namespace
      * @param name
      * @param locale
-     * @return The name of the property- or resource-type as a localized string.
+     * @return The name of the property type as a localized string.
      *         If the type is unknown or a translation is missing for
-     *         the supplied locale, the canonical type name is
+     *         the supplied locale, the canonical type name is always
      *         returned.
      */
     public String getLocalizedPropertyName(PropertyTypeDefinition def,
                                            Locale locale);
     
     /**
+     * Return a localized name of the resource type.
      * 
      * @param def
      * @param locale
-     * @return
+     * 
+     * @return The name of the resource type as a localized string.
+     *         If the type is unknown or a translation is missing for
+     *         the supplied locale, the canonical type name is always
+     *         returned.
      */
     public String getLocalizedResourceTypeName(ResourceTypeDefinition def,
                                                Locale locale);

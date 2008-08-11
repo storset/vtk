9cb142ffe8bec50cc011f0c150c6a12eb9b2ff2b
Reza Mirzaei <rezam@usit.uio.no>
2008-08-11 13:07:20 +0000

    Forgot the pom (sorry) and comments on new classes.

--- src/main/java/org/vortikal/web/controller/CollectionListingAsAtomFeed.java
+++ src/main/java/org/vortikal/web/controller/CollectionListingAsAtomFeed.java
@@ -1,6 +1,35 @@
+/* Copyright (c) 2008, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.vortikal.web.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -42,10 +71,6 @@ public class CollectionListingAsAtomFeed implements Controller {
         String uri = RequestContext.getRequestContext().getResourceURI();
 
         Resource resource = this.repository.retrieve(token, uri, true);
-        
-        if (!resource.isCollection()) {
-            return null;
-        }
 
         String viewUrl = viewService.constructLink(uri);
         String host = viewService.constructURL(uri).getHost();
@@ -57,35 +82,33 @@ public class CollectionListingAsAtomFeed implements Controller {
         feed.addAuthor(resource.getModifiedBy().getDescription());
         feed.addLink(viewUrl, "self");
         
-        Map<String, Object> searchResult = new HashMap<String, Object>();
         for (SearchComponent searchComponent : searchComponents) {
-            searchResult.putAll(searchComponent.execute(request, resource));
-        }
-        
-        List<PropertySet> files = (List<PropertySet>) searchResult.get("files");
-        for (PropertySet child : files) {
-            Entry entry = feed.addEntry();
-            entry.setId(child.getURI());
-            entry.setTitle(child.getName());
-            entry.addCategory(child.getResourceType());
-            Property prop = child.getProperty(NS, PropertyType.TITLE_PROP_NAME);
-            entry.setTitle(prop.getFormattedValue("name", null));
+            Map<String, Object> searchResult = searchComponent.execute(request, resource);
+            List<PropertySet> files = (List<PropertySet>) searchResult.get("files");
+            for (PropertySet child : files) {
+                Entry entry = feed.addEntry();
+                entry.setId(child.getURI());
+                entry.setTitle(child.getName());
+                entry.addCategory(child.getResourceType());
+                Property prop = child.getProperty(NS, PropertyType.TITLE_PROP_NAME);
+                entry.setTitle(prop.getFormattedValue("name", null));
 
-            prop = child.getProperty(NS, "description");
-            if (prop != null) {
-                entry.setSummary(prop.getFormattedValue());
+                prop = child.getProperty(NS, "description");
+                if (prop != null) {
+                    entry.setSummary(prop.getFormattedValue());
+                }
+
+                prop = child.getProperty(NS, PropertyType.LASTMODIFIED_PROP_NAME);
+                entry.setUpdated(prop.getDateValue());
+
+                prop = child.getProperty(NS, PropertyType.CREATIONTIME_PROP_NAME);
+                entry.setPublished(prop.getDateValue());
+
+                prop = child.getProperty(NS, PropertyType.MODIFIEDBY_PROP_NAME);
+                entry.addAuthor(prop.getFormattedValue("name", null));
+
+                entry.addLink(viewService.constructLink(child.getURI()));
             }
-
-            prop = child.getProperty(NS, PropertyType.LASTMODIFIED_PROP_NAME);
-            entry.setUpdated(prop.getDateValue());
-
-            prop = child.getProperty(NS, PropertyType.CREATIONTIME_PROP_NAME);
-            entry.setPublished(prop.getDateValue());
-
-            prop = child.getProperty(NS, PropertyType.MODIFIEDBY_PROP_NAME);
-            entry.addAuthor(prop.getFormattedValue("name", null));
-
-            entry.addLink(viewService.constructLink(child.getURI()));
         }
 
         response.setContentType("application/atomxml;charset=utf-8");

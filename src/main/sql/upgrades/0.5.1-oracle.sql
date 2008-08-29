95eef90f145cb2ede5c83f73fc56787965b5338c
Gorm Paulsen <gormap@usit.uio.no>
2008-08-29 13:23:59 +0000

    SQL fixes

--- src/main/sql/upgrades/0.5.1-oracle.sql
+++ src/main/sql/upgrades/0.5.1-oracle.sql
@@ -1,13 +1,13 @@
 -- Insert property hasBodyContent = true for all HTML resources:
 
 insert into extra_prop_entry 
-select nextval('extra_prop_entry_seq_pk'),
+select extra_prop_entry_seq_pk.nextval,
        resource_id,
        4,
        null,
-       hasBodyContent,
+       'hasBodyContent',
        'true'
 from vortex_resource
-where resource_type in ('html', 'xhtml10trans', 'article', 'event')
+where resource_type in ('html', 'xhtml10trans', 'article', 'event');
 
 commit;

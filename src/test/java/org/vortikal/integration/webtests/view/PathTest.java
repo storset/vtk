e24b66a2e00cc47a7dc753ed27e46ec061ba8624
Reza Mirzaei <rezam@usit.uio.no>
2008-09-29 15:58:34 +0000

    VRTXA-37 - Tests for visual profile removed, should not be part of Vortikal (duh...). Resourcebundle updated.

--- src/test/java/org/vortikal/integration/webtests/view/PathTest.java
+++ src/test/java/org/vortikal/integration/webtests/view/PathTest.java
@@ -3,19 +3,7 @@ package org.vortikal.integration.webtests.view;
 import org.vortikal.integration.webtests.BaseWebTest;
 
 public class PathTest extends BaseWebTest {
-    
-    public void testVisualProfileOff() { 
-    	clickLink("visualprofileoff"); 
-    	assertElementPresent("breadcrumb");
-    	assertLinkPresent("vrtx-manage-url");
-    }
 
-    public void testVisualProfileOn() {
-     	clickLink("visualprofileon"); 
-    	assertElementPresent("breadcrumb");
-    	assertLinkPresent("vrtx-admin-link");
-    }
-    
     public void testFeedElement() {
     	assertLinkPresent("vrtx-feed-link");
     }

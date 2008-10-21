dc6274fbfd6195c3904499656acd35058e76c8f5
Jo Størset <storset@usit.uio.no>
2008-10-21 20:23:42 +0000

    Added exclusion of articles not past publised date (VTK-1148)

---
+++ src/main/java/org/vortikal/web/search/ParsedQueryBuilder.java
@@ -0,0 +1,30 @@
+package org.vortikal.web.search;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.vortikal.repository.Resource;
+import org.vortikal.repository.search.QueryParser;
+import org.vortikal.repository.search.query.Query;
+
+public class ParsedQueryBuilder implements QueryBuilder {
+
+    private String queryString;
+    private QueryParser queryParser;
+    
+    public Query build(Resource base, HttpServletRequest request) {
+        return this.queryParser.parse(this.queryString);
+    }
+
+    @Required
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
+    }
+
+    @Required
+    public void setQueryParser(QueryParser queryParser) {
+        this.queryParser = queryParser;
+    }
+
+
+}

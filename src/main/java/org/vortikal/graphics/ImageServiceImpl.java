3598c6dd6b8752ba7a450fcf994ee2a655eecb4c
Reza Mirzaei <rezam@usit.uio.no>
2008-12-11 21:20:32 +0000

    backport to last good release

--- src/main/java/org/vortikal/graphics/ImageServiceImpl.java
+++ src/main/java/org/vortikal/graphics/ImageServiceImpl.java
@@ -32,7 +32,7 @@ package org.vortikal.graphics;
 
 import java.awt.Dimension;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.Image;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -47,30 +47,27 @@ import org.vortikal.repository.Repository;
 import org.vortikal.security.SecurityContext;
 
 public class ImageServiceImpl implements ImageService {
-
+        
     private Repository repository;
-
     
     public ScaledImage scaleImage(String path, String width, String height) throws IOException {
-        BufferedImage originalImage = getImage(path);
-        String format = path.substring(path.lastIndexOf(".") + 1);
+    	BufferedImage originalImage = getImage(path);
+    	String format = path.substring(path.lastIndexOf(".") + 1);
         return scaleImage(originalImage, format, width, height);
     }
 
-
     public ScaledImage scaleImage(BufferedImage image, String format, String width, String height) throws IOException {
         Dimension scaleDimension = getDimension(width, height, image);
-        BufferedImage scaledImage = getScaledInstance(image, scaleDimension.width, scaleDimension.height);
+        BufferedImage scaledImage = scaleImage(image, scaleDimension.width, scaleDimension.height);
         return new ScaledImage(scaledImage, format);
     }
-
-
+    
     private BufferedImage getImage(String path) throws IOException {
-
+        
         BufferedImage originalImage = null;
-
+        
         if (isRepoImage(path)) {
-            String token = SecurityContext.getSecurityContext().getToken();
+        	String token = SecurityContext.getSecurityContext().getToken();
             InputStream imageStream = repository.getInputStream(token, Path.fromString(path), true);
             originalImage = ImageIO.read(imageStream);
         } else if (isWebImage(path)) {
@@ -78,25 +75,22 @@ public class ImageServiceImpl implements ImageService {
         } else {
             throw new IOException("Can not fetch requested image, uknown pathformat: '" + path + "'");
         }
-
+        
         return originalImage;
     }
-
-
+    
     private boolean isRepoImage(String path) {
         return path.matches("^(/)\\S*\\.(?i:png|gif|jp(e?)g)$");
     }
-
-
+    
     private boolean isWebImage(String path) {
         return path.matches("^(http(s?)\\:\\/\\/|www)\\S*\\.(?i:png|gif|jp(e?)g)$");
     }
 
-
     private Dimension getDimension(String width, String height, BufferedImage originalImage) {
-
+        
         // Width has precedence over height
-
+        
         if (StringUtils.isNotBlank(width)) {
             // If width is defined, use it as base for calculating
             // new dimensions (ignore height)
@@ -111,50 +105,23 @@ public class ImageServiceImpl implements ImageService {
         }
         // Both parameters were blank, return original dimensions
         return new Dimension(originalImage.getWidth(), originalImage.getHeight());
-
-    }
-
-
-    // Algorithm courtesy of Chris Campbell from the Java2D team at sun
-    public BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight) {
-        
-        int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB
-                : BufferedImage.TYPE_INT_ARGB;
-        BufferedImage ret = (BufferedImage) img;
-        int w = img.getWidth();
-        int h = img.getHeight();
-
-        do {
-            if (w > targetWidth) {
-                w /= 2;
-                if (w < targetWidth) {
-                    w = targetWidth;
-                }
-            }
-
-            if (h > targetHeight) {
-                h /= 2;
-                if (h < targetHeight) {
-                    h = targetHeight;
-                }
-            }
-
-            BufferedImage tmp = new BufferedImage(w, h, type);
-            Graphics2D g2 = tmp.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g2.drawImage(ret, 0, 0, w, h, null);
-            g2.dispose();
-
-            ret = tmp;
-        } while (w != targetWidth || h != targetHeight);
-
-        return ret;
         
     }
     
+    private BufferedImage scaleImage(BufferedImage image, int width, int height) {
 
+        int type = (image.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage newScaledImage = new BufferedImage(width, height, type);
+        Graphics2D g = newScaledImage.createGraphics();
+        g.drawImage(scaledImage, 0, 0, width, height, null);
+        g.dispose();
+
+        return newScaledImage;
+    }
+    
     public void setRepository(Repository repository) {
-        this.repository = repository;
+    	this.repository = repository;
     }
 
 }

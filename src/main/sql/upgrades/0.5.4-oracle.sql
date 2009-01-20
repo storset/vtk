a57fc5511913516543ff29480f18e7074c455f6b
Reza Mirzaei <rezam@usit.uio.no>
2009-01-20 10:36:04 +0000

    VTK-1241 - SQL added

---
+++ src/main/sql/upgrades/0.5.4-oracle.sql
@@ -0,0 +1,17 @@
+-- Use property 'description' as 'introduction' for collections that have no 'introduction', but have 'description'
+
+insert into extra_prop_entry
+  select extra_prop_entry_seq_pk.nextval,
+    e.resource_id,
+    0,
+    null,
+    'introduction',
+    e.value,
+    null,
+    null
+  from extra_prop_entry e
+    where e.name = 'description' and e.resource_id in (select resource_id from vortex_resource 
+      where resource_id not in (select distinct(resource_id) from extra_prop_entry where name = 'introduction')
+      and (resource_type = 'collection' or resource_type = 'article-listing' or resource_type = 'event-listing'));
+
+commit;

d240b87ba5423bf05c4622e4008d9c68eae2f4bd
Øyvind Stegard <oyviste@usit.uio.no>
2009-02-03 21:24:07 +0000

    A stupid bean which does exactly what its name implies. When configured, it will synchronously start a re-indexing of the configured index during bean initialization. If re-indexing fails, then bean initialization fails.

---
+++ src/main/java/org/vortikal/repository/index/management/ReindexAtStartupBean.java
@@ -0,0 +1,77 @@
+/* Copyright (c) 2009, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.repository.index.management;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Required;
+import org.vortikal.repository.index.PropertySetIndex;
+import org.vortikal.repository.store.IndexDao;
+
+/**
+ * A stupid bean which starts a synchronous re-indexing of the configured
+ * property set index at bean initialization time. 
+ *
+ */
+public class ReindexAtStartupBean implements InitializingBean {
+    
+    private Log logger = LogFactory.getLog(ReindexAtStartupBean.class);
+    
+    private PropertySetIndex index;
+    private IndexDao indexDao;
+    
+    public void afterPropertiesSet() throws Exception {
+        IndexOperationManager manager = new IndexOperationManagerImpl(this.index, 
+                                                                      this.indexDao);
+        logger.info("Starting synchronous re-indexing of index with ID '" 
+                + this.index.getId() + "' using IndexOperationManager ..");
+        manager.reindex(false);
+        if (manager.getLastReindexingException() != null) {
+            throw new BeanInitializationException("Re-indexing failed", 
+                                          manager.getLastReindexingException());
+        }
+        logger.info("Re-indexing finished.");
+    }
+
+    @Required
+    public void setIndex(PropertySetIndex index) {
+        this.index = index;
+    }
+
+    @Required
+    public void setIndexDao(IndexDao indexDao) {
+        this.indexDao = indexDao;
+    }
+    
+
+}

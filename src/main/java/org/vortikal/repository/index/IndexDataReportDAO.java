2ca9769c8487944bd0c44c92527c2bd3856f7928
Øyvind Stegard <oyviste@usit.uio.no>
2009-02-12 12:49:54 +0000

    Remove unused import.

--- src/main/java/org/vortikal/repository/index/IndexDataReportDAO.java
+++ src/main/java/org/vortikal/repository/index/IndexDataReportDAO.java
@@ -35,7 +35,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 

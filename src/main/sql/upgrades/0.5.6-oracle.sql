c28ad6cd7763eae3edf45316985969c31de435db
Øyvind Stegard <oyviste@usit.uio.no>
2009-03-18 14:41:48 +0000

    More SQL upgrades for next release.

--- src/main/sql/upgrades/0.5.6-oracle.sql
+++ src/main/sql/upgrades/0.5.6-oracle.sql
@@ -1,8 +1,22 @@
--- Set ordering requirement and caching on changelog_entry_seq_pk sequence
+-- Set ordering requirement and caching on changelog_entry_seq_pk sequence:
 
 alter sequence changelog_entry_seq_pk cache 40 order;
-commit;
-exit;
+
+-- Create some new indexes necessary to avoid deadlock-situations:
+
+create index acl_entry_index2 on acl_entry(action_type_id);
+
+create index extra_prop_entry_index2 on extra_prop_entry(prop_type_id);
+
+create index vortex_comment_index1 on vortex_comment(resource_id);
+
+-- Remove cascading deletes on prop_type table:
+alter table extra_prop_entry drop constraint extra_prop_entry_FK_2;
+alter table extra_prop_entry
+     add constraint extra_prop_entry_FK_2 foreign key (prop_type_id)
+     references prop_type(prop_type_id)
+;
+
 
 -- Insert property 'visual-profile:disabled = true' for
 -- resources that do not have 'visual-profile:enabled = true':
@@ -39,3 +53,10 @@ and resource_id not in
 delete from extra_prop_entry
        where name_space = 'http://www.uio.no/visual-profile'
        and name = 'enabled';
+
+
+-- Commit and exit:
+commit;
+exit;
+
+-- END

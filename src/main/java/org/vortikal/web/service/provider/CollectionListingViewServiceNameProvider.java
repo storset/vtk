7bb2dd366af4a147fd11ba8ad5c285140f31b056
Reza Mirzaei <rezam@usit.uio.no>
2009-04-03 11:32:40 +0000

    VTK-1342 - Fixed

---
+++ src/main/java/org/vortikal/web/service/provider/CollectionListingViewServiceNameProvider.java
@@ -0,0 +1,38 @@
+package org.vortikal.web.service.provider;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.support.RequestContext;
+import org.vortikal.repository.Path;
+import org.vortikal.repository.Resource;
+import org.vortikal.web.controller.AbstractCollectionListingController;
+
+public class CollectionListingViewServiceNameProvider implements ServiceNameProvider {
+
+    public String getLocalizedName(Resource resource, HttpServletRequest request) {
+
+        String page = request.getParameter(AbstractCollectionListingController.USER_DISPLAY_PAGE);
+        if (StringUtils.isBlank(page)) {
+            return null;
+        }
+        try {
+            int p = Integer.parseInt(page);
+            if (p < 2) {
+                return null;
+            }
+        } catch (NumberFormatException nfe) {
+            return null;
+        }
+
+        Path uri = resource.getURI();
+        RequestContext rc = new RequestContext(request);
+
+        if (Path.ROOT.equals(uri)) {
+            return rc.getMessage("viewCollectionListing.serviceTitle.root", new Object[] { page });
+        }
+
+        return rc.getMessage("viewCollectionListing.serviceTitle", new Object[] { resource.getTitle(), page });
+    }
+
+}

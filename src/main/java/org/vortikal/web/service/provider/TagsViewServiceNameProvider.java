1defcf07ac1ac95dc55762f42c439c90c30b8b56
Reza Mirzaei <rezam@usit.uio.no>
2009-04-21 08:47:15 +0000

    VTK-1352 - Fixed

--- src/main/java/org/vortikal/web/service/provider/TagsViewServiceNameProvider.java
+++ src/main/java/org/vortikal/web/service/provider/TagsViewServiceNameProvider.java
@@ -1,7 +1,12 @@
 package org.vortikal.web.service.provider;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.httpclient.URIException;
+import org.apache.commons.httpclient.util.URIUtil;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.web.servlet.support.RequestContext;
 import org.vortikal.repository.Path;
@@ -18,6 +23,11 @@ public class TagsViewServiceNameProvider implements ServiceNameProvider {
 
         String tag = request.getParameter("tag");
         boolean noTagSpecified = StringUtils.isBlank(tag);
+        try {
+            tag = noTagSpecified ? tag : URLDecoder.decode(tag, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // Don't break the entire breadcrumb if uridecoding fails
+        }
         if (Path.ROOT.equals(uri)) {
             return rc.getMessage(noTagSpecified ? "tags.serviceTitle" : "tags.title",
                     new Object[] { repositoryId, tag });

fa8b2a9fd920c9b117b73834ec1c47250352a12d
Gorm Paulsen <gormap@usit.uio.no>
2009-06-16 15:24:56 +0000

    [VTK-1410] * Use proper URL API: avoid hard-coding ?vrtx=tags&... * Add license headers

--- src/main/java/org/vortikal/web/tags/RepositoryTagElementsDataProvider.java
+++ src/main/java/org/vortikal/web/tags/RepositoryTagElementsDataProvider.java
@@ -1,3 +1,33 @@
+/* Copyright (c) 2008, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.vortikal.web.tags;
 
 import java.util.ArrayList;
@@ -12,10 +42,13 @@ import org.vortikal.repository.reporting.PropertyValueFrequencyQueryResult;
 import org.vortikal.repository.resourcetype.Value;
 import org.vortikal.web.RequestContext;
 import org.vortikal.web.reporting.TagsReportingComponent;
+import org.vortikal.web.service.Service;
+import org.vortikal.web.service.URL;
 
 public class RepositoryTagElementsDataProvider {
 
     private TagsReportingComponent tagsReporter;
+    private Service tagService;
     
     private boolean servesRoot = true;
     
@@ -27,6 +60,11 @@ public class RepositoryTagElementsDataProvider {
     @Required // to be removed..
     public void setServesRoot(boolean servesRoot) {
         this.servesRoot = servesRoot;
+    }
+
+    @Required
+    public void setTagService(Service tagService) {
+        this.tagService = tagService;
     }
 
     public List<TagElement> getTagElements(Path scopeUri, String token,
@@ -58,7 +96,7 @@ public class RepositoryTagElementsDataProvider {
 
             for (Pair<Value, Integer> pair : freqList) {
                 String tagName = pair.first().getStringValue();
-                String link = getUrl(tagName, scopeUri);
+                URL link = getUrl(tagName, scopeUri);
 
                 int magnitude = getNormalizedMagnitude(pair.second().intValue(), maxFreq, minFreq, magnitudeMin,
                         magnitudeMax);
@@ -87,16 +125,14 @@ public class RepositoryTagElementsDataProvider {
     }
 
 
-    private String getUrl(String tagName, Path scopeUri) {
-        if (scopeUri.isRoot()) {
-            if (!this.servesRoot) {
-                return RequestContext.getRequestContext().getCurrentCollection() + "/?vrtx=tags&tag=" + tagName + "&scope=" + Path.ROOT;
-            } 
-            return "/?vrtx=tags&tag=" + tagName;
+    private URL getUrl(String tagName, Path scopeUri) {
+        URL url = this.tagService.constructURL(scopeUri);
+        if (scopeUri.isRoot() && !this.servesRoot) {
+            scopeUri = RequestContext.getRequestContext().getCurrentCollection();
+            url = this.tagService.constructURL(scopeUri);
+            url.addParameter("scope", Path.ROOT.toString());
         }
-        
-        return scopeUri + "/?vrtx=tags&tag=" + tagName; 
+        url.addParameter("tag", tagName);
+        return url;
     }
-
-
 }

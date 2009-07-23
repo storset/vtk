c8aae256f1f23e686edd9f8309faa3d34249423b
Gorm Paulsen <gormap@usit.uio.no>
2009-07-23 11:51:31 +0000

    [VTK-1397] Implement decorator component that renders a component definitions written in the "simple (org.vortikal.text.tl) template language".

---
+++ src/main/java/org/vortikal/resourcemanagement/TemplateLanguageDecoratorComponent.java
@@ -0,0 +1,110 @@
+/* Copyright (c) 2009, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.resourcemanagement;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.vortikal.text.html.HtmlContent;
+import org.vortikal.text.html.HtmlFragment;
+import org.vortikal.text.html.HtmlPageParser;
+import org.vortikal.text.tl.Context;
+import org.vortikal.text.tl.DirectiveNodeFactory;
+import org.vortikal.text.tl.NodeList;
+import org.vortikal.text.tl.ParseResult;
+import org.vortikal.text.tl.Parser;
+import org.vortikal.web.decorating.DecoratorRequest;
+import org.vortikal.web.decorating.DecoratorResponse;
+import org.vortikal.web.decorating.HtmlDecoratorComponent;
+import org.vortikal.web.decorating.components.AbstractDecoratorComponent;
+
+public class TemplateLanguageDecoratorComponent extends AbstractDecoratorComponent
+        implements HtmlDecoratorComponent {
+
+    private ComponentDefinition definition;
+    private String modelKey;
+    private NodeList nodeList;
+    private HtmlPageParser htmlParser;
+    
+    public TemplateLanguageDecoratorComponent(ComponentDefinition definition, 
+            String modelKey, Map<String, DirectiveNodeFactory> directiveHandlers, 
+            HtmlPageParser htmlParser) throws Exception {
+        this.definition = definition;
+        this.modelKey = modelKey;
+        Parser parser = new Parser(new StringReader(definition.getDefinition()), directiveHandlers);
+        ParseResult result = parser.parse(new HashSet<String>());
+        this.nodeList = result.getNodeList();
+        this.htmlParser = htmlParser;
+    }
+    
+    public List<HtmlContent> render(DecoratorRequest request) throws Exception {
+        Context ctx = new Context(request.getLocale());
+        if (this.modelKey != null) { 
+            ctx.define(this.modelKey, request.getMvcModel(), true);
+        }
+        StringWriter writer = new StringWriter();
+        this.nodeList.render(ctx, writer);
+        HtmlFragment fragment = this.htmlParser.parseFragment(writer.getBuffer().toString());
+        return fragment.getContent();
+    }
+
+    public void render(DecoratorRequest request, DecoratorResponse response)
+            throws Exception {
+        Context ctx = new Context(request.getLocale());
+        if (this.modelKey != null) { 
+            ctx.define(this.modelKey, request.getMvcModel(), true);
+        }
+        this.nodeList.render(ctx, response.getWriter());
+    }
+
+    @Override
+    protected String getDescriptionInternal() {
+        return null;
+    }
+
+    @Override
+    protected Map<String, String> getParameterDescriptionsInternal() {
+        return null;
+    }
+    
+    @Override
+    public String getName() {
+        return this.definition.getName();
+    }
+    
+    @Override
+    public String getNamespace() {
+        return "#namespace#";
+    }
+}

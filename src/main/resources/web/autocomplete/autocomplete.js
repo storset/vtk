aeb47bb70531e55a09b33b868f794f0df04e1779
Reza Mirzaei <rezam@usit.uio.no>
2009-08-05 13:25:12 +0000

    Comment

--- src/main/resources/web/autocomplete/autocomplete.js
+++ src/main/resources/web/autocomplete/autocomplete.js
@@ -2,6 +2,12 @@
  * Autocomplete
  */
 
+/**
+ * @id Unique id of the field to hook up autocomplete to
+ * @service Name of service to invoke for autocomplete suggestions
+ * @params List of parameters to override/extend default behavior. Must be
+ *         supplied as a name:value array, e.g. {minChars:1, selectFirst:false}
+ */
 function setAutoComplete(id, service, params) {
   // Default parameters, extended by 'params' if supplied
   var p = {

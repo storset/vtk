1367005c0687bb07617a48e448cb3ed087be0de1
Øyvind Hatland <oyvihatl@usit.uio.no>
2009-10-07 16:30:46 +0000

    VTK-1489 Frida - differantiate publications in backend (FridaPublicationsProvider) into books, sci.articles / book chapters and other works and put them in separate tabs in publications.ftl. first version formatting each publication into <li>-elements as on sv-faculty

--- src/main/ftl/layouts/publications.ftl
+++ src/main/ftl/layouts/publications.ftl
@@ -5,13 +5,75 @@
   - Description: List publications from Frida
   -
   -->
+  
+<#assign counter = 1>
+<#assign t1 = 0><#assign t2 = 0><#assign t3 = 0><#assign t4 = 0>
 
-<#if publications?exists && publications?size &gt; 0>
-  <div class="vrtx-frida-publications">
-  <#list publications as publication>
-    <div class="vrtx-frida-publication">
-      <p><b>${publication.publicationTitle}</b>, ${publication.researchers} (${publication.id})</p>
-    </div>
-  </#list>
+<#if selectedPublications?exists && selectedPublications != "">
+   <#assign t1 = counter>
+   <#assign counter = counter +1>
+</#if>
+<#if pBooks?exists && pBooks?size &gt; 0>
+   <#assign t2 = counter>
+   <#assign counter = counter +1>
+</#if>
+<#if pSciArtBookChapters?exists && pSciArtBookChapters?size &gt; 0>
+   <#assign t3 = counter>
+   <#assign counter = counter +1>
+</#if>
+<#if pOther?exists && pOther?size &gt; 0>
+   <#assign t4 = counter>
+</#if>
+
+
+
+<ul>
+    <#if t1 != 0>
+      <li><a href="#tabs-${t1}">Utvalgte</a></li>
+    </#if>
+    <#if t2 != 0>
+      <li><a href="#tabs-${t2}">Bøker</a></li>
+    </#if>
+    <#if t3 != 0>
+      <li><a href="#tabs-${t3}">Vitenskapelige artikler og bokkapitler</a></li>
+    </#if>
+    <#if t4 != 0>
+      <li><a href="#tabs-${t4}">Andre arbeider</a></li>
+    </#if>
+</ul> 
+
+<#if t1 != 0>
+  <div id="tabs-${t1}">
+    ${selectedPublications}
+  </div>
+</#if>
+
+<#if t2 != 0>
+  <div id="tabs-${t2}">
+    <ul class="vrtx-frida-publication">
+      <#list pBooks as publication>
+        <li>${publication.researchers}&nbsp;(${publication.year}). ${publication.title}</li>
+      </#list>
+    </ul>
+  </div>
+</#if>
+
+<#if t3 != 0>
+  <div id="tabs-${t3}">
+    <ul class="vrtx-frida-publication">
+      <#list pSciArtBookChapters as publication>
+        <li>${publication.researchers}&nbsp;(${publication.year}). ${publication.title}</li>
+      </#list>
+    </ul>
+  </div>
+</#if>
+
+<#if t4 != 0>
+  <div id="tabs-${t4}">
+    <ul class="vrtx-frida-publication">
+      <#list pOther as publication>
+        <li>${publication.researchers}&nbsp;(${publication.year}). ${publication.title}</li>
+      </#list>
+    </ul>
   </div>
 </#if>
--- src/main/java/org/vortikal/web/referencedata/provider/FridaPublicationsProvider.java
+++ src/main/java/org/vortikal/web/referencedata/provider/FridaPublicationsProvider.java
@@ -3,11 +3,13 @@ package org.vortikal.web.referencedata.provider;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.vortikal.repository.ContentStream;
 import org.vortikal.repository.Namespace;
@@ -22,20 +24,27 @@ public class FridaPublicationsProvider implements ReferenceDataProvider {
 
     private Repository repository = null;
 
+
     public void setRepository(Repository repository) {
         this.repository = repository;
     }
 
+
     @SuppressWarnings("unchecked")
     public void referenceData(Map model, HttpServletRequest request) throws Exception {
-        
+
         SecurityContext securityContext = SecurityContext.getSecurityContext();
         RequestContext requestContext = RequestContext.getRequestContext();
 
-        Resource resource = this.repository.retrieve(securityContext.getToken(), requestContext.getResourceURI(), false);
+        Resource resource = this.repository
+                .retrieve(securityContext.getToken(), requestContext.getResourceURI(), false);
+
         Property prop = (Property) resource.getProperty(Namespace.STRUCTURED_RESOURCE_NAMESPACE,
                 "externalScientificInformation");
-        
+
+        Property prop2 = (Property) resource.getProperty(Namespace.STRUCTURED_RESOURCE_NAMESPACE,
+                "selectedPublications");
+
         if (prop != null) {
             ContentStream cs = prop.getBinaryStream();
             InputStream is = cs.getStream();
@@ -51,9 +60,41 @@ public class FridaPublicationsProvider implements ReferenceDataProvider {
             br.close();
             isr.close();
             is.close();
-            
+
             JSONArray publications = JSONArray.fromObject(sb.toString());
-            model.put("publications", publications);
+
+            Iterator it = publications.listIterator();
+
+            // Differantiate
+            JSONArray pBooks = new JSONArray();
+            JSONArray pSciArtBookChapters = new JSONArray();
+            JSONArray pOther = new JSONArray();
+
+            while (it.hasNext()) {
+                JSONObject publication = (JSONObject) it.next();
+                String mainCategory = (String) publication.get("mainCategoryCode");
+                String subCategory = (String) publication.get("subCategoryNO");
+
+                if (mainCategory.equals("BOK")) {
+                    pSciArtBookChapters.add(publication);
+                } else if (subCategory.equals("Vitenskapelig artikkel")
+                        || subCategory.equals("Populærvitenskapelig artikkel") || mainCategory.equals("BOKRAPPORTDEL")) {
+                    pBooks.add(publication);
+                } else {
+                    pOther.add(publication);
+                }
+            }
+
+            model.put("pBooks", pBooks);
+            model.put("pSciArtBookChapters", pSciArtBookChapters);
+            model.put("pOther", pOther);
+
+        }
+
+        if (prop2 != null) {
+            model.put("selectedPublications", "here comes the selectedPublications");
+        } else {
+            model.put("selectedPublications", null);
         }
 
     }

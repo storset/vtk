3f3e4e687eeff23ef22dd398057460671f10bfc7
Reza Mirzaei <rezam@usit.uio.no>
2009-10-07 19:48:33 +0000

    Review. Remove warnings & unused code, format and iterate list '1.5+' style

--- src/main/java/org/vortikal/web/decorating/components/BreadcrumbMenuComponent.java
+++ src/main/java/org/vortikal/web/decorating/components/BreadcrumbMenuComponent.java
@@ -20,13 +20,9 @@ import org.vortikal.web.referencedata.provider.BreadcrumbElement;
 import org.vortikal.web.service.Service;
 
 public class BreadcrumbMenuComponent extends ViewRenderingDecoratorComponent {
-    private static final String PARAMETER_AUTENTICATED = "authenticated";
-    private static final String PARAMETER_AUTENTICATED_DESC = "The default is that only resources readable for everyone is listed. "
-            + "If this is set to 'true', the listing is done as the currently "
-            + "logged in user (if any).";
 
+    private static final String PARAMETER_AUTENTICATED = "authenticated";
     private static final String PARAMETER_DISPLAY_FROM_LEVEL = "display-from-level";
-    private static final String PARAMETER_DISPLAY_FROM_LEVEL_DESC = "Defines the starting URI level for the menu";
 
     private Repository repository;
     private Service service;
@@ -35,12 +31,13 @@ public class BreadcrumbMenuComponent extends ViewRenderingDecoratorComponent {
     private PropertyTypeDefinition navigationTitlePropDef;
     private PropertyTypeDefinition titlePropDef;
 
-    public void processModel(Map<Object, Object> model,DecoratorRequest request, DecoratorResponse response) throws Exception {
+    public void processModel(Map<Object, Object> model, DecoratorRequest request, DecoratorResponse response)
+            throws Exception {
         initRequestParameters(request);
         Path uri = RequestContext.getRequestContext().getResourceURI();
         Principal principal = SecurityContext.getSecurityContext().getPrincipal();
         boolean isIndexFile = RequestContext.getRequestContext().isIndexFile();
-        
+
         List<BreadcrumbElement> breadCrumbElements = getBreadcrumbElements();
         if (displayFromLevel > breadCrumbElements.size()) {
             return;
@@ -52,26 +49,28 @@ public class BreadcrumbMenuComponent extends ViewRenderingDecoratorComponent {
         Resource currentResource = repository.retrieve(token, uri, false);
         if (!currentResource.isCollection()) {
             currentResource = repository.retrieve(token, uri.getParent(), false);
-            if (!isIndexFile) { // Files that are not index files shall not be displayed in the menu.
+            if (!isIndexFile) { // Files that are not index files shall not be
+                                // displayed in the menu.
                 breadCrumbElements.remove(breadCrumbElements.size() - 1);
             }
         }
-        String markedUrl = this.service.constructLink(currentResource,principal, false);
+        String markedUrl = this.service.constructLink(currentResource, principal, false);
 
-        Map<String, String> childElements = generateChildElements( currentResource.getChildURIs(), principal);        
-        // If there is no children of the current resource, then we shall instead display the children of the parent node. 
-        if (childElements.size() == 0) { 
+        Map<String, String> childElements = generateChildElements(currentResource.getChildURIs(), principal);
+        // If there is no children of the current resource, then we shall
+        // instead display the children of the parent node.
+        if (childElements.size() == 0) {
             Resource childResource = repository.retrieve(token, currentResource.getURI().getParent(), false);
             childElements = generateChildElements(childResource.getChildURIs(), principal);
-            breadCrumbElements.remove(breadCrumbElements.size() - 1); 
+            breadCrumbElements.remove(breadCrumbElements.size() - 1);
         }
         model.put("breadcrumb", breadCrumbElements);
         model.put("children", childElements);
         model.put("markedurl", markedUrl);
     }
-    
-    private List<BreadcrumbElement> getBreadcrumbElements() throws Exception{
-        String breadcrumbName = "breadcrumb"; 
+
+    private List<BreadcrumbElement> getBreadcrumbElements() throws Exception {
+        String breadcrumbName = "breadcrumb";
         BreadCrumbProvider p = new BreadCrumbProvider();
         p.setService(service);
         p.setRepository(repository);
@@ -81,20 +80,20 @@ public class BreadcrumbMenuComponent extends ViewRenderingDecoratorComponent {
         titleProp[0] = titlePropDef;
         p.setTitleOverrideProperties(titleProp);
         p.afterPropertiesSet();
-        Map<String,List<BreadcrumbElement>> map = new HashMap<String,List<BreadcrumbElement>>();
+        Map<String, List<BreadcrumbElement>> map = new HashMap<String, List<BreadcrumbElement>>();
         p.referenceData(map, RequestContext.getRequestContext().getServletRequest());
         return map.get(breadcrumbName);
     }
 
     private Map<String, String> generateChildElements(List<Path> children, Principal principal) throws Exception {
         Map<String, String> childElements = new LinkedHashMap<String, String>();
-        for (int i = 0; i < children.size(); i++) {
-            Resource childResource = repository.retrieve(token,children.get(i), false);
+        for (Path childPath : children) {
+            Resource childResource = repository.retrieve(token, childPath, false);
             if (!childResource.isCollection()) {
                 continue;
             }
-            String url = this.service.constructLink(childResource, principal,false);
-            childElements.put(url, getMenuTitle(childResource)); 
+            String url = this.service.constructLink(childResource, principal, false);
+            childElements.put(url, getMenuTitle(childResource));
         }
         return childElements;
     }
@@ -106,14 +105,13 @@ public class BreadcrumbMenuComponent extends ViewRenderingDecoratorComponent {
         }
         return resource.getTitle();
     }
-    
+
     private void initRequestParameters(DecoratorRequest request) {
         String displayFromLevel = request.getStringParameter(PARAMETER_DISPLAY_FROM_LEVEL);
         if (displayFromLevel != null && !"".equals(displayFromLevel.trim())) {
             int level = Integer.parseInt(displayFromLevel);
             if (level <= 0) {
-                throw new DecoratorComponentException("Parameter '"
-                        + PARAMETER_DISPLAY_FROM_LEVEL
+                throw new DecoratorComponentException("Parameter '" + PARAMETER_DISPLAY_FROM_LEVEL
                         + "' must be an integer > 0");
             }
             this.displayFromLevel = level;

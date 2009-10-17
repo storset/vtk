9df8c1160836fe821d85a262a4e3ccb0a9c0bded
Øyvind Hatland <oyvihatl@usit.uio.no>
2009-10-17 13:10:43 +0000

    VTK-1501 possible to add share on link to Twitter, LinkedIn and Digg. e.g. if we want to add Twitter we only need to add <bean class='org.vortikal.web.referencedata.provider.socialwebsites.Twitter' /> to decorator-components.xml->socialWebsitesProvider->socialWebsites. Occurance in XML equals occurance in 'Share at...'-box

---
+++ src/main/java/org/vortikal/web/referencedata/provider/socialwebsites/LinkedIn.java
@@ -0,0 +1,64 @@
+/* Copyright (c) 2009, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.web.referencedata.provider.socialwebsites;
+
+public class LinkedIn implements SocialWebsite {
+
+    private String link;
+    private String name;
+
+
+    public void generateLink(String url, String title, String description, String name) {
+        this.link = "http://www.linkedin.com/shareArticle?mini=true&amp;url=" + url + "&amp;title=" + title;
+        this.name = name;
+    }
+
+
+    public String getLink() {
+        return link;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

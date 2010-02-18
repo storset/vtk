6c306c415fad944fc44025468e3d69c32f85cd84
Reza Mirzaei <rezam@usit.uio.no>
2010-02-18 10:21:07 +0000

    VTK-1619 - Move js to separate file.

---
+++ src/main/resources/web/jquery/include-jquery.js
@@ -0,0 +1,7 @@
+
+if (typeof jQuery == 'undefined') {
+  var scriptNode = document.createElement("script");
+  document.getElementsByTagName("head")[0].appendChild(scriptNode);
+  scriptNode.setAttribute("type", "text/javascript", 0);
+  scriptNode.setAttribute("src", "/vrtx/__vrtx/static-resources/jquery/jquery-1.3.2.min.js", 0);
+}

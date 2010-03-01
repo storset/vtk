c1d75562342ce9be0ceb60c61fa1f5413fe24433
Gorm Paulsen <gormap@usit.uio.no>
2010-03-01 14:17:22 +0000

    Add license header, use singleton HTML parser.

--- src/main/java/org/vortikal/web/decorating/components/AbstractFeedComponent.java
+++ src/main/java/org/vortikal/web/decorating/components/AbstractFeedComponent.java
@@ -1,3 +1,33 @@
+/* Copyright (c) 2009, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.vortikal.web.decorating.components;
 
 import java.util.ArrayList;
@@ -25,6 +55,7 @@ public abstract class AbstractFeedComponent extends ViewRenderingDecoratorCompon
     protected static final String PARAMETER_FEED_ELEMENT_ORDER = "element-order";
     protected static final String PARAMETER_FEED_ELEMENT_ORDER_DESC = "The order that the elementes are listed";
 
+    private HtmlPageParser parser = new HtmlPageParserImpl();
     private HtmlPageFilter imgHtmlFilter;
     private HtmlPageFilter noImgHtmlFilter;
     private List<String> defaultElementOrder;
@@ -45,8 +76,7 @@ public abstract class AbstractFeedComponent extends ViewRenderingDecoratorCompon
             String htmlFragment = null;
             if (entry.getDescription() != null)
                 htmlFragment = entry.getDescription().getValue();
-            HtmlPageParser parser = new HtmlPageParserImpl();
-            HtmlFragment fragment = parser.parseFragment(htmlFragment);
+            HtmlFragment fragment = this.parser.parseFragment(htmlFragment);
             fragment.filter(filter);
             result.put(entry.toString(),fragment.getStringRepresentation());
         }

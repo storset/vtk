dd8ea0f7cba60b681f18063ca355c013a08720ca
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-05 13:45:41 +0000

    VTK-1628 new function 'addPagingClickEvent()' taking next or prev as parameter, and center thumbnail images in their containers

--- src/main/resources/web/js/image-listing-component.js
+++ src/main/resources/web/js/image-listing-component.js
@@ -34,63 +34,73 @@ $(window).bind("load", function () {
   $(container + "-nav-pure-css").addClass("vrtx-image-listing-include-container-nav");
   
   //paging (relative to li a.active)
-  $(wrapper + " " + " a.prev").click(function(g) {
-	if($(wrapper + " ul li a.active").parent().prev().length != 0) {
-	  $(wrapper + " ul li a.active").parent().prev().find("a").click();
-	} else {
-	  $(wrapper + " ul li:last a").click();   
-	}
-    g.preventDefault(); 
-  });
-	  
-  $(wrapper + " " + " a.next").click(function(h) {
-	if($(wrapper + " ul li a.active").parent().next().length != 0) {
-      $(wrapper + " ul li a.active").parent().next().find("a").click();
-	} else {
-	  $(wrapper + " ul li:first a").click();  
-	}
-	h.preventDefault(); 
-  });
+  addPagingClickEvent("next", wrapper);
+  addPagingClickEvent("prev", wrapper);  
+  
+  function addPagingClickEvent(navClass, wrapper) {
+    $(wrapper + " " + " a." + navClass).click(function(h) {
+      if(navClass == "next") {
+		  if($(wrapper + " ul li a.active").parent().next().length != 0) {
+		    $(wrapper + " ul li a.active").parent().next().find("a").click();
+		  } else {
+			$(wrapper + " ul li:first a").click();
+		  }
+      } else {
+    	  if($(wrapper + " ul li a.active").parent().prev().length != 0) {
+    		$(wrapper + " ul li a.active").parent().prev().find("a").click();
+          } else {
+    		$(wrapper + " ul li:last a").click();   
+          }
+      }
+	  h.preventDefault(); 
+	});  
+  }
 	  
   return this.each(function (i) {
-	       
-	       $(this).click(function(e) {
-	    	  
-	    	  //change image
-	    	  var img = new Image();
-	    	  var src = $("img", this).attr("src").split("?")[0]; img.src = src; img.alt = src;
-	    	  
-	          //change link
-	          link = document.createElement("a"); 
-	          link.setAttribute("href", $(this).attr("href"));
-	          link.setAttribute("class", "vrtx-image-listing-include-container-link");
-	          // IE
-	          link.setAttribute("className", "vrtx-image-listing-include-container-link");
-	          
-	      	  //replace link and image (w/ fade effect down to fadedOutOpacity) + stop() current animation.
-	          $(wrapper + " " + container).stop().fadeTo(settings.fadeInOutTime, settings.fadedOutOpacity, function() {
-	        	  //done fade out -> remove
-	        	  $("a.vrtx-image-listing-include-container-link", this).remove();
-	        	  //start fading in ...
-	        	  $(this).fadeTo(settings.fadeInOutTime, 1);
-	        	  //... before adding new image for smoother change
-	        	  $(this).prepend(link);
-	        	  $(link, this).append(img);
-	          });
-	          
-	          //remove active classes
-	          jQuery(wrapper + " ul li a").each(function(j) {
-	        	  jQuery(this).removeClass("active");
-		      });    
-	         
-	          //add new active class
-	      	  $(this).addClass("active");
-	      	  
-	          //prevent default event action
-	    	  e.preventDefault(); 
+	  
+	   //center thumbnails images
+	   var imgWidth = $("img", this).width();
+	   if(imgWidth > $(this).width()) {
+	      var leftAdjust = -(imgWidth - $(this).width()) / 2;
+		  $("img", this).css("marginLeft", leftAdjust + "px");
+	   }
+	   
+	   $(this).click(function(e) {
+		  
+		  //change image
+		  var img = new Image();
+		  var src = $("img", this).attr("src").split("?")[0]; img.src = src; img.alt = src;
+		  
+	      //change link
+	      link = document.createElement("a"); 
+	      link.setAttribute("href", $(this).attr("href"));
+	      link.setAttribute("class", "vrtx-image-listing-include-container-link");
+	      // IE
+	      link.setAttribute("className", "vrtx-image-listing-include-container-link");
+	      
+	  	  //replace link and image (w/ fade effect down to fadedOutOpacity) + stop() current animation.
+	      $(wrapper + " " + container).stop().fadeTo(settings.fadeInOutTime, settings.fadedOutOpacity, function() {
+	    	  //done fade out -> remove
+	    	  $("a.vrtx-image-listing-include-container-link", this).remove();
+	    	  //start fading in ...
+	    	  $(this).fadeTo(settings.fadeInOutTime, 1);
+	    	  //... before adding new image for smoother change
+	    	  $(this).prepend(link);
+	    	  $(link, this).append(img);  
 	      });
+	      
+	      //remove active classes
+	      jQuery(wrapper + " ul li a").each(function(j) {
+	    	  jQuery(this).removeClass("active");
+	      });    
+	     
+	      //add new active class
+	  	  $(this).addClass("active");
+	  	  
+	      //prevent default event action
+		  e.preventDefault(); 
+	  });
     });
   
   };
-        
 })(jQuery);

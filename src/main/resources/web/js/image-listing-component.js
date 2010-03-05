09d882c24ead46f58830e08f8bf374343199188f
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-05 10:58:39 +0000

    VTK-1628 now as jQuery plugin - vrtxSGallery(). Also use fadeTo() instead of animate() and stop() for stopping existing animation when fast user clicks. Also use .bind('load') for waiting for all images to be loaded before running and .bind() for noticable performance increase

--- src/main/resources/web/js/image-listing-component.js
+++ src/main/resources/web/js/image-listing-component.js
@@ -1,76 +1,93 @@
-// Vortex Simple Gallery v0.1b
-// w/ paging and fade effect
-// by UiO / USIT
+// Vortex Simple Gallery jQuery plugin v0.1b
+// w/ paging, fade effect
+// by Øyvind Hatland - UiO / USIT
 
-$(document).ready( function() {
+// ('load') so that all images is loaded before running
+// and .bind for performance increase: http://jqueryfordesigners.com/demo/fade-method2.html
+$(window).bind("load", function () {
 	
-  var wrapper = ".vrtx-image-listing-include";
+  var wrapper = ".vrtx-image-listing-include";	
   var container = ".vrtx-image-listing-include-container";
-  var fadeInOutTime = 125; //ms
-  var fadedOutOpacity = 0.5;
-  
+
   // Unobtrusive JavaScript
-  $(container + "-pure-css").addClass("vrtx-image-listing-include-container");
-  $(".vrtx-image-listing-include-container-nav-pure-css").addClass("vrtx-image-listing-include-container-nav");
+  $(container + "-pure-css").addClass("vrtx-image-listing-include-container");	
   
-  $(wrapper + " ul li a").each( function(i) {
-      $(this).click(function(e) {
-    	  
-    	  //change image
-    	  var img = new Image();
-    	  var src = $("img", this).attr("src").split("?")[0]; img.src = src; img.alt = src;
-    	  
-          //change link
-          link = document.createElement("a"); 
-          link.setAttribute("href", $(this).attr("href"));
-          link.setAttribute("class", "vrtx-image-listing-include-container-link");
-          // IE
-          link.setAttribute("className", "vrtx-image-listing-include-container-link");
-          
-      	  //replace link and image (w/ fade effect down to fadedOutOpacity)
-          $(wrapper + " " + container).animate({opacity: fadedOutOpacity}, fadeInOutTime, function() {
-        	  //done fade out -> remove
-        	  $("a.vrtx-image-listing-include-container-link", this).remove();
-        	  //start fading in ...
-        	  $(this).animate({opacity: 1.0}, fadeInOutTime);
-        	  //... before adding new image for smoother change
-        	  $(this).prepend(link);
-        	  $(link, this).append(img);
-          });
-          
-          //remove active classes
-          jQuery(wrapper + " ul li a").each(function(j) {
-        	  jQuery(this).removeClass("active");
-	      });    
-         
-          //add new active class
-      	  $(this).addClass("active");
-      	  
-          //prevent default event action
-    	  e.preventDefault(); 
-      });
-  });
-  
+  alert(container);
+  $(wrapper + " ul li a").vrtxSGallery();
+	
   //choose first image in <li>
   $(wrapper + " ul li:first a").click();
+	  
+});
+
+(function ($) {
+  $.fn.vrtxSGallery = function (options) {
+	  
+  var wrapper = ".vrtx-image-listing-include";
+  var container = ".vrtx-image-listing-include-container";
+  var fadeInOutTime = 250; //ms
+  var fadedOutOpacity = 0;
+	  
+  $(container + "-nav-pure-css").addClass("vrtx-image-listing-include-container-nav");
   
   //paging (relative to li a.active)
   $(wrapper + " " + " a.prev").click(function(g) {
-	  if($(wrapper + " ul li a.active").parent().prev().length != 0) {
-		 $(wrapper + " ul li a.active").parent().prev().find("a").click();
-	  } else {
-		 $(wrapper + " ul li:last a").click();   
-	  }
-	  g.preventDefault(); 
+	if($(wrapper + " ul li a.active").parent().prev().length != 0) {
+	  $(wrapper + " ul li a.active").parent().prev().find("a").click();
+	} else {
+	  $(wrapper + " ul li:last a").click();   
+	}
+    g.preventDefault(); 
   });
-  
+	  
   $(wrapper + " " + " a.next").click(function(h) {
-	  if($(wrapper + " ul li a.active").parent().next().length != 0) {
-		  $(wrapper + " ul li a.active").parent().next().find("a").click();
-	  } else {
-		  $(wrapper + " ul li:first a").click();  
-	  }
-	  h.preventDefault(); 
+	if($(wrapper + " ul li a.active").parent().next().length != 0) {
+      $(wrapper + " ul li a.active").parent().next().find("a").click();
+	} else {
+	  $(wrapper + " ul li:first a").click();  
+	}
+	h.preventDefault(); 
   });
+	  
+  return this.each(function (i) {
+	       
+	       $(this).click(function(e) {
+	    	  
+	    	  //change image
+	    	  var img = new Image();
+	    	  var src = $("img", this).attr("src").split("?")[0]; img.src = src; img.alt = src;
+	    	  
+	          //change link
+	          link = document.createElement("a"); 
+	          link.setAttribute("href", $(this).attr("href"));
+	          link.setAttribute("class", "vrtx-image-listing-include-container-link");
+	          // IE
+	          link.setAttribute("className", "vrtx-image-listing-include-container-link");
+	          
+	      	  //replace link and image (w/ fade effect down to fadedOutOpacity) + stop() current animations.
+	          $(wrapper + " " + container).stop().fadeTo(fadeInOutTime, fadedOutOpacity, function() {
+	        	  //done fade out -> remove
+	        	  $("a.vrtx-image-listing-include-container-link", this).remove();
+	        	  //start fading in ...
+	        	  $(this).fadeTo(fadeInOutTime, 1);
+	        	  //... before adding new image for smoother change
+	        	  $(this).prepend(link);
+	        	  $(link, this).append(img);
+	          });
+	          
+	          //remove active classes
+	          jQuery(wrapper + " ul li a").each(function(j) {
+	        	  jQuery(this).removeClass("active");
+		      });    
+	         
+	          //add new active class
+	      	  $(this).addClass("active");
+	      	  
+	          //prevent default event action
+	    	  e.preventDefault(); 
+	      });
+    });
   
-});
+  };
+        
+})(jQuery);

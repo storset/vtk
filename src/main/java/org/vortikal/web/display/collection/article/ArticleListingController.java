76b7ae889c1b3f7054bb60b6cf1ed1a7d77d8c9c
Reza Mirzaei <rezam@usit.uio.no>
2010-03-10 09:23:45 +0000

    VTK-1509 - Cleanup, reorganize package structure a wee bit more.

--- src/main/java/org/vortikal/web/display/collection/article/ArticleListingController.java
+++ src/main/java/org/vortikal/web/display/collection/article/ArticleListingController.java
@@ -28,7 +28,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.vortikal.web.display.collection;
+package org.vortikal.web.display.collection.article;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +38,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Required;
 import org.vortikal.repository.Resource;
+import org.vortikal.web.display.collection.AbstractCollectionListingController;
 import org.vortikal.web.display.listing.ListingPager;
 import org.vortikal.web.search.Listing;
 import org.vortikal.web.service.URL;
--- src/main/java/org/vortikal/web/display/collection/article/ArticleListingSearcher.java
+++ src/main/java/org/vortikal/web/display/collection/article/ArticleListingSearcher.java
@@ -28,7 +28,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.vortikal.web.display.collection;
+package org.vortikal.web.display.collection.article;
 
 import java.util.ArrayList;
 import java.util.List;

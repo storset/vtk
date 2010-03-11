2983413cbff8b479a193a7279a355d98f676ee59
Reza Mirzaei <rezam@usit.uio.no>
2010-03-11 10:33:50 +0000

    VTK-1638 - God damn it... The whole point of "propertyfying" the version goes to hell with this kinda bullsh*t. Make it work again and start thinking about a good fix NOW.

--- src/main/resources/web/jquery/include-jquery.js
+++ src/main/resources/web/jquery/include-jquery.js
@@ -2,5 +2,5 @@ if (typeof jQuery == 'undefined') {
   var scriptNode = document.createElement("script");
   document.getElementsByTagName("head")[0].appendChild(scriptNode);
   scriptNode.setAttribute("type", "text/javascript", 0);
-  scriptNode.setAttribute("src", "/vrtx/__vrtx/static-resources/jquery/jquery-1.3.2.min.js", 0);
+  scriptNode.setAttribute("src", "/vrtx/__vrtx/static-resources/jquery/jquery-1.4.2.min.js", 0);
 }

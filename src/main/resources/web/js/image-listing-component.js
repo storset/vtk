c37c2c211de54080a946af75c093d96b14782466
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-25 19:13:15 +0000

    VTK-1628 - refactor initFirstImage() into main loop

--- src/main/resources/web/js/image-listing-component.js
+++ src/main/resources/web/js/image-listing-component.js
@@ -22,19 +22,18 @@
 	  $(container + "-pure-css").addClass(container.substring(1));
 	  $(container + "-nav-pure-css").addClass(container.substring(1) + "-nav");
 	  $(wrapper + "-thumbs-pure-css").addClass(wrapper.substring(1) + "-thumbs");
-
-	  initFirstImage();
 	  
 	  //paging (relative to li a.active)
 	  addPagingClickAndHoverEvents("next");
 	  addPagingClickAndHoverEvents("prev");
-		  
+	  
+	  //init
+	  calculateImage($(wrapper + " ul li a.active img"), 0, true);
+	  
 	  return this.each(function (i) {
 		   
-		   var link = generateLinkImage($("img", this), $(this), container);
-	      
-	       //cache image
-	       images[i] = link;
+		   var link = generateLinkImage($("img", this), $(this));
+	       images[i] = link; //cache image
 	       
 		   $(this).hover(function () {
 		    	if(!$(this).hasClass("active")) {
@@ -46,47 +45,58 @@
 		    	}
 	       });
 		   
-		   $(this).click(function(e) {
-		  	  //replace link and image (w/ fade effect down to fadedOutOpacity) + stop() current animation.
-		      if(settings.fadeInOutTime > 0) {
-			      $(wrapper + " " + container).stop().fadeTo(settings.fadeInOutTime, settings.fadedOutOpacity, function() {
-			    	  //done fade out -> remove
-			    	  $("a" + container + "-link", this).remove();
-			    	  //append image
-			    	  $(this).append(images[i]); 
-			    	  //fade in and make sure calculations are done after fully loaded
-			    	  $(this).fadeTo(settings.fadeInOutTime, 1, function() {
-			    		  addPagingClickAndHoverEvents(container + "-link");
-			    		  calculateImageAndPagingNavigationPosition();
-			    	  });
-			      });
-		      } else {
-		    	  $("a" + container + "-link", wrapper + " " + container).remove();
-		    	  $(wrapper + " " + container).append(images[i]);
-		    	  addPagingClickAndHoverEvents(container + "-link");
-		    	  calculateImageAndPagingNavigationPosition();
-		      }
-		      //remove active classes
-		      jQuery(wrapper + " ul li a").each(function(j) {
-		    	if(jQuery(this).hasClass("active")) {
-		    	   jQuery(this).removeClass("active");
-		    	   jQuery("img", this).stop().fadeTo(settings.fadeThumbsInOutTime, settings.fadedThumbsOutOpacity);
-		    	} else {
-		    	   jQuery("img", this).stop().fadeTo("0", settings.fadedThumbsOutOpacity);
-		    	}
-		      });
-		      
-		      //add new active class
-		  	  $(this).addClass("active");
-		  	  //make sure opacity is 1
-		  	  $("img", this).stop().fadeTo("0", 1);
-
-		  	  addDescription($("img", this));
-
-			  e.preventDefault(); 
+		  $(this).click(function(e) {
+			   calculateImage($("img", this), i, false)
+	  	       $(this).addClass("active");
+	  	       $("img", this).stop().fadeTo(0, 1);
+			   e.preventDefault(); 
 	      });
+		  
 		  centerThumbnailImage($("img", this)); 
 	 });
+
+	 function calculateImage(image, i, init) {
+		  //replace link and image (w/ fade effect down to fadedOutOpacity) + stop() current animation.
+	      if(settings.fadeInOutTime > 0 && !init) {
+		      $(wrapper + " " + container).stop().fadeTo(settings.fadeInOutTime, settings.fadedOutOpacity, function() {
+		    	  //done fade out -> remove
+		    	  $("a" + container + "-link", this).remove();
+		    	  //append image
+		    	  $(this).append(images[i]); 
+		    	  //fade in and make sure calculations are done after fully loaded
+		    	  $(this).fadeTo(settings.fadeInOutTime, 1, function() {
+		    		  addPagingClickAndHoverEvents(container + "-link");
+		    		  calculateImageAndPagingNavigationPosition();
+		    	  });
+		      });
+	      } else {
+	    	  $(wrapper + " " + container + " a" + container + "-link").remove();
+	    	  if(init) {
+	    		var link = generateLinkImage($(image), $(image).parent());
+	    		$(wrapper + " " + container).append(link);
+	    	  } else {
+	    	    $(wrapper + " " + container).append(images[i]);  
+	    	  }
+	    	  addPagingClickAndHoverEvents(container + "-link");
+	    	  calculateImageAndPagingNavigationPosition();
+	      }
+	      //remove active classes
+	      jQuery(wrapper + " ul li a").each(function(j) {
+	    	if(jQuery(this).hasClass("active")) {
+	    	   if(init) {
+	    		 $(image).stop().fadeTo(0, 1);
+	    	   } else {
+	    	     jQuery(this).removeClass("active");
+	    	     jQuery("img", this).stop().fadeTo(settings.fadeThumbsInOutTime, settings.fadedThumbsOutOpacity);
+	    	   }
+	    	} else {
+	    	   jQuery("img", this).stop().fadeTo(0, settings.fadedThumbsOutOpacity);
+	    	}
+	      });
+	  	  $(wrapper + " " + container + "-description").remove();
+		  $("<div class='" + container.substring(1) + "-description'>" 
+		          + $(image).attr("alt") + "</div>").insertAfter(wrapper + " " + container);
+	 }
 	  
 	 function centerThumbnailImage(thumb) {
 		 setTimeout(function() {
@@ -124,11 +134,11 @@
 		});
 	    //Fading of transparent block and prev / next icon
 	    if(navClass == "next" || navClass == "prev") { 
-	      fadeMultiple(wrapper + " " + " a." + navClass, wrapper + " " + " a." + navClass + " span", "0", 0);
+	      fadeMultiple(wrapper + " " + " a." + navClass, wrapper + " " + " a." + navClass + " span", 0, 0);
 	      $(wrapper + " " + " a." + navClass).hover(function () {
 			  $(wrapper + " " + " a." + navClass).stop().fadeTo(settings.fadeNavInOutTime, 1);
 			  $(wrapper + " " + " a." + navClass + " span").stop().fadeTo(settings.fadeNavInOutTime, 0.2);
-			}, function () { //hover out
+	      }, function () { //hover out
 			  fadeMultiple(new Array(wrapper + " " + " a." + navClass, wrapper + " " + " a." + navClass + " span"), settings.fadeNavInOutTime, 0)
 	      });
 	    } else if (navClass == container + "-link") {
@@ -141,31 +151,11 @@
 		  });
 	    }
 	 }
-
-	 //TODO: try to refactor this into main loop - could possible remove lot of lines and sub-functions..
-     function initFirstImage() {
-		
-	   var link = generateLinkImage(wrapper + " ul li a.active img", wrapper + " ul li a.active");
-      
-       $("a" + container + "-link", wrapper + " " + container).remove();
-       $(wrapper + " " + container).append(link);
-      
-       addPagingClickAndHoverEvents(container + "-link");
-       calculateImageAndPagingNavigationPosition();
-       addDescription(wrapper + " ul li a.active img");
-	  
-	   //set all thumbnails not active to settings.fadedThumbsOutOpacityopacity
-	   jQuery(wrapper + " ul li a").each(function(j) {
-		 if(!jQuery(this).hasClass("active")) {
-		   jQuery("img", this).stop().fadeTo("0", settings.fadedThumbsOutOpacity);
-		 }
-	   });
-	 }
      
      function generateLinkImage(theimage, thelink) {
        //create image
   	   var img = new Image();
-  	   var src = $(theimage).attr("src").split("?")[0]; 
+  	   var src = $(theimage).attr("src").split("?")[0];
   	   var alt = $(theimage).attr("alt");
   	   img.src = src; img.alt = alt;
   	  
@@ -181,12 +171,6 @@
 
   	   return link;
   	 }
-	  
-	 function addDescription(fromSource) {
-	   $(wrapper + " " + container + "-description").remove();
-	   $("<div class='" + container.substring(1) + "-description'>" 
-	          + $(fromSource).attr("alt") + "</div>").insertAfter(wrapper + " " + container);
-	 }
 
 	 function calculateImageAndPagingNavigationPosition() {
 	   var minHeight = 100; 

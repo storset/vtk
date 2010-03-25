9c34eca62e3b5eacf1d3ac506681d861ec78c51e
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-25 12:38:34 +0000

    VTK-1509 - show-hide JS very fast slideDown / slideUp (100ms)

--- src/main/resources/web/js/admin-edit-show-hide.js
+++ src/main/resources/web/js/admin-edit-show-hide.js
@@ -1,9 +1,13 @@
 // JavaScript Document
 
+var init = true;
+
 $(document).ready(function() {
   //init
   showHide();
 	
+  init = false;
+  
   //bind() click events
   $("#resource\\.recursive-listing\\.unspecified").bind("click", showHide);
   $("#resource\\.recursive-listing\\.false").bind("click", showHide);
@@ -15,15 +19,15 @@ $(document).ready(function() {
 
 function showHide() {
   if($("#resource\\.recursive-listing\\.false:checked").val() == 'false'){
-	$("#vrtx-resource\\.aggregation").hide();
-	$("#vrtx-resource\\.recursiveAggregation").hide();
+	$("#vrtx-resource\\.aggregation").slideUp(100);
+	$("#vrtx-resource\\.recursiveAggregation").slideUp(100);
   } else {
-    $("#vrtx-resource\\.aggregation").show();
-  	$("#vrtx-resource\\.recursiveAggregation").show();
+	$("#vrtx-resource\\.aggregation").slideDown(100);
+	$("#vrtx-resource\\.recursiveAggregation").slideDown(100);
   }
   if($("#resource\\.display-type\\.calendar:checked").val() != 'calendar') {
-	$("#vrtx-resource\\.event-type-title").hide();
+	$("#vrtx-resource\\.event-type-title").slideUp(100);
   } else {
-	$("#vrtx-resource\\.event-type-title").show();
+	$("#vrtx-resource\\.event-type-title").slideDown(100);
   }
 }

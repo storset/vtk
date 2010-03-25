196d31e997504fb9918c56952a7d41b388421bbd
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-25 13:14:52 +0000

    VTK-1509 add TODO

--- src/main/resources/web/js/admin-edit-show-hide.js
+++ src/main/resources/web/js/admin-edit-show-hide.js
@@ -2,7 +2,6 @@
   
 $(document).ready(function() {
   
-  //bind() click events
   showHide(new Array("#resource\\.recursive-listing\\.unspecified", "#resource\\.recursive-listing\\.false"), "#resource\\.recursive-listing\\.false:checked", 
 		               'false', new Array("#vrtx-resource\\.aggregation", "#vrtx-resource\\.recursiveAggregation"));
   showHide(new Array("#resource\\.display-type\\.unspecified", "#resource\\.display-type\\.calendar"), "#resource\\.display-type\\.calendar:checked",
@@ -24,15 +23,18 @@ function showHide(radioIds, condition, conditionVal, showHideProps) {
 	    }
 	  });
   }
+  
+  //init
+  //TODO: refactor with above code
   if($(condition).val() == conditionVal){
-      for(var i = 0; i < showHideProps.length; i++) {
-	    showHideProp(showHideProps[i], true, false);
-      }
-    } else {
-      for(var i = 0; i < showHideProps.length; i++) {
-        showHideProp(showHideProps[i], true, true);
-      }
+    for(var i = 0; i < showHideProps.length; i++) {
+	  showHideProp(showHideProps[i], true, false);
     }
+  } else {
+    for(var i = 0; i < showHideProps.length; i++) {
+      showHideProp(showHideProps[i], true, true);
+    }
+  }
 }
 
 function showHideProp(id, init, show) {

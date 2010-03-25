78d1ed4927ebdfb8b1929396aba80d04c9052446
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-25 12:14:28 +0000

    VTK-1509 - fix show-hide JS and add show / hide for 'Standard' / 'Calendar'

--- src/main/resources/web/js/admin-edit-show-hide.js
+++ src/main/resources/web/js/admin-edit-show-hide.js
@@ -1,19 +1,26 @@
 // JavaScript Document
-$( document ).ready( function() { 
+$(document ).ready( function() { 
   $( "#resource\\.recursive-listing\\.unspecified" ).bind( "click", showHide );
   $( "#resource\\.recursive-listing\\.false" ).bind( "click", showHide );
+  $( "#resource\\.display-type\\.unspecified" ).bind( "click", showHide );
+  $( "#resource\\.display-type\\.calendar" ).bind( "click", showHide );
   
-  if($( "#resource\\.recursive-listing\\.false:checked" ).val() != null){
-	  	$("#vrtx-resource\\.recursive-listing-subfolders").hide();
-  }
+  showHide();
 
 });
 
-function showHide()
-{
-  if( $( this ).val() ){
-	   $("#vrtx-resource\\.recursive-listing-subfolders").hide();
-  }else {
-	  $("#vrtx-resource\\.recursive-listing-subfolders").show();
+function showHide() {
+  if($("#resource\\.recursive-listing\\.false:checked").val() == 'false'){
+	  	$("#vrtx-resource\\.aggregation").hide();
+	  	$("#vrtx-resource\\.recursiveAggregation").hide();
+  } else {
+	    $("#vrtx-resource\\.aggregation").show();
+	  	$("#vrtx-resource\\.recursiveAggregation").show();
+  }
+  
+  if($("#resource\\.display-type\\.calendar:checked").val() != 'calendar') {
+	  $("#vrtx-resource\\.event-type-title").hide();
+  } else {
+	  $("#vrtx-resource\\.event-type-title").show();
   }
 }

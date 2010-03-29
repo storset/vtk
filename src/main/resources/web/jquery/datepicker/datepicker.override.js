9c4a51dc792d47a3f13aaa2c21896c716c889196
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-03-29 10:50:01 +0000

    VTK-1509 use formatDate() for date formatting

--- src/main/resources/web/jquery/datepicker/datepicker.override.js
+++ src/main/resources/web/jquery/datepicker/datepicker.override.js
@@ -32,7 +32,7 @@ function eventListingCalendar(allowedDates, activeDate, clickableDayTitle, notCl
 
       // Add classes and tooltip for dates with and without events
       if ($.inArray(date_str, allowedDates) != -1) {
-        if (removeZeroesBeforeDayAndMonth(activeDate) == date_str) {
+        if ($.datepicker.formatDate("yy-m-d", new Date(activeDate)).toString() == date_str) {
           return [ true, 'state-active', clickableDayTitle ];
         } else {
           return [ true, '', clickableDayTitle ];
@@ -42,20 +42,15 @@ function eventListingCalendar(allowedDates, activeDate, clickableDayTitle, notCl
       }
     },
     onChangeMonthYear : function(year, month, inst) {
-      if (month <= 9) {
-        month = "0" + month;
-      }
-
-      // If not init (when prev / next month click), refresh page with year and
-      // month
+      var date = $.datepicker.formatDate("yy-mm", new Date(year, month - 1)).toString();
+      
       if (!init) {
-        location.href = location.href.split('?')[0] + "?date=" + year + '-' + month;
+        location.href = location.href.split('?')[0] + "?date=" + date;
       } else {
         init = false;
       }
-      
       //wait.. and make month link
-      setTimeout(function(){makeMonthLink(year, month)}, 100);
+      setTimeout(function(){makeMonthLink(date)}, 100);
     }
   });
 
@@ -64,13 +59,9 @@ function eventListingCalendar(allowedDates, activeDate, clickableDayTitle, notCl
   });
 }
 
-function makeMonthLink(year, month) {
-  $(".ui-datepicker-month").html("<a href='" + location.href.split('?')[0] + "?date=" + year + '-' + month + "'>" 
+function makeMonthLink(date) {
+  $(".ui-datepicker-month").html("<a href='" + location.href.split('?')[0] + "?date=" + date + "'>" 
   + $(".ui-datepicker-month").text() + ' ' + $(".ui-datepicker-year").remove().text() + "</a>");
-}
-
-function removeZeroesBeforeDayAndMonth(date) {
-  return date.replace(/-0/g, "-");
 }
 
 // For init of calender / datepicker()

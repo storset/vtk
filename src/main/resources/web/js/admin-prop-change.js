9760867a457c84de83bfff755ccfc7fa48e7be25
Magnus Korvald <korvald@usit.uio.no>
2010-05-22 08:17:35 +0000

    Extra null check

--- src/main/resources/web/js/admin-prop-change.js
+++ src/main/resources/web/js/admin-prop-change.js
@@ -3,37 +3,39 @@ var INITIAL_INPUT_FIELDS = new Array();
 var NEED_TO_CONFIRM = true;
 var UNSAVED_CAHANGES_CONFIRMATION;
 
-function initPropChange(){
+function initPropChange() {
     var i = 0;
     $("input").each(function() {
         INITIAL_INPUT_FIELDS[i++] = this.value;
     });
 }
 
-function unsavedChangesInEditor(){
-    if(!NEED_TO_CONFIRM)
+function unsavedChangesInEditor() {
+    if (!NEED_TO_CONFIRM)
         return false;
     var dirtyState = false;
     currentStateOfInputFields = $("input");
-    for(i = 0; i < INITIAL_INPUT_FIELDS.length; i++){
-        if(currentStateOfInputFields[i].value != INITIAL_INPUT_FIELDS[i]){
+    for (i = 0; i < INITIAL_INPUT_FIELDS.length; i++) {
+        if (currentStateOfInputFields[i].value != INITIAL_INPUT_FIELDS[i]) {
             dirtyState = true;
             break;
         }
     }
     $("textarea").each(function() {
-        if(typeof(FCKeditorAPI) != "undefined"){
-            if(FCKeditorAPI.GetInstance(this.name).IsDirty()){
-                dirtyState = true;
-                return;
+        if (typeof (FCKeditorAPI) != "undefined") {
+            if (FCKeditorAPI.GetInstance(this.name) != null) {
+                if (FCKeditorAPI.GetInstance(this.name).IsDirty()) {
+                    dirtyState = true;
+                    return;
+                }
             }
         }
-     });
-     return dirtyState;
+    });
+    return dirtyState;
 }
 
-function unsavedChangesInEditorMessage(){
-    if(unsavedChangesInEditor()){
+function unsavedChangesInEditorMessage() {
+    if (unsavedChangesInEditor()) {
         return UNSAVED_CAHANGES_CONFIRMATION;
     }
 }

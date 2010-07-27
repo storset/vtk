2fed58ab175873d00b3d5968ed2a933ef776fd0b
Gorm Paulsen <gormap@usit.uio.no>
2010-07-27 13:26:41 +0000

    [VTK-1711] Avoid outputting '=' for empty query string parameters.

--- src/main/java/org/vortikal/web/service/URL.java
+++ src/main/java/org/vortikal/web/service/URL.java
@@ -256,11 +256,11 @@ public class URL {
             for (Iterator<String> j = values.iterator(); j.hasNext();) {
                 String val = j.next();
                 val = encode(val);
-//                if ("".equals(val)) {
-//                    qs.append(encodedParam);
-//                } else {
+                if ("".equals(val)) {
+                    qs.append(encodedParam);
+                } else {
                     qs.append(encodedParam).append("=").append(val);    
-//                }
+                }
                 if (j.hasNext()) {
                     qs.append("&");
                 }

97a1a84564ea5381b88875ac630723ed9f319316
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-08-09 10:48:50 +0000

    Gallery IE 6 - set maxSize outside loop

--- src/main/resources/web/js/ie6-image-listing.js
+++ src/main/resources/web/js/ie6-image-listing.js
@@ -2,10 +2,12 @@
 
 $(document).ready( function() {
   if (jQuery.browser.msie && jQuery.browser.version <= 6) {
+
+    var maxSize = parseInt($(this).css('height'));
+
     $(".vrtx-image-container").each( function(i) {
 
       var height = $(this).find("img").height();
-      var maxSize = parseInt($(this).css('height'));
 
       if (height > maxSize) {
         $(this).find("img").css('height', maxSize + 'px');

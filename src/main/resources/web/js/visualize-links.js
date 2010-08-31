4b205e8b08f77357018deb63fa96e4144842b2ef
Reza Mirzaei <rezam@usit.uio.no>
2010-08-31 11:52:10 +0000

    VTK-1542 - Use resourceUrl as part of ajax call.

--- src/main/resources/web/js/visualize-links.js
+++ src/main/resources/web/js/visualize-links.js
@@ -1,66 +1,46 @@
 // AJAX request against link-check action
 // that returns links seperated by new line
 // Return array of dead links
-function visualizeDeadLinks() {
-	
-	var deadLinks = [];
-	
-	var LINK_CHECK_URL = "?vrtx=admin&action=link-check";
-	var URL = $(location).attr('href');
-	
-	if ((URL.indexOf("?") != -1)) {
-		URL = URL.split("?")[0];
-	}
-	
-	if(URL.indexOf("localhost") == -1) {
-	
-	  URL = URL.split(".");
-	  URL[0] = URL[0] + "-adm";
-	  URL = URL.shift() + "." + URL.join(".");
-	
-	}
-	
-	URL = URL + LINK_CHECK_URL;
-	
-	$.ajax({
-	  type : "GET",
-	  url : URL,
-	  dataType : "text",
-	  complete : function(msg) {
-		
-          deadLinks = msg.responseText.split("\n");
-        
-		  $("iframe").contents().find("body")
-            .filter(function() {
-              return this.id.match(/^(?!vrtx-[\S]+-listing|vrtx-collection)[\S]+/);
-            })
-            .find("#main")
-            .not("#left-main")
-              .find("a").each(function(i, e){
-                visualizeDeadLink(this, deadLinks);
-              });
-		
-		  if(msg.status != 200) {
-		    $("iframe").contents().find("body").append(msg.status + " " + msg.statusText);
-		  }
-        
-	  },
-      error : function (xhr, ajaxOptions, thrownError){
-	    $("iframe").contents().find("body").append(" " + thrownError + " ");
-	  }
-	});
+function visualizeDeadLinks(resourceUrl) {
+
+  var deadLinks = [];
+  var LINK_CHECK_URL = resourceUrl + "?vrtx=link-check";
+
+  $.ajax( {
+    type :"GET",
+    url :URL,
+    dataType :"text",
+    complete : function(msg) {
+
+      deadLinks = msg.responseText.split("\n");
+
+      $("iframe").contents().find("body").filter( function() {
+        return this.id.match(/^(?!vrtx-[\S]+-listing|vrtx-collection)[\S]+/);
+      }).find("#main").not("#left-main").find("a").each( function(i, e) {
+        visualizeDeadLink(this, deadLinks);
+      });
+
+      if (msg.status != 200) {
+        $("iframe").contents().find("body").append(msg.status + " " + msg.statusText);
+      }
+
+    },
+    error : function(xhr, ajaxOptions, thrownError) {
+      $("iframe").contents().find("body").append(" " + thrownError + " ");
+    }
+  });
 }
 
 // Check if current link is in array of dead links
 function visualizeDeadLink(that, deadLinks) {
-	
-	var TARGET_URL = $(that).attr("href");
-	
-	var deadLinksLength = deadLinks.length;
-	for(var i = 0; i < deadLinksLength; i++) {
-	  if(TARGET_URL == deadLinks[i]) {
-	    $(that).append(" - 404").css("color", "red"); 
-	  }
-	}
-	
+
+  var TARGET_URL = $(that).attr("href");
+
+  var deadLinksLength = deadLinks.length;
+  for ( var i = 0; i < deadLinksLength; i++) {
+    if (TARGET_URL == deadLinks[i]) {
+      $(that).append(" - 404").css("color", "red");
+    }
+  }
+
 }

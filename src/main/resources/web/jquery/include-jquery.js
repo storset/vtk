cf53254154f0b1325dea71fd8afd7ae41100eb82
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-10-20 11:25:46 +0000

    VTK-1938 - adding to head does not work in Chrome / Safari, and need it before image-listing-component.js is initialized, so the simplest solution is just writing it to document

--- src/main/resources/web/jquery/include-jquery.js
+++ src/main/resources/web/jquery/include-jquery.js
@@ -1,6 +1,3 @@
 if (typeof jQuery == 'undefined') {
-  var scriptNode = document.createElement("script");
-  document.getElementsByTagName("head")[0].appendChild(scriptNode);
-  scriptNode.setAttribute("type", "text/javascript", 0);
-  scriptNode.setAttribute("src", "/vrtx/__vrtx/static-resources/jquery/jquery-1.4.2.min.js", 0);
+  document.write("<scr" + "ipt src='/vrtx/__vrtx/static-resources/jquery/jquery-1.4.2.min.js' type='text/javascript'><\/script>");
 }

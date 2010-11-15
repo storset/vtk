a59c91517b4ec8b4072b6c76ea2082739dce3d80
Magnus Korvald <korvald@usit.uio.no>
2010-11-15 14:23:27 +0000

    VTK-1970 Documentation

--- src/main/java/org/vortikal/web/decorating/components/IncludeMediaPlayerComponent.java
+++ src/main/java/org/vortikal/web/decorating/components/IncludeMediaPlayerComponent.java
@@ -1,5 +1,6 @@
 package org.vortikal.web.decorating.components;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.vortikal.web.decorating.DecoratorRequest;
@@ -8,10 +9,15 @@ import org.vortikal.web.decorating.DecoratorResponse;
 public class IncludeMediaPlayerComponent extends ResourceMediaPlayerComponent {
 
     private final static String PARAMETER_URL = "url";
+    private final static String PARAMETER_URL_DESCRIPTION = "Media file url or uri";
     private final static String PARAMETER_HEIGHT = "height";
+    private final static String PARAMETER_HEIGHT_DESCRIPTION = "Height of player";
     private final static String PARAMETER_WIDTH = "width";
+    private final static String PARAMETER_WIDTH_DESCRIPTION = "Width of player";
     private final static String PARAMETER_AUTOPLAY = "autoplay";
+    private final static String PARAMETER_AUTOPLAY_DESCRIPTION = "Start playing immediately if set to 'true'. Default is 'false'";
     private final static String PARAMETER_CONTENT_TYPE = "content-type";
+    private final static String PARAMETER_CONTENT_TYPE_DESCRIPTION = "Content type of media file";
 
     protected void processModel(Map<Object, Object> model, DecoratorRequest request, DecoratorResponse response)
             throws Exception {
@@ -42,4 +48,13 @@ public class IncludeMediaPlayerComponent extends ResourceMediaPlayerComponent {
         model.put("extension", extension);
     }
 
+    protected Map<String, String> getParameterDescriptionsInternal() {
+        Map<String, String> map = new LinkedHashMap<String, String>();
+        map.put(PARAMETER_URL, PARAMETER_URL_DESCRIPTION);
+        map.put(PARAMETER_HEIGHT, PARAMETER_HEIGHT_DESCRIPTION);
+        map.put(PARAMETER_WIDTH, PARAMETER_WIDTH_DESCRIPTION);
+        map.put(PARAMETER_AUTOPLAY, PARAMETER_AUTOPLAY_DESCRIPTION);
+        map.put(PARAMETER_CONTENT_TYPE, PARAMETER_CONTENT_TYPE_DESCRIPTION);
+        return map;
+    }
 }

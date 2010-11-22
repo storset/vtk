e7f6d715bfd5c392bdf95dc7b9a7262bf2429dba
David Egeland <dave@usit.uio.no>
2010-11-22 12:41:49 +0000

    Corrected styleset and css-config for ckeditor.

--- src/main/resources/web/js/admin-fck-setup.js
+++ src/main/resources/web/js/admin-fck-setup.js
@@ -8,6 +8,183 @@ function newEditor(name, completeEditor, withoutSubSuper, baseFolder, baseUrl, b
 
   var ck = CKEDITOR;
   ck.baseHref = baseUrl + "/";
+
+  ck.config.stylesSet = [
+                {
+                    name : 'Facts left',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-facts-container vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Facts right',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-facts-container vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Image left',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-img-container vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Image center',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-img-container vrtx-container-middle vrtx-img-container-middle-ie'
+                    }
+                },
+                {
+                    name : 'Image right',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-img-container vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt left (800px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt left (700px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt left (600px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-l vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt left (500px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-m vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt left (400px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-s vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt left (300px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xs vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt left (200px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xxs vrtx-container-left'
+                    }
+                },
+                {
+                    name : 'Img & capt center (full)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-full vrtx-container-middle'
+                    }
+                },
+                {
+                    name : 'Img & capt center (800px)',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-middle'
+                    }
+                },
+                {
+                    name : 'Img & capt center (700px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-middle'
+                    }
+                },
+                {
+                    name : 'Img & capt center (600px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-l vrtx-container-middle'
+                    }
+                },
+                {
+                    name : 'Img & capt center (500px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-m vrtx-container-middle'
+                    }
+                },
+                {
+                    name : 'Img & capt center (400px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-s vrtx-container-middle'
+                    }
+                },
+                {
+                    name : 'Img & capt right (800px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt right (700px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt right (600px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-l vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt right (500px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-m vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt right (400px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-s vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt right (300px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xs vrtx-container-right'
+                    }
+                },
+                {
+                    name : 'Img & capt right (200px) ',
+                    element : 'div',
+                    attributes : {
+                        'class' : 'vrtx-container vrtx-container-size-xxs vrtx-container-right'
+                    }
+                } ];                
   
   ck.config['DefaultLanguage'] = defaultLanguage;
 
@@ -65,7 +242,7 @@ function newEditor(name, completeEditor, withoutSubSuper, baseFolder, baseUrl, b
         cssFileList[cssFileList.length] = "/vrtx/__vrtx/static-resources/themes/default/editor-container-ie.css";
       }
 
-      ck.config['EditorAreaCSS'] = cssFileList;
+      ck.config.contentsCss = cssFileList;
 
   	  ck.replace(name, ck.config);
 }

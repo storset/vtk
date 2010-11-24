8c3c481ab6212cb0aa09c220847148dc8f671d7c
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-11-24 12:39:42 +0000

    CKEditor: thats a little closer

--- src/main/resources/web/js/admin-fck-setup.js
+++ src/main/resources/web/js/admin-fck-setup.js
@@ -51,7 +51,7 @@ function newEditor(name, completeEditor, withoutSubSuper, baseFolder, baseUrl, b
   } else if (isCompleteEditor) {	  
 	var height = 220; var maxHeight = 400;
     if (name.indexOf("supervisor-box") != -1) { height = 130; maxHeight = 300; }
-    else if (name.indexOf("content") != -1 && name != "related-content") { height = 400; maxHeight = 800; }
+    else if (name == "content" || name == "resource.content" ) { height = 400; maxHeight = 800; }
 
 	setCKEditorConfig(name, linkBrowseUrl, imageBrowseUrl, flashBrowseUrl, 
 			          defaultLanguage, cssFileList, height ,maxHeight, 50, completeToolbarPointer, isCompleteEditor, true);

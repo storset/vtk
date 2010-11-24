3f6996f07a6e8ebc5a0f1f1f5a06b7a638bf01b8
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-11-24 12:34:05 +0000

    CKEditor: remove console.log

--- src/main/resources/web/js/admin-fck-setup.js
+++ src/main/resources/web/js/admin-fck-setup.js
@@ -52,9 +52,7 @@ function newEditor(name, completeEditor, withoutSubSuper, baseFolder, baseUrl, b
 	var height = 220; var maxHeight = 400;
     if (name.indexOf("supervisor-box") != -1) { height = 130; maxHeight = 300; }
     else if (name.indexOf("content") != -1 && name != "related-content") { height = 400; maxHeight = 800; }
-    
-    console.log(name + " " + height + " " + maxHeight);
-	
+
 	setCKEditorConfig(name, linkBrowseUrl, imageBrowseUrl, flashBrowseUrl, 
 			          defaultLanguage, cssFileList, height ,maxHeight, 50, completeToolbarPointer, isCompleteEditor, true);
   } else if (isWithoutSubSuper) {

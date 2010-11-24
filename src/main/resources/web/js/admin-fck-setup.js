9f5bc37b2bff72767a37acbe7e2cebb57579ef8f
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-11-24 11:23:23 +0000

    CKEditor: start refactoring configs and set smaller height when boxContent for completeEditor

--- src/main/resources/web/js/admin-fck-setup.js
+++ src/main/resources/web/js/admin-fck-setup.js
@@ -1,5 +1,24 @@
+var inlineToolbar = [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
+                        'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript',
+                          'SpecialChar' ] ];
+var withoutSubSuperToolbar = [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
+                       'Italic', 'Underline', 'Strike', 'SpecialChar' ] ];
+
+var introductionCaptionToolbar = [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
+                                     'Italic', 'Underline', 'Strike', 'SpecialChar' ] ];
+
+var completeToolbar = [ [ 'Source', 'PasteText', '-', 'Undo', 'Redo', '-', 'Replace',
+                          'RemoveFormat', '-', 'Link', 'Unlink', 'Anchor',
+                          'Image', 'CreateDiv', 'MediaEmbed', 'Table',
+                          'HorizontalRule', 'SpecialChar' 
+                        ],['Format', 'Bold', 'Italic', 'Underline', 'Strike',
+                           'Subscript', 'Superscript', 'NumberedList',
+                           'BulletedList', 'Outdent', 'Indent', 'JustifyLeft',
+                           'JustifyCenter', 'JustifyRight', 'TextColor',
+                           'Maximize' ]];
+
 function newEditor(name, completeEditor, withoutSubSuper, baseFolder, baseUrl, baseDocumentUrl, browsePath,
-    defaultLanguage, cssFileList) {
+                   defaultLanguage, cssFileList) {
 
   // File browser
   var linkBrowseUrl  = baseUrl + '/plugins/filemanager/browser/default/browser.html?BaseFolder=' + baseFolder + '&Connector=' + browsePath;
@@ -12,279 +31,240 @@ function newEditor(name, completeEditor, withoutSubSuper, baseFolder, baseUrl, b
   if(browser.indexOf("MSIE") > -1 && ieversion <= 7){
     cssFileList.push("/vrtx/__vrtx/static-resources/themes/default/editor-container-ie.css");
   }
-
-  //CKEditor configurations
-  var completeEditorConfig = {
-  	filebrowserBrowseUrl : linkBrowseUrl,
-  	filebrowserImageBrowseUrl : imageBrowseUrl,
-  	filebrowserFlashBrowseUrl : flashBrowseUrl, 
-  	extraPlugins : 'MediaEmbed',
-  	toolbarCanCollapse : false,
-  	contentsCss : cssFileList,
-      defaultLanguage : 'no',
-      language : defaultLanguage,
-      toolbar : [
-
-              [ 'Source', 'PasteText', '-', 'Undo', 'Redo', '-', 'Replace',
-                      'RemoveFormat', '-', 'Link', 'Unlink', 'Anchor',
-                      'Image', 'CreateDiv', 'MediaEmbed', 'Table',
-                      'HorizontalRule', 'SpecialChar' 
-               ],['Format', 'Bold', 'Italic', 'Underline', 'Strike',
-                      'Subscript', 'Superscript', 'NumberedList',
-                      'BulletedList', 'Outdent', 'Indent', 'JustifyLeft',
-                      'JustifyCenter', 'JustifyRight', 'TextColor',
-                      'Maximize' ]
-
-      ],
-      resize_enabled : true,
-      autoGrow_maxHeight : '400px',
-      autoGrow_minHeight : '50px',
-      stylesSet : [
-              {
-                  name : 'Facts left',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-facts-container vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Facts right',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-facts-container vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Image left',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-img-container vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Image center',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-img-container vrtx-container-middle vrtx-img-container-middle-ie'
-                  }
-              },
-              {
-                  name : 'Image right',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-img-container vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt left (800px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt left (700px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt left (600px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-l vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt left (500px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-m vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt left (400px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-s vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt left (300px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xs vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt left (200px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xxs vrtx-container-left'
-                  }
-              },
-              {
-                  name : 'Img & capt center (full)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-full vrtx-container-middle'
-                  }
-              },
-              {
-                  name : 'Img & capt center (800px)',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-middle'
-                  }
-              },
-              {
-                  name : 'Img & capt center (700px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-middle'
-                  }
-              },
-              {
-                  name : 'Img & capt center (600px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-l vrtx-container-middle'
-                  }
-              },
-              {
-                  name : 'Img & capt center (500px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-m vrtx-container-middle'
-                  }
-              },
-              {
-                  name : 'Img & capt center (400px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-s vrtx-container-middle'
-                  }
-              },
-              {
-                  name : 'Img & capt right (800px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt right (700px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt right (600px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-l vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt right (500px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-m vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt right (400px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-s vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt right (300px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xs vrtx-container-right'
-                  }
-              },
-              {
-                  name : 'Img & capt right (200px) ',
-                  element : 'div',
-                  attributes : {
-                      'class' : 'vrtx-container vrtx-container-size-xxs vrtx-container-right'
-                  }
-              } ]
-  }
-
-  var inlineEditorConfig = {
-      filebrowserBrowseUrl : linkBrowseUrl,
-      defaultLanguage : 'no',
-      language : defaultLanguage,
-      toolbar : [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
-              'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript',
-              'SpecialChar' ] ],
-      resize_enabled : true,
-		toolbarCanCollapse : false,		
-      height : '40px',
-      autoGrow_maxHeight : '400px',
-      autoGrow_minHeight : '40px'
-
-  }
-
-  var withoutSubSuperEditorConfig = {
-  	filebrowserBrowseUrl : linkBrowseUrl,
-      defaultLanguage : 'no',
-      language : defaultLanguage,
-      toolbar : [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
-              'Italic', 'Underline', 'Strike', 'SpecialChar' ] ],
-      resize_enabled : true,
-      toolbarCanCollapse : false,
-      height : '40px',
-      autoGrow_maxHeight : '400px',
-      autoGrow_minHeight : '40px'
-  }
-
-  var introductionEditorConfig = {
-  	filebrowserBrowseUrl : linkBrowseUrl,
-      defaultLanguage : 'no',
-      language : defaultLanguage,
-      toolbar : [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
-              'Italic', 'Underline', 'Strike', 'SpecialChar' ] ],
-      resize_enabled : false,
-   	  toolbarCanCollapse : false,
-   	  height : '150px',
-      autoGrow_maxHeight : '400px',
-      autoGrow_minHeight : '40px'
-  }
-
-  var captionEditorConfig = {
-  	filebrowserBrowseUrl : linkBrowseUrl,
-      defaultLanguage : 'no',
-      language : defaultLanguage,
-      toolbar : [ [ 'Source', 'PasteText', 'Link', 'Unlink', 'Bold',
-              'Italic', 'Underline', 'Strike', 'SpecialChar' ] ],
-      resize_enabled : false,
-      toolbarCanCollapse : false,        
-      height : '93px',
-      autoGrow_maxHeight : '400px',
-      autoGrow_minHeight : '40px'
-  }
   
   var completeEditor = completeEditor != null ? completeEditor : false;
   var withoutSubSuper = withoutSubSuper != null ? withoutSubSuper : false;
-
+  
+  //CKEditor configurations
   if (name.indexOf("introduction") != -1) {
-	  setCKEditorConfig(name, introductionEditorConfig);
+    setCKEditorConfig(name, createCKEditorConfig("intro", linkBrowseUrl, null, null, 
+			          defaultLanguage, null, 150, 400, 40, introductionCaptionToolbar));
   } else if (name.indexOf("caption") != -1) {
-	  setCKEditorConfig(name, captionEditorConfig);
-  } else if (completeEditor) {
-	  setCKEditorConfig(name,  completeEditorConfig);
+    setCKEditorConfig(name, createCKEditorConfig("caption", linkBrowseUrl, null, null, 
+			          defaultLanguage, null, 93, 400, 40, introductionCaptionToolbar));
+  } else if (completeEditor) {	  
+	var height = 400;
+	var maxHeight = 800;
+	if (name.indexOf("boxContent") != -1) {
+	  height = 220;
+	  maxHeight = 400;
+	}
+	setCKEditorConfig(name, createCKEditorConfig("complete", linkBrowseUrl, imageBrowseUrl, flashBrowseUrl, 
+			          defaultLanguage, cssFileList, height ,maxHeight, 50, completeToolbar));
   } else if (withoutSubSuper) {
-	  setCKEditorConfig(name,  inlineEditorConfig);
+	setCKEditorConfig(name, createCKEditorConfig("inline", linkBrowseUrl, null, null, 
+			          defaultLanguage, null, 40, 400, 40, inlineToolbar));
   } else {
-	  setCKEditorConfig(name,  withoutSubSuperEditorConfig);
+	setCKEditorConfig(name, createCKEditorConfig("withoutSubSuper", linkBrowseUrl, null, null,
+			          defaultLanguage, null, 40, 400, 40, withoutSubSuperToolbar));
   }
+
+}
+
+function createCKEditorConfig(name, linkBrowseUrl, imageBrowseUrl, flashBrowseUrl, defaultLanguage, cssFileList, height, maxHeight, minHeight, toolbar) {
+
+  var config = [{}];
+  config.filebrowserBrowseUrl = linkBrowseUrl;
+  if("complete") {
+	config.filebrowserImageBrowseUrl = imageBrowseUrl;
+	config.filebrowserFlashBrowseUrl = flashBrowseUrl;
+	config.extraPlugins = 'MediaEmbed';
+	config.contentsCss = cssFileList;
+    config.stylesSet = createDivContainerStyleSet();
+  }
+  config.toolbarCanCollapse = false;
+  if("complete" || "withoutSubSuper" || "inline") {
+	config.resize_enabled = true;
+  } else {
+    config.resize_enabled = false;
+  }
+	
+  config.defaultLanguage = 'no';
+  config.language = defaultLanguage;
+  config.toolbar = toolbar;
+  config.height = height + 'px';
+  config.autoGrow_maxHeight = maxHeight + 'px';
+  config.autoGrow_minHeight = minHeight + 'px';
+	
+  return config;
+}
+
+function createDivContainerStyleSet() {
+	return [{
+     name : 'Facts left',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-facts-container vrtx-container-left'
+     }
+ },
+ {
+     name : 'Facts right',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-facts-container vrtx-container-right'
+     }
+ },
+ {
+     name : 'Image left',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-img-container vrtx-container-left'
+     }
+ },
+ {
+     name : 'Image center',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-img-container vrtx-container-middle vrtx-img-container-middle-ie'
+     }
+ },
+ {
+     name : 'Image right',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-img-container vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt left (800px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt left (700px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt left (600px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-l vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt left (500px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-m vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt left (400px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-s vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt left (300px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xs vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt left (200px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xxs vrtx-container-left'
+     }
+ },
+ {
+     name : 'Img & capt center (full)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-full vrtx-container-middle'
+     }
+ },
+ {
+     name : 'Img & capt center (800px)',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-middle'
+     }
+ },
+ {
+     name : 'Img & capt center (700px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-middle'
+     }
+ },
+ {
+     name : 'Img & capt center (600px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-l vrtx-container-middle'
+     }
+ },
+ {
+     name : 'Img & capt center (500px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-m vrtx-container-middle'
+     }
+ },
+ {
+     name : 'Img & capt center (400px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-s vrtx-container-middle'
+     }
+ },
+ {
+     name : 'Img & capt right (800px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xxl vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt right (700px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xl vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt right (600px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-l vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt right (500px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-m vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt right (400px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-s vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt right (300px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xs vrtx-container-right'
+     }
+ },
+ {
+     name : 'Img & capt right (200px) ',
+     element : 'div',
+     attributes : {
+         'class' : 'vrtx-container vrtx-container-size-xxs vrtx-container-right'
+     }
+ }];
 
 }
 

68785877eac967340de843dd5caa1c485b20cf19
Reza Mirzaei <rezam@usit.uio.no>
2010-11-24 11:01:10 +0000

    VTK-1880 - Add ddl for deleted_resource table to default schema sql.

--- src/main/sql/oracle-schema.sql
+++ src/main/sql/oracle-schema.sql
@@ -405,7 +405,7 @@ values (
     'Y',
     'vortex@localhost',
     sysdate
-);    
+);
 
 
 -- (pseudo:owner, all)
@@ -426,6 +426,35 @@ values (
     'Y',
     'vortex@localhost',
     sysdate
-);    
+);
 
+-----------------------------------------------------------------------------
+-- deleted_resource (trash can)
+-----------------------------------------------------------------------------
 
+drop sequence deleted_resource_seq_pk;
+
+create sequence deleted_resource_seq_pk increment by 1 start with 1000;
+
+drop table deleted_resource cascade constraints;
+
+create table deleted_resource
+(
+  id number not null,
+  resource_trash_uri VARCHAR2 (64) not null,
+  parent_id number not null,
+  deleted_by VARCHAR2 (64) not null,
+  deleted_time timestamp not null,
+  was_inherited_acl char(1) default 'N' not null
+);
+
+alter table deleted_resource
+  add constraint deleted_resource_PK primary key (id);
+
+alter table deleted_resource
+  add constraint deleted_resource_uri_FK foreign key (resource_trash_uri)
+  references vortex_resource (uri) on delete cascade;
+
+alter table deleted_resource
+  add constraint deleted_resource_parent_FK foreign key (parent_id)
+  references vortex_resource (resource_id) on delete cascade;
--- src/main/sql/postgresql-schema.sql
+++ src/main/sql/postgresql-schema.sql
@@ -403,7 +403,7 @@ values (
     'Y',
     'vortex@localhost',
     current_timestamp
-);    
+);
 
 
 -- (pseudo:owner, all)
@@ -424,5 +424,35 @@ values (
     'Y',
     'vortex@localhost',
     current_timestamp
-);    
+);
 
+-----------------------------------------------------------------------------
+-- deleted_resource (trash can)
+-----------------------------------------------------------------------------
+
+drop sequence deleted_resource_seq_pk;
+
+create sequence deleted_resource_seq_pk INCREMENT 1 START 1000;
+
+drop table deleted_resource CASCADE;
+
+create table deleted_resource
+(
+  id int not null,
+  resource_trash_uri varchar (64) not null,
+  parent_id int not null,
+  deleted_by varchar(64) not null,
+  deleted_time timestamp not null,
+  was_inherited_acl CHAR(1) default 'N' not null
+);
+
+alter table deleted_resource
+  add constraint deleted_resource_PK primary key (id);
+
+alter table deleted_resource
+  add constraint deleted_resource_uri_FK foreign key (resource_trash_uri)
+  references vortex_resource (uri) on delete cascade;
+
+alter table deleted_resource
+  add constraint deleted_resource_parent_FK foreign key (parent_id)
+  references vortex_resource (resource_id) on delete cascade;

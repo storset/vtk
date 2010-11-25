d6bdc9cdf427a137f039bc4d28f3cf0a8ba5f759
Øyvind Hatland <oyvihatl@usit.uio.no>
2010-11-25 20:51:15 +0000

    VTK - performance: reversing loops order for fewer operations per iteration, and use function pointer outside loops because local variables (function scope) are faster to access than out-of-scope variables

--- src/main/resources/web/js/multipleinputfields.js
+++ src/main/resources/web/js/multipleinputfields.js
@@ -8,13 +8,13 @@ function loadMultipleInputFields(name, addName, removeName, moveUpName, moveDown
     var id = "#" + name;
     if ($(id).val() == null) { return; }
     var formFields = $(id).val().split(",");
-    
+
     COUNTER_FOR_MULTIPLE_INPUT_FIELD[name] = 1; // 1-index
     LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] = formFields.length;
     MULTIPLE_INPUT_FIELD_NAMES.push(name);
-    
+
     var size = $(id).attr("size");
-    
+
     registerClicks(name);
 
     $(id).hide();
@@ -22,13 +22,14 @@ function loadMultipleInputFields(name, addName, removeName, moveUpName, moveDown
     		  + "<button  onClick=\"addFormField('" + name + "',null, '"
     		  + removeName + "','" + moveUpName + "','" + moveDownName + "','" + size + "'," + false + "); return false;\">"
     		  + addName + "</button></div>");
-     
-    for (var i = 0; i < LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]; i++) {
-       addFormField(name, jQuery.trim(formFields[i]), removeName, moveUpName, moveDownName, size, true);    
-    }
-} 
 
-function registerClicks(name) {
+    var addFormFieldFunc = addFormField;
+    for (var i = LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]; i--; ) {
+       addFormFieldFunc(name, jQuery.trim(formFields[i]), removeName, moveUpName, moveDownName, size, true);
+    }
+}
+
+function registerClicks(name) { // Use delegate() for dynamic eventhandlers
   $("." + name).delegate(".remove", "click", function(){
 	removeFormField(name, $(this));
   });
@@ -42,70 +43,70 @@ function registerClicks(name) {
 
 function addFormField(name, value, removeName, moveUpName, moveDownName, size, init) {
 	if (value == null) { value = ""; }
-	
+
     var idstr = "vrtx-" + name + "-";
     var i = COUNTER_FOR_MULTIPLE_INPUT_FIELD[name];
     var removeButton = "";
     var moveUpButton = "";
     var moveDownButton = "";
-    
+
     if (removeName != null) {
         removeButton = "<button class='remove' type='button' " + "id='" + idstr + "remove' >" + removeName + "</button>";
     }
     if (moveUpName != null && i > 1) {
-    	moveUpButton = "<button class='moveup' type='button' " + "id='" + idstr + "moveup' >" 
+    	moveUpButton = "<button class='moveup' type='button' " + "id='" + idstr + "moveup' >"
     	+ "&uarr; " + moveUpName + "</button>";
-    } 
+    }
     if (moveDownName != null && i < LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]) {
     	moveDownButton = "<button class='movedown' type='button' " + "id='" + idstr + "movedown' >"
     	+ "&darr; " + moveDownName + "</button>";
     }
-    
+
     $("#vrtx-" + name + "-add")
       .before("<div class='vrtx-multipleinputfield' id='" + idstr + "row-" + i + "'>"
-      + "<input value='" + value + "' type='text' size='" + size + "' id='" + idstr + i + "'>" 
+      + "<input value='" + value + "' type='text' size='" + size + "' id='" + idstr + i + "'>"
       + removeButton + moveUpButton + moveDownButton + "</div>");
-    
+
     if(!init) {
     	if(LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] > 0) {
     	  var fields = "." + name + " div.inputfield div.vrtx-multipleinputfield";
           if($(fields).eq(LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] - 1).not("has:button.movedown")) {
-        	
+
             var theId = $(fields).eq(LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] - 1).attr("id");
-        	
+
         	moveDownButton = "<button class='movedown' type='button' " + "id='" + idstr + "movedown' >"
         	+ "&darr; " + moveDownName + "</button>";
 
         	$(fields).eq(LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] - 1).append(moveDownButton);
-        	logMultipleInputFields("Added before-last movedown");
+        	//logMultipleInputFields("Added before-last movedown");
           }
     	}
         LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]++;
       }
-    
+
     COUNTER_FOR_MULTIPLE_INPUT_FIELD[name]++;
 }
 
 function removeFormField(name, that) {
 
     $(that).parent().remove();
-    logMultipleInputFields("Fjerner felt");
-    
+    //logMultipleInputFields("Fjerner felt");
+
     LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]--;
     COUNTER_FOR_MULTIPLE_INPUT_FIELD[name]--;
-    logMultipleInputFields("Number of inputfields: " + LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]);
-    logMultipleInputFields("Next number for inputfield: " + COUNTER_FOR_MULTIPLE_INPUT_FIELD[name]);
-    
+    //logMultipleInputFields("Number of inputfields: " + LENGTH_FOR_MULTIPLE_INPUT_FIELD[name]);
+    //logMultipleInputFields("Next number for inputfield: " + COUNTER_FOR_MULTIPLE_INPUT_FIELD[name]);
+
     var fields = "." + name + " div.inputfield div.vrtx-multipleinputfield";
-    
+
 	if($(fields).eq(LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] - 1).has("button.movedown")) {
 	  $(fields).eq(LENGTH_FOR_MULTIPLE_INPUT_FIELD[name] - 1).find("button.movedown").remove();
-	  logMultipleInputFields("Removed last movedown");
+	  //logMultipleInputFields("Removed last movedown");
 	}
-			
+
 	if($(fields).eq(0).has("button.moveup")) {
 	  $(fields).eq(0).find("button.moveup").remove();
-	  logMultipleInputFields("Removed first moveup");
+	  //logMultipleInputFields("Removed first moveup");
 	}
 }
 
@@ -116,7 +117,7 @@ function moveUpFormField(that) {
   var prevText = prevInput.val();
   $(thisInput).val(prevText);
   $(prevInput).val(thisText);
-  logMultipleInputFields("Moved up " + thisText + " and swapped with " + prevText);
+  //logMultipleInputFields("Moved up " + thisText + " and swapped with " + prevText);
 }
 
 function moveDownFormField(that) {
@@ -126,17 +127,17 @@ function moveDownFormField(that) {
   var nextText = $(nextInput).val();
   $(thisInput).val(nextText);
   $(nextInput).val(thisText);
-  logMultipleInputFields("Moved down " + thisText + " and swapped with " + nextText);
+  //logMultipleInputFields("Moved down " + thisText + " and swapped with " + nextText);
 }
 
 function formatMultipleInputFields(name) {
     if ($( "#" + name ).val() == null)
-        return;
-    
+      return;
+
     var allFields = $.find("input[id^='vrtx-" + name + "']");
     var result = "";
     var allFieldsLength = allFields.length;
-    for (var i = 0; i < allFieldsLength; i++) {
+    for (var i = allFieldsLength; i--; ) {
         result += allFields[i].value;
         if (i < (allFieldsLength-1)) {
             result += ",";
@@ -146,9 +147,9 @@ function formatMultipleInputFields(name) {
 }
 
 function saveMultipleInputFields(){
-  var MULTIPLE_INPUT_FIELD_NAMES_LENGTH = MULTIPLE_INPUT_FIELD_NAMES.length;
-  for(var i = 0; i < MULTIPLE_INPUT_FIELD_NAMES_LENGTH; i++){
-    formatMultipleInputFields(MULTIPLE_INPUT_FIELD_NAMES[i]);
+  formatMultipleInputFieldsFunc = formatMultipleInputFields;
+  for(var i = MULTIPLE_INPUT_FIELD_NAMES.length; i--; ) {
+    formatMultipleInputFieldsFunc(MULTIPLE_INPUT_FIELD_NAMES[i]);
   }
 }
 

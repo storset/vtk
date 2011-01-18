396b52f3e6d23abd6b26f09eb5d920627f8cce47
Reza Mirzaei <rezam@usit.uio.no>
2011-01-18 10:25:21 +0000

    Add sql to remove property scientific disciplines.

--- src/main/sql/upgrades/0.7.1-oracle.sql
+++ src/main/sql/upgrades/0.7.1-oracle.sql
@@ -1,3 +1,6 @@
 
 delete from extra_prop_entry
   where name_space = 'http://www.uio.no/vrtx/__vrtx/ns/structured-resources' and name = 'employees';
+
+delete from extra_prop_entry
+  where name_space = 'http://www.uio.no/scientific' and name = 'disciplines';

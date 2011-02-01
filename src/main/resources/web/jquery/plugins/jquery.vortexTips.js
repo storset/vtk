1bb9f1987704debee04c792d06aac25eade4c0a9
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-02-01 12:51:54 +0000

    VTK-2048 - comment fix

--- src/main/resources/web/jquery/plugins/jquery.vortexTips.js
+++ src/main/resources/web/jquery/plugins/jquery.vortexTips.js
@@ -1,7 +1,7 @@
 ﻿/* 
  *  vortexTips plugin
  *  
- *  Based loosely tinyTips v1.1 by Mike Merritt (se license.txt)
+ *  Based loosely on tinyTips v1.1 by Mike Merritt (se license.txt)
  *  Modified by Øyvind Hatland (USIT)
  *  
  *  Changes

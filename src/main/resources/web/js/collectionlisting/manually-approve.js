564a2c7cabdf20a9a1e78695f374ffd7b38455fc
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-02-23 17:58:28 +0000

    VTK-2073 - add comments and increase readability of JS-code

--- src/main/resources/web/js/collectionlisting/manually-approve.js
+++ src/main/resources/web/js/collectionlisting/manually-approve.js
@@ -2,32 +2,121 @@
  * JS for handling manually approved resources
  */
 
-function toggleManuallyApprovedContainer(resources) {
-  // Toggle visibility of container for list of resources to manually approve.
-  // "resources" is complete list (array) of resources. If empty, hide
-  // container, else display. Use to mark resources.
+$(document).ready(function() {
 
-  // TODO: i18n ++
-  // TODO: refactor duplicate code
-	
-  //var startTime = new Date();
-	
+	// Retrieve initial resources
+    retrieveResources(".", null);
+
+    // Refresh when folders to approve from are changed
+    $("#manually-approve-refresh").click(function(e) {
+      var folders = $("#resource\\.manually-approve-from").val().split(",");
+      retrieveResources(".", folders);
+      return false; 
+    });
+    
+    // Add / remove uri's
+    $("#manually-approve-container").delegate("input", "click", function(e) {
+      var textfield = $("#resource\\.manually-approved-resources");
+      var val = textfield.val();
+      var uri = $(this).parent().parent().find("td.uri").text();
+      if($(this).attr("checked")) {
+    	if(val.length) {
+          val += ", " + uri;
+    	} else {
+    	  val = uri;	
+    	}
+      } else {
+    	if(val.indexOf(uri) == 0) { // not first
+    	  val = val.replace(uri, ""); 
+    	} else {
+    	  val = val.replace(", " + uri, ""); 
+    	}
+      }
+      textfield.val(val);
+    });
+
+    // Paging - next
+    $("#manually-approve-container").delegate(".next", "click", function() {
+      var that = $(this).parent();
+      var next = that.next();
+      if(next.attr("id") && next.attr("id").indexOf("approve-page") != -1) {
+         $(that).hide();
+         $(next).show();
+      }
+      return false;
+    });
+
+    // Paging - previous
+    $("#manually-approve-container").delegate(".prev", "click", function() {
+      var that = $(this).parent();
+      var prev = that.prev();
+      if(prev.attr("id") && prev.attr("id").indexOf("approve-page") != -1) {
+         $(that).hide();
+         $(prev).show();
+      }
+      return false;
+    });
+ });
+
+/**
+ * Retrieves resources as JSON array for folders to manually approve from
+ * 
+ * @param serviceUri as string
+ * @param folders as array
+ * 
+ */
+
+function retrieveResources(serviceUri, folders) {
+
+  var getUri = serviceUri + "/?vrtx=manually-approve-resources";
+  if(folders != null) {
+	for(var i = 0, len = folders.length; i < len; i++) {
+	  getUri += "&folders=" + $.trim(folders[i]);  
+	}
+  }
+
+  $.ajax({
+    url: getUri,
+	dataType: "json",
+	success: function(data){
+	  if(data != null && data.length > 0) {
+	    generateManuallyApprovedContainer(data);
+	  }
+	}
+  });
+  
+}
+
+/**
+ * Generate tables with resources
+ * 
+ * First page synchronous (if more than one page)
+ * Rest of pages asynchrounous adding each to DOM when complete
+ * 
+ * @param resources as JSON array
+ * 
+ * TODO: i18n
+ * 
+ */
+
+function generateManuallyApprovedContainer(resources) {
+
+  // Initial setup
   var pages = 1, prPage = 25, len = resources.length,
       remainder = len % prPage, moreThanOnePage = len > prPage,
       totalPages = len > prPage ? (parseInt(len / prPage) + 1) : 1;
-
-  var html = "<div id='approve-page-" + pages + "'>"
-           + "<table><thead><tr><th>Tittel</th><th>Uri</th><th>Publisert</th></thead></tr><tbody>";
-  
-  var i = 0;  
-  
+      
   // Function pointers
   var generateTableRowFunc = generateTableRow;
   var generateTableEndAndPageInfoFunc = generateTableEndAndPageInfo;
   var generateNavAndEndPageFunc = generateNavAndEndPage; 
   var generateStartPageAndTableHeadFunc = generateStartPageAndTableHead;
   
-  if(moreThanOnePage) { // If more than one page
+  var i = 0;
+
+  var html = generateStartPageAndTableHead(pages);
+  //If more than one page
+  if(moreThanOnePage) { 
     for(; i < prPage; i++) { // Generate first page synchronous
       html += generateTableRowFunc(resources[i]);
     }
@@ -40,10 +129,12 @@ function toggleManuallyApprovedContainer(resources) {
 	$("#manually-approve-container").html(""); // clear if only one page
   }
   
+  // Add spinner
   $("#manually-approve-container").prepend("<span id='approve-spinner'><img src='/vrtx/__vrtx/static-resources/themes/default/icons/tabmenu-spinner.gif' alt='spinner' />"
                                       + "&nbsp;Genererer side <span id='approve-spinner-generated-pages'>" + pages + "</span> av " + totalPages + "...</span>");
   
-  setTimeout(function() { // Generate rest of pages asynchronous
+  // Generate rest of pages asynchronous
+  setTimeout(function() { 
 	html += generateTableRowFunc(resources[i]);
     if((i+1) % prPage == 0) {
       html += generateTableEndAndPageInfoFunc(pages, prPage, len, false);
@@ -70,13 +161,13 @@ function toggleManuallyApprovedContainer(resources) {
       }
       html += "</div>";
       $("#manually-approve-container").append(html)
-        //.prepend("Tid: " + (new Date() - startTime) + "ms")
         .find("#approve-spinner").remove();
       if(len > prPage) { 
         $("#manually-approve-container #approve-page-" + pages).hide();
       }
     }
   }, 1);
+  
 }
 
 /* HTML generation functions */
@@ -114,80 +205,3 @@ function generateStartPageAndTableHead(pages) {
 }
 
 /* ^ HTML generation functions */
-
-function retrieveResources(serviceUri, folders) {
-  // Retrieve and return array of resources for folders to manually approve from.
-  // Needs Vortex-service.
-  
-  var getUri = serviceUri + "/?vrtx=manually-approve-resources";
-  if(folders != null) {
-	for(var i = 0, len = folders.length; i < len; i++) {
-	  getUri += "&folders=" + $.trim(folders[i]);  
-	}
-  }
-	
-  $.ajax({
-    url: getUri,
-	dataType: "json",
-	success: function(data){
-	  if(data != null && data.length > 0) {
-	    toggleManuallyApprovedContainer(data);
-	  }
-	}
-  });
-}
-
-$(document).ready(function() {
-
-    retrieveResources(".", null);
-
-    // Refresh table
-    $("#manually-approve-refresh").click(function(e) {
-      var folders = $("#resource\\.manually-approve-from").val().split(",");
-      retrieveResources(".", folders);
-      return false; 
-    });
-    
-    // Add / remove uri's
-    $("#manually-approve-container").delegate("input", "click", function(e) {
-      var textfield = $("#resource\\.manually-approved-resources");
-      var val = textfield.val();
-      var uri = $(this).parent().parent().find("td.uri").text();
-      if($(this).attr("checked")) {
-    	if(val.length) {
-          val += ", " + uri;
-    	} else {
-    	  val = uri;	
-    	}
-      } else {
-    	if(val.indexOf(uri) == 0) { // not first
-    	  val = val.replace(uri, ""); 
-    	} else {
-    	  val = val.replace(", " + uri, ""); 
-    	}
-      }
-      textfield.val(val);
-    });
-
-    // Paging - Next
-    $("#manually-approve-container").delegate(".next", "click", function() {
-      var that = $(this).parent();
-      var next = that.next();
-      if(next.attr("id") && next.attr("id").indexOf("approve-page") != -1) {
-         $(that).hide();
-         $(next).show();
-      }
-      return false;
-    });
-
-    // Paging - Previous
-    $("#manually-approve-container").delegate(".prev", "click", function() {
-      var that = $(this).parent();
-      var prev = that.prev();
-      if(prev.attr("id") && prev.attr("id").indexOf("approve-page") != -1) {
-         $(that).hide();
-         $(prev).show();
-      }
-      return false;
-    });
- });

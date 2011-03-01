bf8857dd11c1c35e67b7da088826f055cfdcc16b
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-03-01 10:50:54 +0000

    VTK-2065 - fix table - was out-of-sync with new CKEditor version

--- src/main/ext/CKEditor/ckeditor-build/_source/plugins/table/dialogs/table.js
+++ src/main/ext/CKEditor/ckeditor-build/_source/plugins/table/dialogs/table.js
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
@@ -167,7 +167,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 						{
 							var th = theRow.getChild( i );
 							// Skip bookmark nodes. (#6155)
-							if ( th.type == CKEDITOR.NODE_ELEMENT && !th.hasAttribute( '_cke_bookmark' ) )
+							if ( th.type == CKEDITOR.NODE_ELEMENT && !th.data( 'cke-bookmark' ) )
 							{
 								th.renameNode( 'th' );
 								th.setAttribute( 'scope', 'col' );
@@ -330,7 +330,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 										{
 											type : 'select',
 											id : 'selHeaders',
-											'default' : 'row',
+											'default' : '',
 											label : editor.lang.table.headers,
 											items :
 											[
@@ -387,13 +387,13 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 											id : 'cmbAlign',
 											type : 'select',
 											'default' : '',
-											label : editor.lang.table.align,
+											label : editor.lang.common.align,
 											items :
 											[
 												[ editor.lang.common.notSet , ''],
-												[ editor.lang.table.alignLeft , 'left'],
-												[ editor.lang.table.alignCenter , 'center'],
-												[ editor.lang.table.alignRight , 'right']
+												[ editor.lang.common.alignLeft , 'left'],
+												[ editor.lang.common.alignCenter , 'center'],
+												[ editor.lang.common.alignRight , 'right']
 											],
 											setup : function( selectedTable )
 											{
@@ -423,8 +423,8 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 													type : 'text',
 													id : 'txtWidth',
 													style : 'width:5em',
-													label : editor.lang.table.width,
-													'default' : 100,
+													label : editor.lang.common.width,
+													'default' : 500,
 													validate : CKEDITOR.dialog.validate['number']( editor.lang.table.invalidWidth ),
 
 													// Extra labelling of width unit type.
@@ -468,7 +468,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 													type : 'select',
 													label : editor.lang.table.widthUnit,
 													labelStyle: 'visibility:hidden',
-													'default' : 'percents',
+													'default' : 'pixels',
 													items :
 													[
 														[ editor.lang.table.widthPx , 'pixels'],
@@ -497,7 +497,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 													type : 'text',
 													id : 'txtHeight',
 													style : 'width:5em',
-													label : editor.lang.table.height,
+													label : editor.lang.common.height,
 													'default' : '',
 													validate : CKEDITOR.dialog.validate['number']( editor.lang.table.invalidHeight ),
 

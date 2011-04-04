c37dfe09fbea327da29cdc0dd5a849cb1abb9bc0
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-04-04 11:02:07 +0000

    VTK-2124 - simulate/test extract for READ

--- src/test/java/org/vortikal/web/actions/permissions/ACLEditControllerTest.java
+++ src/test/java/org/vortikal/web/actions/permissions/ACLEditControllerTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.vortikal.security.Principal;
 import org.vortikal.security.PrincipalFactory;
+import org.vortikal.security.Principal.Type;
 import org.vortikal.web.actions.copymove.CopyMoveToSelectedFolderController;
 
 import sun.security.acl.PrincipalImpl;
@@ -14,11 +15,13 @@ import junit.framework.TestCase;
 public class ACLEditControllerTest extends TestCase {
     
     private ACLEditController controller;
+    private PrincipalFactory principalFactory;
     
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         controller = new ACLEditController();
+        principalFactory = new PrincipalFactory();
     }
     
     public void testExtractAndCheckShortcuts() {
@@ -27,15 +30,22 @@ public class ACLEditControllerTest extends TestCase {
         List<Principal> authorizedGroups = new ArrayList<Principal>();
         List<String> shortcuts = new ArrayList<String>();
         
-        Principal group = PrincipalFactory.ALL;
-        authorizedUsers.add(group);
+        // ACLs
+        Principal userAll = PrincipalFactory.ALL;
+        authorizedUsers.add(userAll);
+        Principal groupAll = principalFactory.getPrincipal("alle@uio.no", Type.GROUP);
+        authorizedGroups.add(groupAll);
         
+        // Shortcuts
         shortcuts.add("user:pseudo:all");
+        shortcuts.add("group:alle@uio.no");
         
         String[][] extractedShortcuts = controller.extractAndCheckShortcuts(authorizedUsers, authorizedGroups, shortcuts);
         
         assertEquals("user:pseudo:all", extractedShortcuts[0][0]);
         assertEquals("checked", extractedShortcuts[0][1]);
+        assertEquals("group:alle@uio.no", extractedShortcuts[1][0]);
+        assertEquals("checked", extractedShortcuts[1][1]);
         
     }
 

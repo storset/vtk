4a4d234d58b517749ac8f2a7176f22a8a2b56b6c
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-04-14 17:39:48 +0000

    VTK-2124
    Assert that the rest of the shortcuts should be unchecked

--- src/test/java/org/vortikal/web/actions/permissions/ACLEditControllerTest.java
+++ src/test/java/org/vortikal/web/actions/permissions/ACLEditControllerTest.java
@@ -12,15 +12,15 @@ import org.vortikal.security.Principal;
 import org.vortikal.security.PrincipalFactory;
 
 public class ACLEditControllerTest extends TestCase {
-    
+
     private ACLEditController controller;
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         controller = new ACLEditController();
     }
-     
+
     @Test
     public void testExtractAndCheckShortcuts() {
 
@@ -28,11 +28,11 @@ public class ACLEditControllerTest extends TestCase {
         List<Principal> authorizedGroups = new ArrayList<Principal>();
         List<String> shortcuts = new ArrayList<String>();
         Map<String, List<String>> shortcutsConfig = new HashMap<String, List<String>>();
-        
+
         // ACLs (which shortcut should be checked)
         Principal userAll = PrincipalFactory.ALL;
         authorizedUsers.add(userAll);
-        
+
         // Shortcut for privilege (test with all)
         shortcuts.add("all");
         shortcuts.add("all-uio");
@@ -43,29 +43,32 @@ public class ACLEditControllerTest extends TestCase {
         List<String> groupsUsersAll = new ArrayList<String>();
         groupsUsersAll.add("user:pseudo:all");
         shortcutsConfig.put("all", groupsUsersAll);
-        
+
         List<String> groupsUsersAllUiO = new ArrayList<String>();
         groupsUsersAllUiO.add("group:alle@uio.no");
         shortcutsConfig.put("all-uio", groupsUsersAllUiO);
-        
+
         List<String> groupsUsersAllUniCollege = new ArrayList<String>();
         groupsUsersAllUniCollege.add("group:alle@uio.no");
         groupsUsersAllUniCollege.add("group:alle@feide.no");
         shortcutsConfig.put("all-uni-college", groupsUsersAllUniCollege);
-        
+
         List<String> groupsUsersAllLoggedIn = new ArrayList<String>();
         groupsUsersAllLoggedIn.add("group:alle@uio.no");
         groupsUsersAllLoggedIn.add("group:alle@feide.uio.no");
         groupsUsersAllLoggedIn.add("group:alle@webid.uio.no");
         shortcutsConfig.put("all-logged-in", groupsUsersAllLoggedIn );
-        
+
         int validShortcuts = controller.countValidshortcuts(shortcuts, shortcutsConfig);
         assertEquals(4, validShortcuts);
-        
+
         String[][] extractedShortcuts = controller.extractAndCheckShortcuts(authorizedGroups, authorizedUsers, validShortcuts, shortcuts, shortcutsConfig);
         assertEquals(4, extractedShortcuts.length);
-        
+
         assertEquals("checked", extractedShortcuts[0][1]);
+        assertEquals("", extractedShortcuts[1][1]);
+        assertEquals("", extractedShortcuts[2][1]);
+        assertEquals("", extractedShortcuts[3][1]);
 
     }
 

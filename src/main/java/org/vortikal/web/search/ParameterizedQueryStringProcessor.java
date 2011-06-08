556a75853eeabf9e70e3e84abb616f855bc1967a
Reza Mirzaei <rezam@usit.uio.no>
2011-06-08 11:05:07 +0000

    Header date 2010 -> 2011

--- src/main/java/org/vortikal/web/search/ParameterizedQueryStringProcessor.java
+++ src/main/java/org/vortikal/web/search/ParameterizedQueryStringProcessor.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010, University of Oslo, Norway
+/* Copyright (c) 2011, University of Oslo, Norway
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without

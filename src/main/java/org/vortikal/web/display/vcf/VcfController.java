79af8d5ae9c11c02ae4f8243af7d0c3b61f361fa
Adrian Jørgensen <adrianhj@usit.uio.no>
2011-07-04 14:09:31 +0000

    Vtk2313 - Added vCard for persons

---
+++ src/main/java/org/vortikal/web/display/vcf/VcfController.java
@@ -0,0 +1,212 @@
+/* Copyright (c) 2008, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.web.display.vcf;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import org.vortikal.repository.Namespace;
+import org.vortikal.repository.Path;
+import org.vortikal.repository.Property;
+import org.vortikal.repository.Repository;
+import org.vortikal.repository.Resource;
+import org.vortikal.repository.resourcetype.PropertyTypeDefinition;
+import org.vortikal.web.RequestContext;
+
+public class VcfController implements Controller  {
+    
+    private PropertyTypeDefinition firstNamePropDef;
+	private PropertyTypeDefinition surnamePropDef;
+	private PropertyTypeDefinition usernamePropDef;
+    private PropertyTypeDefinition positionPropDef;
+    private PropertyTypeDefinition phonePropDef;
+    private PropertyTypeDefinition mobilePropDef;
+    private PropertyTypeDefinition postalAddressPropDef;
+    private PropertyTypeDefinition visitingAddressPropDef;
+    private PropertyTypeDefinition emailPropDef;
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        RequestContext requestContext = RequestContext.getRequestContext();
+        Repository repository = requestContext.getRepository();
+        String token = requestContext.getSecurityToken();
+        Path uri = requestContext.getResourceURI();
+
+        Resource person = repository.retrieve(token, uri, true);
+        
+        String vcard = createVcard(person);
+        if (vcard == null) {
+            return null;
+        }
+        
+        response.setContentType("text/x-vcard;charset=utf-8");
+        String vcardFileName = getVcardFileName(person);
+        response.setHeader("Content-Disposition", "filename=" + vcardFileName + ".vcf");
+        ServletOutputStream out = response.getOutputStream();
+        out.print(vcard);
+        out.close();
+
+        return null;
+    }
+    
+    private String getVcardFileName(Resource person) {
+        String name = person.getName();
+        if (name.contains(".")) {
+            name = name.substring(0, name.indexOf("."));
+        }
+        return name;
+    }
+
+    private String createVcard(Resource person) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("BEGIN:VCARD\n");
+        sb.append("VERSION:3.0\n");
+        
+        if(getProperty(person, surnamePropDef) != null) {
+        	sb.append("N:" + getProp(person, surnamePropDef));
+        	if(getProperty(person, firstNamePropDef) != null)
+        		sb.append(";" + getProp(person, firstNamePropDef));
+        	sb.append("\n");
+        }
+        else if(getProperty(person, firstNamePropDef) != null)
+        		sb.append("N:" + getProp(person, firstNamePropDef) + "\n");
+        else if(getProperty(person, usernamePropDef) != null)
+        	sb.append("N:" + getProp(person, usernamePropDef) + "\n");
+        
+        if(getProperty(person, firstNamePropDef) != null) {
+        	sb.append("FN:" + getProp(person, firstNamePropDef));
+        	if(getProperty(person, surnamePropDef) != null)
+        		sb.append(" " + getProp(person, surnamePropDef));
+        	sb.append("\n");
+        }
+        else if(getProperty(person, surnamePropDef) != null)
+        		sb.append("FN:" + getProp(person, surnamePropDef) + "\n");
+        else if(getProperty(person, usernamePropDef) != null)
+        	sb.append("FN:" + getProp(person, usernamePropDef) + "\n");
+        
+        if(getProperty(person, positionPropDef) != null)
+        	sb.append("TITLE:" + getProp(person, positionPropDef) + "\n");
+        
+        if(getProperty(person, phonePropDef) != null)
+        	sb.append("TEL;TYPE=WORK,VOICE:" + getProp(person, phonePropDef) + "\n");
+        	
+        if(getProperty(person, mobilePropDef) != null)	
+        	sb.append("TEL;TYPE=CELL:" + getProp(person, mobilePropDef) + "\n");
+        
+        /* For data input reasons addresses has to be put in the street address field.
+         * ADR;TYPE=WORK:PO;Address Line 1;Address Line 2;City;Province;PostalCode;Country 
+         * could however be used if this should change in the future. */
+        if(getProperty(person, postalAddressPropDef) != null)
+            sb.append("ADR;TYPE=WORK,POSTAL:;;" + getProp(person, postalAddressPropDef) + ";;;;\n");
+        
+        if(getProperty(person, visitingAddressPropDef) != null)
+            sb.append("ADR;TYPE=WORK:;;" + getProp(person, visitingAddressPropDef) + ";;;;\n");
+        
+        if(getProperty(person, emailPropDef) != null)
+        	sb.append("EMAIL;TYPE=INTERNET:" + getProp(person, emailPropDef) + "\n");
+        
+        sb.append("REV:" + getDtstamp() + "\n");
+        sb.append("END:VCARD");
+        return sb.toString();
+    }
+
+    private String getProp(Resource person, PropertyTypeDefinition propDef) {
+    	return getProperty(person, propDef).getFormattedValue();
+    }
+    
+    private Property getProperty(Resource person, PropertyTypeDefinition propDef) {
+    	Property prop = person.getProperty(propDef);
+    	if (prop == null) {
+            prop = person.getProperty(Namespace.STRUCTURED_RESOURCE_NAMESPACE, propDef.getName());
+        }
+    	
+        return prop;
+    }
+
+    private String getDtstamp() {
+        String dateFormat = "yyyyMMdd'T'HHmmss'Z'";
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        return sdf.format(Calendar.getInstance().getTime());
+    }
+
+    @Required
+    public void setFirstNamePropDef(PropertyTypeDefinition firstNamePropDef) {
+        this.firstNamePropDef = firstNamePropDef;
+    }
+
+    @Required
+    public void setSurnamePropDef(PropertyTypeDefinition surnamePropDef) {
+		this.surnamePropDef = surnamePropDef;
+	}
+
+    @Required
+	public void setUsernamePropDef(PropertyTypeDefinition usernamePropDef) {
+		this.usernamePropDef = usernamePropDef;
+	}
+
+    @Required
+	public void setPositionPropDef(PropertyTypeDefinition positionPropDef) {
+		this.positionPropDef = positionPropDef;
+	}
+
+    @Required
+	public void setPhonePropDef(PropertyTypeDefinition phonePropDef) {
+		this.phonePropDef = phonePropDef;
+	}
+
+    @Required
+	public void setMobilePropDef(PropertyTypeDefinition mobilePropDef) {
+		this.mobilePropDef = mobilePropDef;
+	}
+
+    @Required
+	public void setPostalAddressPropDef(PropertyTypeDefinition postalAddressPropDef) {
+		this.postalAddressPropDef = postalAddressPropDef;
+	}
+
+    @Required
+	public void setVisitingAddressPropDef(PropertyTypeDefinition visitingAddressPropDef) {
+		this.visitingAddressPropDef = visitingAddressPropDef;
+	}
+
+    @Required
+	public void setEmailPropDef(PropertyTypeDefinition emailPropDef) {
+		this.emailPropDef = emailPropDef;
+	}
+}

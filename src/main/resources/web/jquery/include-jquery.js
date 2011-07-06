17c14883ea6570e1ea342ab5e871b9a02e71849d
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-07-06 22:20:05 +0000

    VTK
    Check that "undefined" is an object

--- src/main/resources/web/jquery/include-jquery.js
+++ src/main/resources/web/jquery/include-jquery.js
@@ -1,3 +1,3 @@
-if (typeof jQuery == 'undefined') {
+if (typeof jQuery === 'undefined') {
   document.write("<scr" + "ipt src='/vrtx/__vrtx/static-resources/jquery/jquery-1.5.1.min.js' type='text/javascript'><\/script>");
 }

d021f8627ac678b7975e8ea78b783a9b355898ae
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-07-07 14:52:42 +0000

    VTK-2327 - upgrade jQuery from 1.5.1 to 1.6.2

--- src/main/resources/web/jquery/include-jquery.js
+++ src/main/resources/web/jquery/include-jquery.js
@@ -1,3 +1,3 @@
 if (typeof jQuery === 'undefined') {
-  document.write("<scr" + "ipt src='/vrtx/__vrtx/static-resources/jquery/jquery-1.5.1.min.js' type='text/javascript'><\/script>");
+  document.write("<scr" + "ipt src='/vrtx/__vrtx/static-resources/jquery/jquery-1.6.2.min.js' type='text/javascript'><\/script>");
 }

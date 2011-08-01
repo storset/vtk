069536228abd2ca3e229738531e0736cedc57c38
Gorm Paulsen <gormap@usit.uio.no>
2011-08-01 13:10:08 +0000

    [VTK-2338] InputStreamWrapper now extends InputStream to avoid breaking existing code.

--- src/main/java/org/vortikal/repository/content/InputStreamWrapper.java
+++ src/main/java/org/vortikal/repository/content/InputStreamWrapper.java
@@ -30,11 +30,12 @@
  */
 package org.vortikal.repository.content;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.vortikal.repository.Path;
 
-public class InputStreamWrapper {
+public class InputStreamWrapper extends InputStream {
     private InputStream inputStream;
     private Path path;
 
@@ -55,4 +56,48 @@ public class InputStreamWrapper {
         return path;
     }
 
+    @Override
+    public int read() throws IOException {
+        return this.inputStream.read();
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return this.inputStream.read(b);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return this.inputStream.read(b, off, len);
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        return this.inputStream.skip(n);
+    }
+
+    @Override
+    public int available() throws IOException {
+        return this.inputStream.available();
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.inputStream.close();
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        this.inputStream.mark(readlimit);
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        this.inputStream.reset();
+    }
+
+    @Override
+    public boolean markSupported() {
+        return this.inputStream.markSupported();
+    }
 }

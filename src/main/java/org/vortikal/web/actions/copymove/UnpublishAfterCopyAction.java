b33db1a901f74a055675cbe198a94f920f2a3d9b
Gorm Paulsen <gormap@usit.uio.no>
2011-08-23 13:28:13 +0000

    [VTK-2365] Unpublish "structured resources" after copying

---
+++ src/main/java/org/vortikal/web/actions/copymove/UnpublishAfterCopyAction.java
@@ -0,0 +1,67 @@
+/* Copyright (c) 2011, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.web.actions.copymove;
+
+import java.util.Map;
+
+import org.vortikal.repository.Namespace;
+import org.vortikal.repository.Path;
+import org.vortikal.repository.Repository;
+import org.vortikal.repository.Repository.Depth;
+import org.vortikal.repository.Resource;
+import org.vortikal.repository.TypeInfo;
+import org.vortikal.web.RequestContext;
+import org.vortikal.web.actions.convert.CopyAction;
+
+/**
+ * CopyAction that performs an un-publish operation on the 
+ * resource after copying.
+ */
+public class UnpublishAfterCopyAction implements CopyAction {
+
+    @Override
+    public void process(Path originalUri, Path copyUri,
+            Map<String, Object> properties) throws Exception {
+        
+        RequestContext requestContext = RequestContext.getRequestContext();
+        Repository repository = requestContext.getRepository();
+        String token = requestContext.getSecurityToken();
+        repository.copy(token, originalUri, copyUri, Depth.INF, false, false);
+        Resource result = repository.retrieve(token, copyUri, true);
+
+        TypeInfo typeInfo = repository.getTypeInfo(result);
+        if (typeInfo.isOfType("managed-json-resource")) {
+            result.removeProperty(Namespace.DEFAULT_NAMESPACE, "publish-date");
+            repository.store(token, result);
+        }
+    }
+
+}

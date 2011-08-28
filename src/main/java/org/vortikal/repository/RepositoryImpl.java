a310754be359d279376d1a0db6be9281be4861c7
Øyvind Stegard <oyviste@usit.uio.no>
2011-08-28 18:20:44 +0000

    Class TrashCanPurger didn't do anything other can calling Repository#purgeTrash(). Wire trigger bean to call repository directly instead.

--- src/main/java/org/vortikal/repository/RepositoryImpl.java
+++ src/main/java/org/vortikal/repository/RepositoryImpl.java
@@ -1227,7 +1227,7 @@ public class RepositoryImpl implements Repository, ApplicationContextAware {
     }
 
     @Override
-    public void purgeTrash() {
+    public synchronized void purgeTrash() {
         List<RecoverableResource> overdue = this.dao.getTrashCanOverdue(this.permanentDeleteOverdueLimitInDays);
         if (overdue != null && overdue.size() > 0) {
             trashLogger.info("Found " + overdue.size()

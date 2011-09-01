ae08780c9f12ca131d859509f6ad4aed95cc88db
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-09-01 12:29:22 +0000

    VTK - tree AJAX formatting (2 spaces indent)

--- src/main/resources/web/jquery/plugins/jquery.treeview.async.js
+++ src/main/resources/web/jquery/plugins/jquery.treeview.async.js
@@ -17,106 +17,106 @@
  *                  4. update settings.url on toggle()
  *                  5. service
  *
- * USIT removed commented out method
+ * USIT removed commented out method and set indent to 2 spaces
  *
  */
 
-;(function($) {
+;
+(function ($) {
 
-function load(settings, root, child, container) {
-	function createNode(parent) {
-        var linkOrPlainText = "";
-        if(this.uri) {
-          if(this.title) {
-            var theuri = this.uri;
-            linkOrPlainText = "<a class='tree-link' href='" + theuri
-                            + "' title='" + this.title + "'>" 
-                            + this.text + "</a>"
-          } else {
-            linkOrPlainText = "<a href='" + this.uri + "'>" 
-                            + this.text + "</a>"
-          }
+  function load(settings, root, child, container) {
+    function createNode(parent) {
+      var linkOrPlainText = "";
+      if (this.uri) {
+        if (this.title) {
+          var theuri = this.uri;
+          linkOrPlainText = "<a class='tree-link' href='" + theuri + "' title='" + this.title + "'>" + this.text + "</a>"
         } else {
-          linkOrPlainText = this.text;
+          linkOrPlainText = "<a href='" + this.uri + "'>" + this.text + "</a>"
         }
-		var current = $("<li/>").attr("id", this.id || "")
-                      .html("<span>" + linkOrPlainText + "</span>").appendTo(parent);
+      } else {
+        linkOrPlainText = this.text;
+      }
+      var current = $("<li/>").attr("id", this.id || "").html("<span>" + linkOrPlainText + "</span>").appendTo(parent);
 
-        if (this.listClasses) current.addClass(this.listClasses);
-		if (this.spanClasses) current.children("span").addClass(this.spanClasses);
-		if (this.expanded) current.addClass("open");
-		
-		if (this.hasChildren || this.children && this.children.length) {
-			var branch = $("<ul/>").appendTo(current);
-			if (this.hasChildren) {
-				current.addClass("hasChildren");
-				createNode.call({
-					classes: "placeholder",
-					text: "&nbsp;",
-					children:[]
-				}, branch);
-			}
-			if (this.children && this.children.length) {
-			  $.each(this.children, createNode, [branch])
-			}
-		}
-	}
-	$.ajax($.extend(true, {
-		url: settings.url,
-		dataType: "json",
-		data: { root: root },
-		success: function(response) {
-			child.empty();
-			$.each(response, createNode, [child]);
-	        $(container).treeview({add: child});
-	    },
-	    error: function(jqXHR, textStatus, errorThrown) {
-	      if(typeof console !== "undefined" && console.log) {
-            console.log(textStatus);
-            if(console.dir) {
-              console.dir(jqXHR);
-            }
+      if (this.listClasses) current.addClass(this.listClasses);
+      if (this.spanClasses) current.children("span").addClass(this.spanClasses);
+      if (this.expanded) current.addClass("open");
+
+      if (this.hasChildren || this.children && this.children.length) {
+        var branch = $("<ul/>").appendTo(current);
+        if (this.hasChildren) {
+          current.addClass("hasChildren");
+          createNode.call({
+            classes: "placeholder",
+            text: "&nbsp;",
+            children: []
+          }, branch);
+        }
+        if (this.children && this.children.length) {
+          $.each(this.children, createNode, [branch])
+        }
+      }
+    }
+    $.ajax($.extend(true, {
+      url: settings.url,
+      dataType: "json",
+      data: {
+        root: root
+      },
+      success: function (response) {
+        child.empty();
+        $.each(response, createNode, [child]);
+        $(container).treeview({
+          add: child
+        });
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        if (typeof console !== "undefined" && console.log) {
+          console.log(textStatus);
+          if (console.dir) {
+            console.dir(jqXHR);
           }
         }
-	}, settings.ajax));
-}
+      }
+    }, settings.ajax));
+  }
 
-var proxied = $.fn.treeview;
-$.fn.treeview = function(settings) {
-	if (!settings.url) {
-		return proxied.apply(this, arguments);
-	}
-	var container = this;
-	if (!container.children().size())
-		load(settings, "source", this, container);
-	var userToggle = settings.toggle;
-	return proxied.call(this, $.extend({}, settings, {
-		collapsed: true,
-		toggle: function() {
-			var $this = $(this);
-			if ($this.hasClass("hasChildren")) {
-				var subFolder = $(this).find("a").attr("href").split("?")[0];
-				if($.browser.msie && $.browser.version <= 7) { // dirty but only way to fix broken IE 7 DOM
-				   var uioCutpoint = "uio.no/";
-				   var devCutpoint = ":9322/";
-				   var uioEnd = window.location.href.indexOf(uioCutpoint) + uioCutpoint.length;
-				   uioEnd = uioEnd > uioCutpoint.length ? uioEnd : window.location.href.indexOf(devCutpoint) + devCutpoint.length;
-				   var base = window.location.href.substring(0, uioEnd);
-				   subFolder = "/" + subFolder.replace(base, "");
-				}
-				var timestamp = 1 - new Date();
-		        var ajaxUrl = {
-		  		  url: "?vrtx=admin&service=" + settings.service + "&uri=" + subFolder + "&ts=" + timestamp
-		  		};
-				$.extend(settings, ajaxUrl);
-				var childList = $this.removeClass("hasChildren").find("ul");
-				load(settings, this.id, childList, container);
-			}
-			if (userToggle) {
-				userToggle.apply(this, arguments);
-			}
-		}
-	}));
-};
+  var proxied = $.fn.treeview;
+  $.fn.treeview = function (settings) {
+    if (!settings.url) {
+      return proxied.apply(this, arguments);
+    }
+    var container = this;
+    if (!container.children().size()) load(settings, "source", this, container);
+    var userToggle = settings.toggle;
+    return proxied.call(this, $.extend({}, settings, {
+      collapsed: true,
+      toggle: function () {
+        var $this = $(this);
+        if ($this.hasClass("hasChildren")) {
+          var subFolder = $(this).find("a").attr("href").split("?")[0];
+          if ($.browser.msie && $.browser.version <= 7) { // dirty but only way to fix broken IE 7 DOM
+            var uioCutpoint = "uio.no/";
+            var devCutpoint = ":9322/";
+            var uioEnd = window.location.href.indexOf(uioCutpoint) + uioCutpoint.length;
+            uioEnd = uioEnd > uioCutpoint.length ? uioEnd : window.location.href.indexOf(devCutpoint) + devCutpoint.length;
+            var base = window.location.href.substring(0, uioEnd);
+            subFolder = "/" + subFolder.replace(base, "");
+          }
+          var timestamp = 1 - new Date();
+          var ajaxUrl = {
+            url: "?vrtx=admin&service=" + settings.service + "&uri=" + subFolder + "&ts=" + timestamp
+          };
+          $.extend(settings, ajaxUrl);
+          var childList = $this.removeClass("hasChildren").find("ul");
+          load(settings, this.id, childList, container);
+        }
+        if (userToggle) {
+          userToggle.apply(this, arguments);
+        }
+      }
+    }));
+  };
 
 })(jQuery);

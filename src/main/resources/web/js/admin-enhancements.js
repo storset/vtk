43ae9eb4eb7940a559df31bd5987e4008f83c61b
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-09-01 12:55:56 +0000

    VTK - another todo (that can wait)

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -4,6 +4,7 @@
  *  TODO: i18n AJAX error messages
  *  TODO: prototypes vs. static (memory vs. speed?) -- it seem to be boiling down to using at the right time and test alot:
  *        http://stackoverflow.com/questions/3493252/javascript-prototype-operator-performance-saves-memory-but-is-it-faster
+ *  TODO: CPU usage in ready() vs. wait for it in load()
  *
  */
  

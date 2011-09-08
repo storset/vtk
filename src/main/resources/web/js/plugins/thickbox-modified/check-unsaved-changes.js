08c2f5315522fbb8f98fe2706b1dece691ffa5e1
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-09-08 20:12:11 +0000

    VTK - formatting

--- src/main/resources/web/js/plugins/thickbox-modified/check-unsaved-changes.js
+++ src/main/resources/web/js/plugins/thickbox-modified/check-unsaved-changes.js
@@ -1,30 +1,30 @@
-//loading thickbox message with check for unsaved changes in editor 
-$(document).ready(function(){   
-    tb_init_check_props('a.thickbox-check-unsaved-changes, area.thickbox-check-unsaved-changes, input.thickbox-check-unsaved-changes');//pass where to apply thickbox
+//loading thickbox message with check for unsaved changes in editor
+$(document).ready(function () {
+  tb_init_check_props('a.thickbox-check-unsaved-changes, area.thickbox-check-unsaved-changes, input.thickbox-check-unsaved-changes'); //pass where to apply thickbox
 });
 
-function tb_init_check_props(domChunk){
-    $(domChunk).click(function(){
+function tb_init_check_props(domChunk) {
+  $(domChunk).click(function () {
     var t = this.title || this.name || null;
     var a = this.href || this.alt;
     var g = this.rel || false;
-    if(typeof(NEED_TO_CONFIRM) != "undefined"){
-        tb_show_check_props(t,a,g);    
-    }else{
-        tb_show(t,a,g);
+    if (typeof (NEED_TO_CONFIRM) != "undefined") {
+      tb_show_check_props(t, a, g);
+    } else {
+      tb_show(t, a, g);
     }
     this.blur();
     return false;
-    });
+  });
 }
 
-function tb_show_check_props(caption, url, imageGroup){
-    var checkForChanges = unsavedChangesInEditor();
-    if(checkForChanges != undefined){
-        if(!confirm(COMPLETE_UNSAVED_CHANGES_CONFIRMATION)){
-            return false;
-        }
-        NEED_TO_CONFIRM = false;
+function tb_show_check_props(caption, url, imageGroup) {
+  var checkForChanges = unsavedChangesInEditor();
+  if (checkForChanges != undefined) {
+    if (!confirm(COMPLETE_UNSAVED_CHANGES_CONFIRMATION)) {
+      return false;
     }
-    tb_show(caption, url, imageGroup);
+    NEED_TO_CONFIRM = false;
+  }
+  tb_show(caption, url, imageGroup);
 }

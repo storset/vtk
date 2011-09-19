1b3891fb2c8c708a02e265caee96710b59bcf612
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-09-19 11:13:54 +0000

    VTK - ...

--- src/main/resources/web/js/iframe-view.js
+++ src/main/resources/web/js/iframe-view.js
@@ -9,11 +9,13 @@
  *    http://benalman.com/code/projects/jquery-postmessage/examples/iframe/
  *    see src: https://raw.github.com/cowboy/jquery-postmessage/master/jquery.ba-postmessage.js
  */
+ 
+var runned = false;
+ 
 $(document).ready(function () {
   var hasPostMessage = window['postMessage'] && (!($.browser.opera && $.browser.version < 9.65));
   var vrtxAdminOrigin = "*"; // TODO: TEMP Need real origin of adm
-  
-  if ($.browser.msie && $.browser.version > 7) {
+  if ($.browser.msie) {
     // iframe load event not firing in IE8 when page w. iframe is inside another iframe
     // Setting the iframe src seems to fix the problem
     var previewViewIframe = $("iframe#previewViewIframe")[0];
@@ -24,27 +26,30 @@ $(document).ready(function () {
     }
   }
   $('iframe#previewViewIframe').load(function (e) {
-    try {
-      // Set inline style to equal the body height of the iframed content,
-      // when body content is at least 350px height
-      var setHeight = 350;
-      var computedHeight = this.contentWindow.document.body.offsetHeight + 45;
-      if (computedHeight > setHeight) {
-        setHeight = computedHeight;
-      }
-      this.style.height = setHeight + "px";
-      if (parent) {
-        // Pass our height to parent since it is typically cross domain (and can't access it directly)
-        if(hasPostMessage) {
-          parent.postMessage(setHeight, vrtxAdminOrigin);
-        } else { // use the hash stuff in plugin from jQuery "Cowboy"
-          var parent_url = decodeURIComponent(document.location.hash.replace(/^#/,''));
-          $.postMessage({height: setHeight}, parent_url, parent);        
+    if(!runned) {
+      runned = true;
+      try {
+        // Set inline style to equal the body height of the iframed content,
+        // when body content is at least 350px height
+        var setHeight = 350;
+        var computedHeight = this.contentWindow.document.body.offsetHeight + 45;
+        if (computedHeight > setHeight) {
+          setHeight = computedHeight;
         }
-      }
-    } catch(e){
-      if(typeof console !== "undefined" && console.log) {
-        console.log("Error in getting iframe height or trying to post it to parent: " + e.message);
+        this.style.height = setHeight + "px";
+        if (parent) {
+          // Pass our height to parent since it is typically cross domain (and can't access it directly)
+          if(hasPostMessage) {
+            parent.postMessage(setHeight, vrtxAdminOrigin);
+          } else { // use the hash stuff in plugin from jQuery "Cowboy"
+            var parent_url = decodeURIComponent(document.location.hash.replace(/^#/,''));
+            $.postMessage({height: setHeight}, parent_url, parent);        
+          }
+        }
+      } catch(e){
+        if(typeof console !== "undefined" && console.log) {
+          console.log("Error in getting iframe height or trying to post it to parent: " + e.message);
+        }
       }
     }
   });

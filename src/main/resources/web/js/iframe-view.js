ad17773e01f0b2b01bffae0c5b22abf02f50648d
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-09-19 11:27:35 +0000

    VTK - try with timer when IE 7

--- src/main/resources/web/js/iframe-view.js
+++ src/main/resources/web/js/iframe-view.js
@@ -10,47 +10,60 @@
  *    see src: https://raw.github.com/cowboy/jquery-postmessage/master/jquery.ba-postmessage.js
  */
  
-var runned = false;
- 
 $(document).ready(function () {
   var hasPostMessage = window['postMessage'] && (!($.browser.opera && $.browser.version < 9.65));
   var vrtxAdminOrigin = "*"; // TODO: TEMP Need real origin of adm
   if ($.browser.msie) {
-    // iframe load event not firing in IE8 when page w. iframe is inside another iframe
-    // Setting the iframe src seems to fix the problem
-    var previewViewIframe = $("iframe#previewViewIframe")[0];
-    if (previewViewIframe) {
-      var iSource = previewViewIframe.src;
-      previewViewIframe.src = '';
-      previewViewIframe.src = iSource; 
+    if($.browser.version > 7) {
+      // iframe load event not firing in IE8 when page w. iframe is inside another iframe
+      // Setting the iframe src seems to fix the problem
+      var previewViewIframe = $("iframe#previewViewIframe")[0];
+      if (previewViewIframe) {
+        var iSource = previewViewIframe.src;
+        previewViewIframe.src = '';
+        previewViewIframe.src = iSource; 
+      } 
+      $('iframe#previewViewIframe').load(function (e) {
+        resize(this); 
+      });
+    } else {
+      var previewIframeReady = setInterval(function() {
+        var offsetHeight = $('iframe#previewViewIframe')[0].contentWindow.document.body.offsetHeight;
+        if(typeof offsetHeight !== "undefined" && offsetHeight > 0) {
+          clearInterval(previewIframeReady);
+          resize($('iframe#previewViewIframe'));
+        }
+      }, 25);
     }
+  } else {
+    $('iframe#previewViewIframe').load(function (e) {
+      resize(this); 
+    });
   }
-  $('iframe#previewViewIframe').load(function (e) {
-    if(!runned) {
-      runned = true;
-      try {
-        // Set inline style to equal the body height of the iframed content,
-        // when body content is at least 350px height
-        var setHeight = 350;
-        var computedHeight = this.contentWindow.document.body.offsetHeight + 45;
-        if (computedHeight > setHeight) {
-          setHeight = computedHeight;
-        }
-        this.style.height = setHeight + "px";
-        if (parent) {
-          // Pass our height to parent since it is typically cross domain (and can't access it directly)
-          if(hasPostMessage) {
-            parent.postMessage(setHeight, vrtxAdminOrigin);
-          } else { // use the hash stuff in plugin from jQuery "Cowboy"
-            var parent_url = decodeURIComponent(document.location.hash.replace(/^#/,''));
-            $.postMessage({height: setHeight}, parent_url, parent);        
-          }
-        }
-      } catch(e){
-        if(typeof console !== "undefined" && console.log) {
-          console.log("Error in getting iframe height or trying to post it to parent: " + e.message);
-        }
+});
+
+function resize(iframe) {
+  try {
+    // Set inline style to equal the body height of the iframed content,
+    // when body content is at least 350px height
+    var setHeight = 350;
+    var computedHeight = iframe.contentWindow.document.body.offsetHeight + 45;
+    if (computedHeight > setHeight) {
+      setHeight = computedHeight;
+    }
+    iframe.style.height = setHeight + "px";
+    if (parent) {
+      // Pass our height to parent since it is typically cross domain (and can't access it directly)
+      if(hasPostMessage) {
+        parent.postMessage(setHeight, vrtxAdminOrigin);
+      } else { // use the hash stuff in plugin from jQuery "Cowboy"
+        var parent_url = decodeURIComponent(document.location.hash.replace(/^#/,''));
+        $.postMessage({height: setHeight}, parent_url, parent);        
       }
     }
-  });
-});
+  } catch(e){
+    if(typeof console !== "undefined" && console.log) {
+      console.log("Error in getting iframe height or trying to post it to parent: " + e.message);
+    }
+  }
+}

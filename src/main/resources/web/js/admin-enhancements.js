d94e81fa1c4ee599f0e813714ff8a85405eda054
Øyvind Hatland <oyvihatl@usit.uio.no>
2011-09-20 11:20:44 +0000

    VTK - english ie 6 msg

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -417,7 +417,7 @@ $(document).ready(function () {
 
   // Fix IE 6 collectionlisting hover
   if (vrtxAdmin.isIE6) {
-    var ie6Msg = "IE 6 er ikke støttet. Oppgrader nettleseren.";
+    var ie6Msg = "You are using an outdated and not supported version of Internet Explorer. Please go to Windows Updates and install the latest version.";
     if ($("#app-content > .message").length) {
       $("#app-content > .message").html(ie6Msg);
     } else {

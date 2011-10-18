b1d25a0a319091031e5e15cb287d6fcf618bda1d
David Egeland <dave@usit.uio.no>
2011-10-18 08:45:28 +0000

    Fixed query string bug in new SSO code.

--- src/main/java/org/vortikal/security/web/SecurityInitializer.java
+++ src/main/java/org/vortikal/security/web/SecurityInitializer.java
@@ -205,9 +205,12 @@ public class SecurityInitializer implements InitializingBean, ApplicationContext
         } else if (getCookie(req, UIO_AUTH_SSO) != null && getCookie(req, VRTXLINK_COOKIE) == null
                 && req.getParameter("authTarget") == null && !req.getRequestURI().contains(serviceProviderURI)) {
             StringBuffer url = req.getRequestURL();
-            if (req.getQueryString() != null) {
-                url = url.append(req.getQueryString());
+            String queryString = req.getQueryString();
+            if (queryString != null) {
+                url = url.append("?");
+                url = url.append(queryString);
             }
+
             URL currentURL = URL.parse(url.toString());
             currentURL.addParameter("authTarget", req.getScheme());
             resp.sendRedirect(currentURL.toString());

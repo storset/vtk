62f88ef95d279fef7d0470031feba8180ce7b3c2
Magnus Korvald <korvald@usit.uio.no>
2011-12-06 12:49:12 +0000

    grant access to revision tables to vrtx user

--- src/main/sql/grant_to_vrtx.sql
+++ src/main/sql/grant_to_vrtx.sql
@@ -1,4 +1,5 @@
-
+grant delete, select, insert, update on SIMPLE_CONTENT_REVISION to vrtx;
+grant delete, select, insert, update on REVISION_ACL_ENTRY to vrtx;
 grant delete, select, insert, update on ACL_ENTRY to vrtx;                      
 grant delete, select, insert, update on ACTION_TYPE to vrtx;                    
 grant delete, select, insert, update on EXTRA_PROP_ENTRY to vrtx;               

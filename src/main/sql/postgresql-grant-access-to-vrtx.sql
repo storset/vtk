db2df634d3fc28dbbda86ed2daa4105c50b824b4
Gorm Paulsen <gormap@usit.uio.no>
2011-12-19 11:02:39 +0000

    Add missing grants

--- src/main/sql/postgresql-grant-access-to-vrtx.sql
+++ src/main/sql/postgresql-grant-access-to-vrtx.sql
@@ -14,8 +14,8 @@ dbname=> \i /Users/evenh/vortikal/trunk/src/sql/postgresql-schema.sql
 * Change 'vrtx' to <jdbcUsername> in the following grant statement if they differ.
 */
 
-GRANT DELETE, SELECT, INSERT, UPDATE ON acl_entry, action_type, changelog_entry, extra_prop_entry, prop_type, vortex_lock, vortex_resource, vortex_tmp, vortex_comment, deleted_resource TO vrtx;
-GRANT SELECT, UPDATE ON acl_entry_seq_pk, changelog_entry_seq_pk, extra_prop_entry_seq_pk, vortex_lock_seq_pk, vortex_resource_seq_pk, vortex_tmp_session_id_seq, vortex_comment_seq_pk, deleted_resource_seq_pk TO vrtx;
+GRANT DELETE, SELECT, INSERT, UPDATE ON acl_entry, action_type, changelog_entry, extra_prop_entry, prop_type, vortex_lock, vortex_resource, vortex_tmp, vortex_comment, deleted_resource, simple_content_revision, revision_acl_entry TO vrtx;
+GRANT SELECT, UPDATE ON acl_entry_seq_pk, changelog_entry_seq_pk, extra_prop_entry_seq_pk, vortex_lock_seq_pk, vortex_resource_seq_pk, vortex_tmp_session_id_seq, vortex_comment_seq_pk, deleted_resource_seq_pk, simple_content_revision_seq_pk, revision_acl_entry_seq_pk TO vrtx;
 
 /*
 dbname=> \z (show access privileges for database "dbname")

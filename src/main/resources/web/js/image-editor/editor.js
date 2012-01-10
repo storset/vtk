1fb7c9a428ba7ab8b651f5160155201bf1c82447
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-01-10 14:33:05 +0000

    VTK-2668 - use Web Workers for calculating Lanczos3 for downscaling. TODO: degrade to setTimeout if Web Workers are not supported

--- src/main/resources/web/js/image-editor/editor.js
+++ src/main/resources/web/js/image-editor/editor.js
@@ -294,8 +294,8 @@ VrtxImageEditor.prototype.renderRestorePoint = function renderRestorePoint() {
 
 /* Thumbnailer / Lanczos algorithm for downscaling
  * Credits: http://stackoverflow.com/questions/2303690/resizing-an-image-in-an-html5-canvas
- * TODO: optimize and use Web Workers if available
- * Modified slighly by USIT
+ * TODO: optimize
+ * Modified slighly by USIT to use Web Workers
  */
 
 /* elem: Canvas element
@@ -304,99 +304,60 @@ VrtxImageEditor.prototype.renderRestorePoint = function renderRestorePoint() {
  * sx: Scaled width
  * lobes: kernel radius (e.g 3)
  */
-function thumbnailer(elem, ctx, img, sx, lobes) {
-  this.canvas = elem;
+function thumbnailer(elem, ctx, img, sx, thelobes) {
+  var canvas = elem;
   elem.width = img.width;
   elem.height = img.height;
   elem.style.display = "none";
-  this.ctx = ctx;
-  this.ctx.drawImage(img, 0, 0);
-  this.img = img;
-  this.src = this.ctx.getImageData(0, 0, img.width, img.height);
-  this.dest = {
-    width: sx,
-    height: Math.round(img.height * sx / img.width),
+  ctx.drawImage(img, 0, 0);
+  var image = img;
+  var thesrc = ctx.getImageData(0, 0, img.width, img.height);
+  var w = sx;
+  var h = Math.round(img.height * w / img.width);
+  var theratio = img.width / w;
+  var therange2 = Math.ceil(theratio * thelobes / 2);
+  
+  var thumbnailerObj = {
+    src: thesrc,
+    lobes: thelobes,
+    dest: {
+      width: w,
+      height: h,
+      data: new Array(w * h * 3)
+    },
+    ratio: theratio,
+    rcp_ratio: 2 / theratio,
+    range2: therange2,
+    cacheLanc: {},
+    center: {},
+    icenter: {}
   };
-  this.dest.data = new Array(this.dest.width * this.dest.height * 3);
-  this.lanczos = lanczosCreate(lobes);
-  this.ratio = img.width / sx;
-  this.rcp_ratio = 2 / this.ratio;
-  this.range2 = Math.ceil(this.ratio * lobes / 2);
-  this.cacheLanc = {};
-  this.center = {};
-  this.icenter = {};
-  setTimeout(this.process1, 0, this, 0);
-}
-
-thumbnailer.prototype.process1 = function (self, u) {
-  self.center.x = (u + 0.5) * self.ratio;
-  self.icenter.x = Math.floor(self.center.x);
-  for (var v = 0; v < self.dest.height; v++) {
-    self.center.y = (v + 0.5) * self.ratio;
-    self.icenter.y = Math.floor(self.center.y);
-    var a, r, g, b;
-    a = r = g = b = 0;
-    for (var i = self.icenter.x - self.range2; i <= self.icenter.x + self.range2; i++) {
-      if (i < 0 || i >= self.src.width) continue;
-      var f_x = Math.floor(1000 * Math.abs(i - self.center.x));
-      if (!self.cacheLanc[f_x]) self.cacheLanc[f_x] = {};
-      for (var j = self.icenter.y - self.range2; j <= self.icenter.y + self.range2; j++) {
-        if (j < 0 || j >= self.src.height) continue;
-        var f_y = Math.floor(1000 * Math.abs(j - self.center.y));
-        if (self.cacheLanc[f_x][f_y] == undefined) {
-          self.cacheLanc[f_x][f_y] = self.lanczos(Math.sqrt(Math.pow(f_x * self.rcp_ratio, 2) 
-                                   + Math.pow(f_y * self.rcp_ratio, 2)) / 1000);
-        }
-        weight = self.cacheLanc[f_x][f_y];
-        if (weight > 0) {
-          var idx = (j * self.src.width + i) * 4;
-          a += weight;
-          r += weight * self.src.data[idx];
-          g += weight * self.src.data[idx + 1];
-          b += weight * self.src.data[idx + 2];
+  
+  var worker = new Worker('/vrtx/__vrtx/static-resources/js/image-editor/lanczos-process1.js');
+  worker.postMessage(thumbnailerObj);
+  worker.addEventListener('message', function(e) {
+    var data = e.data;
+    if(data) {   
+      canvas.width = data.dest.width;
+      canvas.height = data.dest.height;
+      ctx.drawImage(image, 0, 0);
+      data.src = ctx.getImageData(0, 0, data.dest.width, data.dest.height);
+      var idx, idx2;
+      for (var i = 0; i < data.dest.width; i++) {
+        for (var j = 0; j < data.dest.height; j++) {
+          idx = (j * data.dest.width + i) * 3;
+          idx2 = (j * data.dest.width + i) * 4;
+          data.src.data[idx2] = data.dest.data[idx]; 
+          data.src.data[idx2 + 1] = data.dest.data[idx + 1];
+          data.src.data[idx2 + 2] = data.dest.data[idx + 2];
         }
       }
-    }
-    var idx = (v * self.dest.width + u) * 3;
-    self.dest.data[idx] = r / a;
-    self.dest.data[idx + 1] = g / a;
-    self.dest.data[idx + 2] = b / a;
-  }
-
-  if (++u < self.dest.width) setTimeout(self.process1, 0, self, u);
-  else setTimeout(self.process2, 0, self);
-};
-
-thumbnailer.prototype.process2 = function (self) {
-  self.canvas.width = self.dest.width;
-  self.canvas.height = self.dest.height;
-  self.ctx.drawImage(self.img, 0, 0);
-  self.src = self.ctx.getImageData(0, 0, self.dest.width, self.dest.height);
-  var idx, idx2;
-  for (var i = 0; i < self.dest.width; i++) {
-    for (var j = 0; j < self.dest.height; j++) {
-      idx = (j * self.dest.width + i) * 3;
-      idx2 = (j * self.dest.width + i) * 4;
-      self.src.data[idx2] = self.dest.data[idx];
-      self.src.data[idx2 + 1] = self.dest.data[idx + 1];
-      self.src.data[idx2 + 2] = self.dest.data[idx + 2];
-    }
-  }
-  self.ctx.putImageData(self.src, 0, 0);
-  $("#vrtx-image-editor-preview").removeClass("loading");
-  $("#vrtx-image-crop").removeAttr("disabled");
-  self.canvas.style.display = "block";
-}
-
-// Returns a function that calculates lanczos weight
-function lanczosCreate(lobes) {
-  return function (x) {
-    if (x > lobes) return 0;
-    x *= Math.PI;
-    if (Math.abs(x) < 1e-16) return 1
-    var xx = x / lobes;
-    return Math.sin(x) * Math.sin(xx) / x / xx;
-  }
+      ctx.putImageData(data.src, 0, 0); 
+      elem.style.display = "block";
+      $("#vrtx-image-editor-preview").removeClass("loading");
+      $("#vrtx-image-crop").removeAttr("disabled");   
+    } 
+  }, false);
 }
 
 /*

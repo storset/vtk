20d3f46c33db15c975517f241bef8c51a224bac3
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-02-09 11:32:22 +0000

    VTK - added ajaxsave

---
+++ src/main/ext/CKEditor/ckeditor-build/_source/plugins/ajaxsave/plugin.js
@@ -0,0 +1,39 @@
+CKEDITOR.plugins.add('ajaxsave', {
+  init: function (editor) {
+    var pluginName = 'ajaxsave';
+    editor.addCommand(pluginName, {
+      exec: function (editor) {
+        var $form = editor.element.$.form;
+        if ($form) {
+          try {
+            for (instance in CKEDITOR.instances) {
+              CKEDITOR.instances[instance].updateElement();
+            }
+           
+            tb_show(saveDocAjaxText + "...", 
+                   "/vrtx/__vrtx/static-resources/js/plugins/thickbox-modified/loadingAnimation.gif?width=240&height=20", 
+                   false);
+            
+            performSave();
+            $("#editor").ajaxSubmit({
+              success: function () {},
+              complete: function() {
+                initDatePicker(datePickerLang);
+                tb_remove();
+              }
+            });
+          }
+          catch (e) {
+            alert(e);
+          }
+        }
+      },
+      canUndo: true
+    });
+    editor.ui.addButton('Ajaxsave', {
+      label: 'Save Ajax',
+      command: pluginName,
+      className: 'cke_button_save'
+    });
+  }
+});

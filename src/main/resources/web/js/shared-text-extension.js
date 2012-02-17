73ac02a89c176bbc3e17cc25d32c591865f16318
Magnus Korvald <korvald@usit.uio.no>
2012-02-17 11:56:24 +0000

    display content for shared text in editor - VTK-2713

--- src/main/resources/web/js/shared-text-extension.js
+++ src/main/resources/web/js/shared-text-extension.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
 	$(".vrtx-shared-text input").each(function(){
 		var doctype = $("#resource-title").attr("class").split(" ")[0];
-		//var lang = datePickerLang; 
+		var lang = "no"; 
 		var id = $(this).attr("id");
 		var name = $(this).attr("name");
 		var selected = $(this).val();	
 		var path = "/vrtx/fellestekst/" + doctype + "/" + name + ".html?vrtx=source";
 
-		var containerElement = $(this).parents(".inputfield")
+		var containerElement = $(this).parents(".vrtx-shared-text");
+		var inputfieldContainer = $(this).parents(".inputfield");
 		 
 		$.getJSON(path, function(data) {  
 			  
@@ -15,9 +16,10 @@ $(document).ready(function () {
 				return;
 			}
 			  
-			$(containerElement).html("<select id='" + id +"' name='" + name + "'></select>");
+			$(inputfieldContainer).html("<select id='" + id +"' name='" + name + "'></select>");
 			var selectElement =  $("#" + id);
 			$(selectElement).append("<option value=''>Ingen fellestekst</option>")
+			$(containerElement).append("<div class='description'>test</div>");
 			var properties = data.properties; 
 			for(x in properties){
 			  if(x == "shared-text-box"){
@@ -25,15 +27,36 @@ $(document).ready(function () {
 					var s  = "";
 					if(properties[x][y].id  == selected){
 					  s = "selected";
+					  var d = $(containerElement).find(".description")
+					  $(d).html(properties[x][y]['description-' + lang]);
 					}
-					$(selectElement).append("<option value=" + properties[x][y].id + " " + s + ">" + properties[x][y].title + "</option>");
-					// $(selectElement).after("<div class='description-en'>" + properties[x][y]['description-en'] + "</div>");
-					// $(selectElement).after("<div class='description-no'>" + properties[x][y]['description-no'] + "</div>");
-					// $(selectElement).after("<div class='description-nn'>" + properties[x][y]['description-nn'] + "</div>");		 
-					}
+					$(selectElement).append("<option value=" + properties[x][y].id + " " + s + ">" + properties[x][y].title + "</option>");		 
+				  }
 				}
 			  }
-		  });  
 	
+			$(selectElement).change(function(){
+				
+				if($(selectElement).val().trim() == ""){
+					 var d = $(containerElement).find(".description");
+					 $(d).html("");
+					 return;
+				}
+				
+				for(x in properties){
+					  for (y in properties[x]){
+						  	var obj;
+							if(properties[x][y].id  == $(selectElement).val()){
+								  var d = $(containerElement).find(".description")
+								  $(d).html(properties[x][y]['description-' + lang]);
+								  return;
+							}
+					   }
+				}
+
+			});	
+
+		});  
 	});
+		
 });

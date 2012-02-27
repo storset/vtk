25a2ef1ffe7a4f9397e0c9158ba169e175b4cced
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-02-27 10:32:00 +0000

    VTK-2743 - add JS for open in WebDAV

---
+++ src/main/resources/web/js/open-webdav.js
@@ -0,0 +1,18 @@
+$(function() {
+  if(typeof agentWebDav !== "undefined") {
+    var agentWebDav = navigator.userAgent.toLowerCase();         
+    var isWinWebDav = ((agentWebDav.indexOf("win") != -1) || (agentWebDav.indexOf("16bit") != -1));
+    if ($.browser.msie && $.browser.version >= 7 && isWinWebDav) {  
+      $(".vrtx-resource-open-webdav").click(function(e) {
+        var openOffice = new ActiveXObject("Sharepoint.OpenDocuments.1").EditDocument(this.href);
+        e.stopPropagation();
+        e.preventDefault();
+      });
+      $(".vrtx-resource").hover(function (e) { 
+        $(this).find(".vrtx-resource-open-webdav").css("left", ($(this).find(".vrtx-title a").width() + 63) + "px").show(0);
+      }, function (e) {
+        $(this).find(".vrtx-resource-open-webdav").hide(0).css("left", "0px");
+      });
+    }
+  }
+});

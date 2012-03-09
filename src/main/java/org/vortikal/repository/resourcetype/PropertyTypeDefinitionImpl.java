ed0656bae5879ced01339658e51121a60c7c7864
Gorm Paulsen <gormap@usit.uio.no>
2012-03-09 11:55:31 +0000

    indexableAttributes is gone

--- src/main/java/org/vortikal/repository/resourcetype/PropertyTypeDefinitionImpl.java
+++ src/main/java/org/vortikal/repository/resourcetype/PropertyTypeDefinitionImpl.java
@@ -35,20 +35,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Required;
-import org.vortikal.repository.IllegalOperationException;
 import org.vortikal.repository.Namespace;
 import org.vortikal.repository.Property;
 import org.vortikal.repository.PropertyImpl;
 import org.vortikal.repository.RepositoryAction;
 import org.vortikal.repository.Vocabulary;
 import org.vortikal.repository.resourcetype.PropertyType.Type;
-import org.vortikal.resourcemanagement.JSONPropertyAttributeDescription;
 import org.vortikal.security.Principal;
 
 /**
@@ -83,7 +80,6 @@ public class PropertyTypeDefinitionImpl implements PropertyTypeDefinition, Initi
     private transient ValueFormatterRegistry valueFormatterRegistry;
     private transient ContentRelation contentRelation;
     private transient TypeLocalizationProvider typeLocalizationProvider = null;
-    private transient List<JSONPropertyAttributeDescription> indexableAttributes;
 
     public void setContentRelation(ContentRelation contentRelation) {
         this.contentRelation = contentRelation;
@@ -371,21 +367,6 @@ public class PropertyTypeDefinitionImpl implements PropertyTypeDefinition, Initi
     @Required
     public void setValueFormatterRegistry(ValueFormatterRegistry valueFormatterRegistry) {
         this.valueFormatterRegistry = valueFormatterRegistry;
-    }
-
-    public List<JSONPropertyAttributeDescription> getIndexableAttributes() throws IllegalOperationException {
-        if (this.type != Type.JSON) {
-            throw new IllegalOperationException("Only applicable on properties of type JSON");
-        }
-        return this.indexableAttributes;
-    }
-
-    /**
-     * Set a list of names for attributes that are to be indexed Only valid for
-     * json properties
-     */
-    public void setIndexableAttributes(final List<JSONPropertyAttributeDescription> indexableAttributes) {
-        this.indexableAttributes = indexableAttributes;
     }
 
 }

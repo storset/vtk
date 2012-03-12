6c7070a929071d09834cb4ccc4e9a4722c3ccb48
Stian Færden Kristensen <stiank@usit.uio.no>
2012-03-12 13:41:29 +0000

    VTK-2688 javascript

--- src/main/resources/web/js/study-admin-enhancements.js
+++ src/main/resources/web/js/study-admin-enhancements.js
@@ -1,45 +1,45 @@
 function hideShowStudy(typeToDisplay){
         	switch(typeToDisplay){
                 case "so" :
-                    $('.frist-frekvens').show();
-                    $('.metode').show();
-                    $('.internasjonale-sokere').hide();
+                    $('.frist-frekvens-admin').show();
+                    $('.metode-admin').show();
+                    $('.internasjonale-sokere-admin').hide();
                     $('.nordiske-sokere').hide();
-                    $('.opptakskrav').show();
-                    $('.generelle').hide();
+                    $('.opptakskrav-admin').show();
+                    $('.generelle-admin').hide();
                     $('.studiekode').show();
                     $('.pris').hide();
                     $('.regelverk').hide();
                 break;
                 case "nm" : 
-                    $('.frist-frekvens').show();
-                    $('.metode').show();
-                    $('.internasjonale-sokere').hide();
+                    $('.frist-frekvens-admin').show();
+                    $('.metode-admin').show();
+                    $('.internasjonale-sokere-admin').hide();
                     $('.nordiske-sokere').hide();
-                    $('.opptakskrav').hide();
-                    $('.generelle').show();
+                    $('.opptakskrav-admin').hide();
+                    $('.generelle-admin').show();
                     $('.studiekode').hide();
                     $('.pris').show();
                     $('.regelverk').show();
                 break;
                 case "em" :
-                    $('.frist-frekvens').hide();
-                    $('.metode').hide();
-                    $('.internasjonale-sokere').show();
+                    $('.frist-frekvens-admin').hide();
+                    $('.metode-admin').hide();
+                    $('.internasjonale-sokere-admin').show();
                     $('.nordiske-sokere').show();
-                    $('.opptakskrav').hide();
-                    $('.generelle').show();
+                    $('.opptakskrav-admin').hide();
+                    $('.generelle-admin').show();
                     $('.studiekode').hide();
                     $('.pris').show();
                     $('.regelverk').show();
                 break;
                 default :
-                    $('.frist-frekvens').show();
-                    $('.metode').show();
-                    $('.internasjonale-sokere').show();
+                    $('.frist-frekvens-admin').show();
+                    $('.metode-admin').show();
+                    $('.internasjonale-sokere-admin').show();
                     $('.nordiske-sokere').show();
-                    $('.opptakskrav').show();
-                    $('.generelle').show();
+                    $('.opptakskrav-admin').show();
+                    $('.generelle-admin').show();
                     $('.studiekode').show();
                     $('.pris').show();
                     $('.regelverk').show();
@@ -54,7 +54,7 @@ $(document).ready(function() {
         } catch (err){
         	return false;
         }
-        $('#typeToDisplay').change(function() {
+        $(document).on('change', '#typeToDisplay', function() {
            var typeToDisplay = $('#typeToDisplay').val();
            hideShowStudy(typeToDisplay);
           });

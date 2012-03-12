3772e5763b65191a602da6399c1c80ced2eace63
Øyvind Stegard <oyviste@usit.uio.no>
2012-03-12 12:13:08 +0000

    Replace redundant null-check with test for most common case first (later instanceof implicitly handles null-check).

--- src/main/java/org/vortikal/repository/Namespace.java
+++ src/main/java/org/vortikal/repository/Namespace.java
@@ -93,7 +93,7 @@ public class Namespace implements Serializable {
     
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) return false;
+        if (obj == this) return true;
         if (! (obj instanceof Namespace)) return false;
         Namespace ns = (Namespace) obj;
 

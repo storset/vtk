f785a1fdafdbe644e5881b42a70a360f3bf6eacd
Reza Mirzaei <rezam@usit.uio.no>
2012-03-27 10:53:19 +0000

    Comment! FIX! Works for now, because edit links are only used for single listing searches (collection/message listing). Will however fail on multiple listing search, such as event listings (upcoming/previous), article listings (featured articles), project listing (running/closed).

--- src/main/java/org/vortikal/web/decorating/components/CollectionListingComponentHelper.java
+++ src/main/java/org/vortikal/web/decorating/components/CollectionListingComponentHelper.java
@@ -11,15 +11,16 @@ import org.vortikal.web.search.Listing;
 
 public class CollectionListingComponentHelper {
 
+    // XXX FIXME!!!
+    // Indices of return array must match indices of each property set in each
+    // result set in each listing!!!
     public boolean[] isAuthorized(Repository r, String token, Principal principal, int maxItems, List<Listing> ll)
             throws Exception {
         Resource res;
-        String rt = null;
         int i = 0;
         boolean[] edit = new boolean[maxItems];
         for (Listing l : ll) {
             for (PropertySet ps : l.getFiles()) {
-                rt = ps.getResourceType();
                 try {
                     res = r.retrieve(token, ps.getURI(), false);
                     edit[i++] = r.isAuthorized(res, RepositoryAction.READ_WRITE, principal, true);

747748687a0955420c11f6f944e5c042fb983c57
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-04-12 10:23:59 +0000

    VTK-2811 - delegate share component

--- src/main/resources/web/js/social/share.js
+++ src/main/resources/web/js/social/share.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $("a.vrtx-close-toolbox-send-share").click(function(e) {
+  $("body").on("click", "a.vrtx-close-toolbox-send-share", function(e) {
     $("#vrtx-send-share div:visible").hide();
     e.preventDefault();
   });
-  $("a.vrtx-share-link").click(function(e) {
+  $("body").on("click", "a.vrtx-share-link", function(e) {
     $("#vrtx-send-share div:hidden").slideDown("fast");
     e.preventDefault();
   });

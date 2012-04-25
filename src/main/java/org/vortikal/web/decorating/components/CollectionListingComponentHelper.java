cd9c984d952f8d341019623c22c8c3a638f053bd
Øyvind Stegard <oyviste@usit.uio.no>
2012-04-25 07:43:35 +0000

    * Use Repository.authorize to check ACL and avoid loading the same resource twice. * Don't do any checks if no principal (anonymous), disallow all. * Handle any exception when loading from repo, and keep to false for those   cases. Patches racy behaviour, althrough I still don't like the parallel array   approach. * Do not try to load URIs of remote resources (Solr results).

--- src/main/java/org/vortikal/web/decorating/components/CollectionListingComponentHelper.java
+++ src/main/java/org/vortikal/web/decorating/components/CollectionListingComponentHelper.java
@@ -2,9 +2,10 @@ package org.vortikal.web.decorating.components;
 
 import java.util.List;
 
+import org.vortikal.repository.MultiHostSearcher;
+import org.vortikal.repository.Privilege;
 import org.vortikal.repository.PropertySet;
 import org.vortikal.repository.Repository;
-import org.vortikal.repository.RepositoryAction;
 import org.vortikal.repository.Resource;
 import org.vortikal.security.Principal;
 import org.vortikal.web.search.Listing;
@@ -19,23 +20,37 @@ public class CollectionListingComponentHelper {
         Resource res;
         int i = 0;
         boolean[] edit = new boolean[maxItems];
+        
+        // If not logged in, don't provide any edit-authorizations and stop checks.
+        if (principal == null) {
+            return edit;
+        }
+        
         for (Listing l : ll) {
             for (PropertySet ps : l.getFiles()) {
-              res = repo.retrieve(token, ps.getURI(), true); // XXX: Throw or catch exception?
-              edit[i++] = this.isAuthorized(repo, res, principal);                
-            }
+                boolean authorized = false;
+                if (ps.getPropertyByPrefix(null, MultiHostSearcher.MULTIHOST_RESOURCE_PROP_NAME) == null) {
+                    try {
+                        res = repo.retrieve(token, ps.getURI(), true);
+                        authorized = isAuthorized(repo, res, principal);
+                    } catch (Exception exception) {
+                    }
+                }
 
+                edit[i++] = authorized;
+            }
         }
+        
         return edit;
     }
 
-   // XXX: Throw exception?
-   public boolean isAuthorized(Repository repo, Resource res, Principal principal) {
-     try {
-       return repo.isAuthorized(res, RepositoryAction.READ_WRITE, principal, true);
-     } catch(Exception e)  {
-       return false;
-     }
-   }
+    public boolean isAuthorized(Repository repo, Resource res, Principal principal) {
+        if (res.getLock() != null && !res.getLock().getPrincipal().equals(principal)) {
+            return false;
+        }
 
+        return repo.authorize(principal, res.getAcl(), Privilege.ALL)
+                || repo.authorize(principal, res.getAcl(), Privilege.READ_WRITE);
+
+    }
 }

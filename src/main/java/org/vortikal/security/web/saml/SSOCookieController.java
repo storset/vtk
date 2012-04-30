488e7ea44a3675859d633267c5cc08a2ff741242
David Egeland <dave@usit.uio.no>
2012-04-30 10:53:30 +0000

    Added logging.

--- src/main/java/org/vortikal/security/web/saml/SSOCookieController.java
+++ src/main/java/org/vortikal/security/web/saml/SSOCookieController.java
@@ -3,11 +3,15 @@ package org.vortikal.security.web.saml;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import org.vortikal.web.service.URL;
 
 public class SSOCookieController implements Controller {
+
+    private static Log authLogger = LogFactory.getLog("org.vortikal.security.web.AuthLog");
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -19,6 +23,9 @@ public class SSOCookieController implements Controller {
         }
         URL currentURL = URL.parse(url.toString());
         currentURL.addParameter("authTarget", request.getScheme());
+
+        authLogger.debug("SSO-cookie found, redirecting to: " + currentURL.toString());
+
         response.sendRedirect(currentURL.toString());
 
         return null;

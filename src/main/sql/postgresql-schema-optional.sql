54bbf1b36ec4f36b8d3cc14bb1a12460537e64e0
Øyvind Stegard <oyviste@usit.uio.no>
2012-05-08 10:16:57 +0000

    Add optional schema commands which can enable automatic cleanup of binary large objects when using the PostgreSQL database. Use to avoid bleeding disk space.

---
+++ src/main/sql/postgresql-schema-optional.sql
@@ -0,0 +1,21 @@
+-----------------------------------------------------------------------------
+-- VORTEX DDL, extra contrib functions for PostgreSQL
+-----------------------------------------------------------------------------
+
+
+-- Automatic clean up of binary large objects through trigger and 'lo' extension.
+-- Requires:
+--     - PostgreSQL contrib modules to be installed on system where database runs.
+--     - Database super-user access.
+
+-- 0. Install PostgreSQL contrib modules, if it's not present in the database installation.
+
+-- 1. Connect to Vortex database as super-user and run all SQL statements in the
+--    PostgreSQL lo contrib file: 'contrib/lo/lo.sql' (location depends on installation).
+
+-- 2. Run the following to set up trigger for extra_prop_entry.binary content:
+
+alter table extra_prop_entry alter column binary_content TYPE lo;
+
+create trigger extra_prop_entry_trigger1 
+   before update or delete on extra_prop_entry for each row execute procedure lo_manage(binary_content);

2a7b60296274ea3941501b34af49fb9c79dfc5fe
Magnus Korvald <korvald@usit.uio.no>
2012-05-08 11:40:43 +0000

    Moving program listing to vortex

--- src/main/java/org/vortikal/web/display/collection/studies/ProgramOptionListingController.java
+++ src/main/java/org/vortikal/web/display/collection/studies/ProgramOptionListingController.java
@@ -1,7 +1,33 @@
 package org.vortikal.web.display.collection.studies;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.vortikal.repository.Resource;
 import org.vortikal.web.display.collection.CollectionListingController;
+import org.vortikal.web.search.Listing;
+import org.vortikal.web.search.SearchComponent;
 
 public class ProgramOptionListingController extends CollectionListingController {
 
+    @Override
+    public void runSearch(HttpServletRequest request, Resource collection, Map<String, Object> model, int pageLimit)
+            throws Exception {
+
+        List<Listing> results = new ArrayList<Listing>();
+        for (SearchComponent component : searchComponents) {
+            Listing listing = component.execute(request, collection, 1, 500, 0);
+            // Add the listing to the results
+            if (listing.getFiles().size() > 0) {
+                results.add(listing);
+            }
+        }
+
+        model.put(MODEL_KEY_SEARCH_COMPONENTS, results);
+
+    }
+    
 }

cc3ac66d41665254fc66c65c3aefad704c271a6e
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-05-24 16:34:46 +0000

    VTK-2820 - autogrow file name inputfield

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -182,6 +182,8 @@ vrtxAdmin._$(document).ready(function () {
       vrtxAdm = vrtxAdmin,
       _$ = vrtxAdm._$,
       bodyId = _$("body").attr("id");
+      
+  _$("body").addClass("js");
 
   // Buttons into links
   vrtxAdm.logoutButtonAsLink();
@@ -691,6 +693,12 @@ function createFuncComplete() {
     $("head").append("<script src='/vrtx/__vrtx/static-resources/jquery/plugins/jquery.vortexTips.js' type='text/javascript'></script>");
   }
   $(".vrtx-admin-form").vortexTips("a.resource-prop-info", ".vrtx-admin-form", 200, 300, 250, 300, 20, -30, false, false);
+  
+  $("#vrtx-textfield-file-name input, #vrtx-textfield-collection-name input").autoGrowInput({
+    comfortZone: 4,
+    minWidth: 35,
+    maxWidth: 300
+  });
 }
 
 function userTitleChange(titleBind, nameBind, indexBind) {
@@ -1703,6 +1711,7 @@ VrtxAdmin.prototype.getFormAsync = function getFormAsync(options) {
     if(location.protocol == "http:" && url.indexOf("https://") != -1) {
       return; // no AJAX when http -> https (tmp. solution)
     }
+    
     if(GET_FORM_ASYNCS_IN_PROGRESS) { // If there are any getFormAsync() in progress
       return false;
     }
@@ -2452,5 +2461,58 @@ function unique(array) {
   for(i in o) r.push(o[i]);
   return r;
 }
+
+(function ($) {
+  // jQuery autoGrowInput plugin by James Padolsey
+  // Modified to use .on()-events and use Math.max in algorithm, USIT 2012
+  
+  // See related thread: http://stackoverflow.com/questions/931207/is-there-a-jquery-autogrow-plugin-for-text-fields
+  
+  $.fn.autoGrowInput = function (o) {
+    o = $.extend({
+      maxWidth: 1000,
+      minWidth: 0,
+      comfortZone: 70
+    }, o);
+
+    this.filter('input:text').each(function () {
+        var minWidth = o.minWidth || $(this).width(),
+        val = '',
+        input = $(this),
+        testSubject = $('<tester/>').css({
+          position: 'absolute',
+          top: -9999,
+          left: -9999,
+          width: 'auto',
+          fontSize: input.css('fontSize'),
+          fontFamily: input.css('fontFamily'),
+          fontWeight: input.css('fontWeight'),
+          letterSpacing: input.css('letterSpacing'),
+          whiteSpace: 'nowrap'
+        }),
+        check = function () {
+          if (val === (val = input.val())) {
+            return;
+          }
+
+          // Enter new content into testSubject
+          testSubject.html(val);
+
+          // Calculate new width + whether to change
+          var testerWidth = testSubject.width(),
+            newWidth = Math.max(testerWidth + o.comfortZone, minWidth),
+            currentWidth = input.width(),
+            isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
+                              || (newWidth > minWidth && newWidth < o.maxWidth);
+          if (isValidWidthChange) {  // Animate width
+            input.width(newWidth);
+          }
+       };
+       testSubject.insertAfter(input);
+       $(document).on('keyup keydown change blur update', this, check);
+    });
+    return this;
+  };
+})(jQuery);
 
 /* ^ Vortex Admin enhancements */

34020fb71db2ab37e6c052fca5a14ad2bfde5996
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-05-24 19:33:19 +0000

    VTK-2820 - the plugin was slow so modified it more to our use and event-handling. Also remove the same form test

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -693,12 +693,7 @@ function createFuncComplete() {
     $("head").append("<script src='/vrtx/__vrtx/static-resources/jquery/plugins/jquery.vortexTips.js' type='text/javascript'></script>");
   }
   $(".vrtx-admin-form").vortexTips("a.resource-prop-info", ".vrtx-admin-form", 200, 300, 250, 300, 20, -30, false, false);
-  
-  $("#vrtx-textfield-file-name input, #vrtx-textfield-collection-name input").autoGrowInput({
-    comfortZone: 6,
-    minWidth: 35,
-    maxWidth: 350
-  });
+
 }
 
 function changeTemplate(element, hasTitle) {
@@ -748,7 +743,7 @@ function userTitleChange(titleBind, nameBind, indexBind) {
       var indexCheckbox = $("#" + indexBind);
     }
     
-    var nameFieldVal = nameFieldUpdate(titleField.val());
+    var nameFieldVal = nameFieldUpdate(nameField, titleField.val());
     
     if(!indexBind || !indexCheckbox.length || !indexCheckbox.is(":checked")) {
       nameField.val(nameFieldVal);
@@ -764,7 +759,7 @@ function disableReplaceTitle(nameBind) {
   }
   
   var nameField = $("#" + nameBind);
-  var nameFieldVal = nameFieldUpdate(nameField.val());
+  var nameFieldVal = nameFieldUpdate(nameField, nameField.val());
   nameField.val(nameFieldVal);
 
   $("#vrtx-textfield-file-name").removeClass("replaced");
@@ -772,10 +767,10 @@ function disableReplaceTitle(nameBind) {
   $("#vrtx-textfield-collection-name").removeClass("replaced");
 }
 
-function nameFieldUpdate(fromFieldVal) {
+function nameFieldUpdate(nameField, fromFieldVal) {
   var nameFieldVal = replaceInvalidChar(fromFieldVal);
-  return (nameFieldVal.length > 30) ? nameFieldVal.substring(0, 30) 
-                                    : nameFieldVal;
+  growField(nameField, nameFieldVal, 5);
+  return nameFieldVal;
 }
 
 function replaceInvalidChar(val) {
@@ -1757,21 +1752,6 @@ VrtxAdmin.prototype.getFormAsync = function getFormAsync(options) {
         }
         // Another form is already open
         if(existExpandedForm) {
-          var expandedFormForm = expandedForm.find("form");
-          // Check if the form is the same as new based on id
-          if(expandedFormForm.length) {
-            var expandedFormFormId = expandedFormForm.attr("id");
-            var newFormId = $(form).attr("id");
-            if(typeof expandedFormFormId !== "undefined" && expandedFormFormId.length &&
-               typeof newFormId !== "undefined" && newFormId.length &&
-               (expandedFormFormId === newFormId)) {
-                 if(GET_FORM_ASYNCS_IN_PROGRESS) {
-                   GET_FORM_ASYNCS_IN_PROGRESS--;
-                 }
-                 return false;
-            }
-          }
-         
           // Get class for original markup
           var resultSelectorClasses = expandedForm.attr("class").split(" ");
           var resultSelectorClass = "";
@@ -1875,11 +1855,11 @@ VrtxAdmin.prototype.addNewMarkup = function addNewMarkup(options, selectorClass,
     _$(vrtxAdm.wrap(nodeType, "expandedForm nodeType" + nodeType + " " + selectorClass, form))
       .insertAfter(insertAfterOrReplaceClass);
   }
-  if(GET_FORM_ASYNCS_IN_PROGRESS) {
-    GET_FORM_ASYNCS_IN_PROGRESS--;
-  }
   if(funcComplete) {
     funcComplete(selectorClass);
+  }
+  if(GET_FORM_ASYNCS_IN_PROGRESS) {
+    GET_FORM_ASYNCS_IN_PROGRESS--;
   }
   if(nodeType == "tr") {
     _$(nodeType + "." + selectorClass).prepareTableRowForSliding();
@@ -1937,7 +1917,7 @@ VrtxAdmin.prototype.completeFormAsync = function completeFormAsync(options) {
         });
         e.preventDefault();
       } else {
-        if(funcComplete) {
+        if(funcComplete && !isCancelAction) {
           funcComplete();
         }
         return;
@@ -2466,57 +2446,34 @@ function unique(array) {
   return r;
 }
 
-(function ($) {
-  // jQuery autoGrowInput plugin by James Padolsey
-  // Modified to use .on()-events and use Math.max in algorithm, USIT 2012
+// jQuery autoGrowInput plugin by James Padolsey
+// Modified to simplified function ++ for more specific use / event-handling by USIT, 2012
+// See related thread: http://stackoverflow.com/questions/931207/is-there-a-jquery-autogrow-plugin-for-text-fields
   
-  // See related thread: http://stackoverflow.com/questions/931207/is-there-a-jquery-autogrow-plugin-for-text-fields
-  
-  $.fn.autoGrowInput = function (o) {
-    o = $.extend({
-      maxWidth: 1000,
-      minWidth: 0,
-      comfortZone: 70
-    }, o);
+function growField(input, val, comfortZone) {
+  var minWidth = input.width(),
+      testSubject = $('<tester/>').css({
+        position: 'absolute',
+        top: -9999,
+        left: -9999,
+        width: 'auto',
+        fontSize: input.css('fontSize'),
+        fontFamily: input.css('fontFamily'),
+        fontWeight: input.css('fontWeight'),
+        letterSpacing: input.css('letterSpacing'),
+        whiteSpace: 'nowrap'
+      });
 
-    this.filter('input:text').each(function () {
-        var minWidth = o.minWidth || $(this).width(),
-        val = '',
-        input = $(this),
-        testSubject = $('<tester/>').css({
-          position: 'absolute',
-          top: -9999,
-          left: -9999,
-          width: 'auto',
-          fontSize: input.css('fontSize'),
-          fontFamily: input.css('fontFamily'),
-          fontWeight: input.css('fontWeight'),
-          letterSpacing: input.css('letterSpacing'),
-          whiteSpace: 'nowrap'
-        }),
-        check = function () {
-          if (val === (val = input.val())) {
-            return;
-          }
-
-          // Enter new content into testSubject
-          testSubject.html(val);
-
-          // Calculate new width + whether to change
-          var testerWidth = testSubject.width(),
-            newWidth = Math.max(testerWidth + o.comfortZone, minWidth),
-            currentWidth = input.width(),
-            isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
-                              || (newWidth > minWidth && newWidth < o.maxWidth);
-          if (isValidWidthChange) {  // Animate width
-            input.width(newWidth);
-          }
-       };
-       testSubject.insertAfter(input);
-       $(document).on('keyup keydown change blur update', this, check);
-    });
-    return this;
-  };
-})(jQuery);
+  testSubject.insertAfter(input);  
+  testSubject.html(val);
+ 
+  var testerWidth = testSubject.width(),
+      newWidth = Math.max(testerWidth + comfortZone, minWidth),
+      currentWidth = input.width(),
+      isValidWidthChange = (newWidth < currentWidth && newWidth > minWidth) || (newWidth > currentWidth);
+  if (isValidWidthChange) {
+    input.width(newWidth);
+  }
+}
 
 /* ^ Vortex Admin enhancements */

62e216bc4e20292523d7a3bc5b074a9d1db011df
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-06-04 15:30:57 +0000

    VTK - revert local changes

--- src/main/java/org/vortikal/resourcemanagement/property/EvaluatorResolver.java
+++ src/main/java/org/vortikal/resourcemanagement/property/EvaluatorResolver.java
@@ -72,6 +72,7 @@ public class EvaluatorResolver {
     // XXX Reconsider this whole setup. No good implementation.
     private ExternalServiceInvoker serviceInvoker;
     private HtmlDigester htmlDigester;
+    private LocaleResolver localeResolver;
 
 
     public PropertyEvaluator createPropertyEvaluator(PropertyDescription desc,
@@ -306,9 +307,9 @@ public class EvaluatorResolver {
                 Object obj = ctx.getEvaluationAttribute(propName);
                 return Boolean.valueOf(operator.equals(obj));
             case LOCALIZED:
-                RequestContext requestContext = RequestContext.getRequestContext(); 
-                HttpServletRequest request = requestContext.getServletRequest(); 
-                Locale locale = RequestContextUtils.getLocale(request);
+                RequestContext requestContext = RequestContext.getRequestContext();
+                HttpServletRequest request = requestContext.getServletRequest();                
+                Locale locale = localeResolver.resolveLocale(request);
                 return resourceDesc.getLocalizedMsg(propValue, locale, null);
             default:
                 return null;
@@ -430,6 +431,11 @@ public class EvaluatorResolver {
     @Required
     public void setHtmlDigester(HtmlDigester htmlDigester) {
         this.htmlDigester = htmlDigester;
+    }
+    
+    @Required
+    public void setLocaleResolver(LocaleResolver localeResolver) {
+        this.localeResolver = localeResolver;
     }
 
 }

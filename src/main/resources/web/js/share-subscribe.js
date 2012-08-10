afa86510aa67e623fe32dc4591e2295b0a227d5a
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-08-10 10:47:06 +0000

    VTK-2936 - show and hide share/subscribe wrapper

--- src/main/resources/web/js/share-subscribe.js
+++ src/main/resources/web/js/share-subscribe.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function() {
   $("body").on("click", "a.vrtx-close-toolbox-send-share, a#vrtx-close-subscribe", function(e) {
-    $("#vrtx-send-share div:visible, #vrtx-subscribe-wrapper div:visible").hide();
+    $("#vrtx-send-share:visible, #vrtx-subscribe-wrapper:visible").hide();
     e.preventDefault();
   });
   $("body").on("click", "a.vrtx-share-link, a#vrtx-subscribe-link", function(e) {
-    $("#vrtx-send-share div:hidden, #vrtx-subscribe-wrapper div:hidden").slideDown("fast");
+    $("#vrtx-send-share:hidden, #vrtx-subscribe-wrapper:hidden").slideDown("fast");
     e.preventDefault();
   });
 });

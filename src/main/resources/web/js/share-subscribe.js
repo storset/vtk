fd7a7dcd8426f58d54af6594f6f19b3d2972cf8f
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-08-10 13:51:57 +0000

    VTK-2936

--- src/main/resources/web/js/share-subscribe.js
+++ src/main/resources/web/js/share-subscribe.js
@@ -1,12 +1,20 @@
 /* Share and subscribe box */
 
 $(document).ready(function() {
-  $("body").on("click", "a#vrtx-share-close-link, a#vrtx-subscribe-close-link", function(e) {
-    $("#vrtx-share-wrapper:visible, #vrtx-subscribe-wrapper:visible").hide();
+  $("body").on("click", "a#vrtx-share-close-link", function(e) {
+    $("#vrtx-share-wrapper").hide();
     e.preventDefault();
   });
-  $("body").on("click", "a#vrtx-share-link, a#vrtx-subscribe-link", function(e) {
-    $("#vrtx-share-wrapper:hidden, #vrtx-subscribe-wrapper:hidden").slideDown("fast");
+  $("body").on("click", "a#vrtx-subscribe-close-link", function(e) {
+    $("#vrtx-subscribe-wrapper").hide();
+    e.preventDefault();
+  });
+  $("body").on("click", "a#vrtx-share-link, function(e) {
+    $("#vrtx-share-wrapper:hidden").slideDown("fast");
+    e.preventDefault();
+  });  
+  $("body").on("click", "a#vrtx-subscribe-link", function(e) {
+    $("#vrtx-subscribe-wrapper:hidden").slideDown("fast");
     e.preventDefault();
   });
 });

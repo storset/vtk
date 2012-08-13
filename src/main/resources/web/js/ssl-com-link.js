33e4d0ecbf50fa28e3885ec8c098c2dd49eaf9b5
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-08-13 11:18:25 +0000

    VTK-2373 - start on SSL com. link (HTTP<=>HTTPS) lightweight library

---
+++ src/main/resources/web/js/ssl-com-link.js
@@ -0,0 +1,52 @@
+/*
+ *  SSL communication - lightweight library
+ *
+ */
+
+function SSLComLink(winObj) {
+
+  // Class-like singleton pattern (p.145 JavaScript Patterns)
+  var instance; // cached instance
+  VrtxAdmin = function VrtxAdmin() { // rewrite constructor
+    return instance;
+  };
+  VrtxAdmin.prototype = this; // carry over properties
+  instance = new VrtxAdmin(); // instance
+  instance.constructor = VrtxAdmin; // reset construction pointer
+  //--
+
+  // Cache jQuery instance internally
+  this._$ = $;
+  
+  this.winObj = winObj;
+  this.hasPostMessage = this.winObj['postMessage'] && (!($.browser.opera && $.browser.version < 9.65));
+  this.vrtxAdminOrigin = "*"; // TODO: TEMP Need real origin of adm
+  
+  return instance;
+};
+
+var SSLComLink = new SSLComLink(window);
+
+SSLComLink.prototype.postDataToParent = function postDataToParent(data) {
+  if(this.winObj.parent && this.hasPostMessage) {
+    this.winObj.parent.postMessage(data, this.vrtxAdminOrigin);
+  }
+};
+
+SSLComLink.prototype.postDataToIframe = function postDataToIframe(iframeElm, data) {
+  if(this.hasPostMessage) {
+    iframeElm.contentWindow.postMessage(data, this.vrtxAdminOrigin);
+  }
+};
+
+SSLComLink.prototype.setUpReceiveDataHandler = function setUpReceiveDataHandler(callback) {
+  $(this.winObj).on("message", function(e) {
+    if(e.originalEvent) e = e.originalEvent;
+    var receivedData = e.data; 
+    if(receivedData) {
+      callback(receivedData);
+    }
+  });
+};
+
+/* ^ SSL communication - lightweight library */

7eac4d2d993e8715dd189e9074746e7417b73b6c
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-08-16 08:50:59 +0000

    VTK-2936 - let the animation be the same as before for now

--- src/main/resources/web/js/share-subscribe.js
+++ src/main/resources/web/js/share-subscribe.js
@@ -2,19 +2,19 @@
 
 $(document).ready(function() {
   $("body").on("click", "a#vrtx-share-close-link", function(e) {
-    $("#vrtx-share-wrapper").hide();
+    $("#vrtx-share-wrapper div").hide();
     e.preventDefault();
   });
   $("body").on("click", "a#vrtx-subscribe-close-link", function(e) {
-    $("#vrtx-subscribe-wrapper").hide();
+    $("#vrtx-subscribe-wrapper div").hide();
     e.preventDefault();
   });
   $("body").on("click", "a#vrtx-share-link", function(e) {
-    $("#vrtx-share-wrapper:hidden").slideDown("fast");
+    $("#vrtx-share-wrapper div:hidden").slideDown("fast");
     e.preventDefault();
   });  
   $("body").on("click", "a#vrtx-subscribe-link", function(e) {
-    $("#vrtx-subscribe-wrapper:hidden").slideDown("fast");
+    $("#vrtx-subscribe-wrapper div:hidden").slideDown("fast");
     e.preventDefault();
   });
 });

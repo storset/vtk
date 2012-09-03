b26de27e9390d99b9da0a91a9525e302abaf998b
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-09-03 10:13:06 +0000

    VTK-2970 - just toggle visibiliy in IE7

--- src/main/resources/web/js/view-dropdown.js
+++ src/main/resources/web/js/view-dropdown.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $(document).off("click", ".vrtx-dropdown-component-toggled a.vrtx-dropdown-link")
               .on("click", ".vrtx-dropdown-component-toggled a.vrtx-dropdown-link", function(e) {
     if($.browser.msie && $.browser.version <= 7) {
-      $(this).toggleClass("active").next(".vrtx-dropdown-wrapper").animate({"height": toggle, "overflow": "visible"},"fast");
+      $(this).toggleClass("active").next(".vrtx-dropdown-wrapper").toggle(); // Because slide sets "overflow: hidden" causing IE7-bug
     } else {
       $(this).toggleClass("active").next(".vrtx-dropdown-wrapper").slideToggle("fast");
     }

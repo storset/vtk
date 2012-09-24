cac2275287d4572f4e3caa77126e918a47b4c403
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-09-24 14:07:25 +0000

    VTK-2997 - can't use AJAX-post on publish/unpublish on preview as need to update preview iframes (because date and time can be added to e.g. article)

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -285,7 +285,7 @@ vrtxAdmin._$(document).ready(function () {
     vrtxAdm.completeFormAsync({
         selector: "form#" + resourceMenuRightServices[i] + "-form input[type=submit]",
         updateSelectors: ["#resourceMenuRight", "#publishing-status"],
-        post: true
+        post: bodyId !== "vrtx-preview"
     });
   }
   

4f5825e7982c7ee3818d1fc939b6c24cd33abfd0
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-10-15 12:38:19 +0000

    VTK-3044 - try with iwn optimalized click-event-handler without misc overhead checks/functions for .one() etc. to possible fix slow IE8 create document/folder form

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -2114,7 +2114,7 @@ VrtxAdmin.prototype.getFormAsync = function getFormAsync(options) {
       vrtxAdm = this,         // use prototypal hierarchy 
       _$ = vrtxAdm._$;
       
-  vrtxAdm.cachedBody.on("click", options.selector, function (e) {
+  vrtxAdm.cachedBody.dynClick(options.selector, function (e) {
     var url = _$.single(this).attr("href") || _$.single(this).closest("form").attr("action");
     
     if(GET_FORM_ASYNCS_IN_PROGRESS) { // If there are any getFormAsync() in progress
@@ -2298,7 +2298,7 @@ VrtxAdmin.prototype.completeFormAsync = function completeFormAsync(options) {
       vrtxAdm = this,
       _$ = vrtxAdm._$;   
       
-  vrtxAdm.cachedBody.on("click", options.selector, function (e) {
+  vrtxAdm.cachedBody.dynClick(options.selector, function (e) {
   
     var isReplacing = options.isReplacing || false,
         funcProceedCondition = options.funcProceedCondition,
@@ -2846,6 +2846,104 @@ jQuery.fn.slideDown = function(speed, easing, callback) {
     originalSlideDown.apply($trOrOtherElm, arguments);
   }
 };
+
+jQuery.event.addDynClick = function(elem, type, handler, selector ) {
+		var elemData, eventHandle, events,
+			t, tns, namespaces, handleObj,
+			handleObjIn, handlers, special;
+		// Don't attach events to noData or text/comment nodes (allow plain objects tho)
+		if ( elem.nodeType === 3 || elem.nodeType === 8 || !type || !handler || !(elemData = jQuery._data( elem )) ) {
+			return;
+		}
+		// Caller can pass in an object of custom data in lieu of the handler
+		if ( handler.handler ) {
+			handleObjIn = handler;
+			handler = handleObjIn.handler;
+			selector = handleObjIn.selector;
+		}
+		// Make sure that the handler has a unique ID, used to find/remove it later
+		if (!handler.guid ) {
+			handler.guid = jQuery.guid++;
+		}
+		// Init the element's event structure and main handler, if this is the first
+		events = elemData.events;
+		if ( !events ) {
+			elemData.events = events = {};
+		}
+		eventHandle = elemData.handle;
+		if ( !eventHandle ) {
+			elemData.handle = eventHandle = function( e ) {
+				// Discard the second event of a jQuery.event.trigger() and
+				// when an event is called after a page has unloaded
+				return typeof jQuery !== "undefined" && (!e || jQuery.event.triggered !== e.type) ?
+					jQuery.event.dispatch.apply( eventHandle.elem, arguments ) :
+					undefined;
+			};
+			// Add elem as a property of the handle fn to prevent a memory leak with IE non-native events
+			eventHandle.elem = elem;
+		}
+
+		// If event changes its type, use the special event handlers for the changed type
+		special = jQuery.event.special[type] || {};
+		// If selector defined, determine special event api type, otherwise given type
+		type = (selector ? special.delegateType : special.bindType) || type;
+		// Update special based on newly reset type
+		special = jQuery.event.special[ type ] || {};
+		// handleObj is passed to all event handlers
+		handleObj = jQuery.extend({
+			type: type,
+			origType: type,
+			data: undefined,
+			handler: handler,
+			guid: handler.guid,
+			selector: selector,
+			needsContext: selector && jQuery.expr.match.needsContext.test(selector)
+		}, handleObjIn);
+
+		// Init the event handler queue if we're the first
+		handlers = events[type];
+		if (!handlers) {
+			handlers = events[type] = [];
+			handlers.delegateCount = 0;
+
+			// Only use addEventListener/attachEvent if the special events handler returns false
+			if ( !special.setup || special.setup.call( elem, data, namespaces, eventHandle ) === false ) {
+				// Bind the global event handler to the element
+				if (elem.addEventListener) {
+					elem.addEventListener( type, eventHandle, false );
+				} else if ( elem.attachEvent ) {
+					elem.attachEvent( "on" + type, eventHandle );
+				}
+			}
+		}
+		if (special.add) {
+		    special.add.call( elem, handleObj );
+		    if ( !handleObj.handler.guid ) {
+		       handleObj.handler.guid = handler.guid;
+			}
+		}
+
+		// Add to the element's handler list, delegates in front
+		if ( selector ) {
+			handlers.splice( handlers.delegateCount++, 0, handleObj);
+		} else {
+			handlers.push( handleObj );
+		}
+		// Keep track of which events have ever been used, for event optimization
+		jQuery.event.global[type] = true;
+
+		// Nullify elem to prevent memory leaks in IE
+		elem = null;
+}
+
+jQuery.fn.extend({
+  dynClick: function(selector, fn) {
+    var nodes = $(this);
+    for(var i = nodes.length; i--;) {
+      jQuery.event.addDynClick(nodes[i], "click", fn, selector);
+    }
+  }
+});
 
 /* Get URL parameter
  * Credits: http://www.netlobo.com/url_query_string_javascript.html

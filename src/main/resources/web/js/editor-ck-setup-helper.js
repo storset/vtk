cf1442296e70b8aafab0d6a0796ba490743a5fcf
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-10-19 09:18:42 +0000

    VTK-3040 - see if custom formatting has anything with the div container and curstor jumping out in IE

--- src/main/resources/web/js/editor-ck-setup-helper.js
+++ src/main/resources/web/js/editor-ck-setup-helper.js
@@ -185,35 +185,7 @@ function setCKEditorConfig(name, linkBrowseUrl, imageBrowseUrl, flashBrowseUrl, 
 
   config.disableNativeSpellChecker = false;
 
-  // Configure tag formatting in source
-  config.on = {
-    instanceReady: function (ev) {
 
-      var tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-
-      for (var key in tags) {
-        this.dataProcessor.writer.setRules(tags[key], {
-          indent: false,
-          breakBeforeOpen: true,
-          breakAfterOpen: false,
-          breakBeforeClose: false,
-          breakAfterClose: true
-        });
-      }
-
-      tags = ['ol', 'ul', 'li'];
-
-      for (key in tags) {
-        this.dataProcessor.writer.setRules(tags[key], {
-          indent: true,
-          breakBeforeOpen: true,
-          breakAfterOpen: false,
-          breakBeforeClose: false,
-          breakAfterClose: true
-        });
-      }
-    }
-  }
 
   CKEDITOR.replace(name, config);
 }

022de553f4e06b0b0d58ac80613ed4bb3b812e07
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-10-19 13:56:13 +0000

    VTK-3048 - show/hide fields based on choosen semester

--- src/main/resources/web/js/study-admin-enhancements.js
+++ src/main/resources/web/js/study-admin-enhancements.js
@@ -16,6 +16,21 @@ function hideShowStudy(typeToDisplay) {
   }
 }
 
+function hideShowSemester(typeSemester, str) {
+  var container = $("#editor");
+  switch (typeSemester) { // TODO: possible use container.attr("class", "").addClass(""); instead
+    case "bestemt-semester":
+      container.removeClass(str + "-annet").addClass(str + "-bestemt");
+      break;
+    case "annet":
+      container.removeClass(str + "-bestemt").addClass(str + "-annet");
+      break;
+    default:
+      container.removeClass(str + "-annet").removeClass(str + "-bestemt");
+      break;
+  }
+}
+
 function replaceTag(selector, tag, replaceTag) {
   selector.find(tag).replaceWith(function() {
     return "<" + replaceTag + ">" + $(this).text() + "</" + replaceTag + ">";
@@ -42,12 +57,37 @@ $(document).ready(function () {
                              active: false
                            });
     $(".ui-accordion > .vrtx-string:visible:last").addClass("last");
+    
+    $(document).on("change", "#typeToDisplay", function () {
+      hideShowStudy($(this).val());
+      $(".ui-accordion > .vrtx-string.last").removeClass("last");
+      $(".ui-accordion > .vrtx-string:visible:last").addClass("last");
+    });
   }
-  $(document).on("change", "#typeToDisplay", function () {
-    hideShowStudy($(this).val());
-    $(".ui-accordion > .vrtx-string.last").removeClass("last");
-    $(".ui-accordion > .vrtx-string:visible:last").addClass("last");
-  });
+  
+  // Course description - hide/show semesters
+  var typeSemesterUndervisning = $("#undervisningssemester");
+  if(typeSemesterUndervisning.length) {
+    try {
+      hideShowSemester(typeSemesterUndervisning.val(), "undervisning");
+    } catch (err) {
+      vrtxAdmin.error({msg: err});
+    }
+    $(document).on("change", "#undervisningssemester", function () {
+      hideShowSemester($(this).val(), "undervisning");
+    });
+  }
+  var typeSemesterEksamen = $("#eksamenssemester");
+  if(typeSemesterEksamen.length) {
+    try {
+      hideShowSemester(typeSemesterEksamen.val(), "eksamen");
+    } catch (err) {
+      vrtxAdmin.error({msg: err});
+    }
+    $(document).on("change", "#eksamenssemester", function () {
+      hideShowSemester($(this).val(), "eksamen");
+    });
+  }
   
   // 'Samlet program'-document
   var samletElm = $(".samlet-element");

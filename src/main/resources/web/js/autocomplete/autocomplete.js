8e2e295acb9b668a5db8d77a83eb29287eb558c1
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-11-02 10:29:06 +0000

    VTK-3060 - formatting

--- src/main/resources/web/js/autocomplete/autocomplete.js
+++ src/main/resources/web/js/autocomplete/autocomplete.js
@@ -11,13 +11,13 @@
 function setAutoComplete(id, service, params) {
   // Default parameters, extended by 'params' if supplied
   var p = {
-    minChars :2,
-    multiple :true,
-    selectFirst :true,
-    max :20,
-    //extensions for person and cource
-    resultsBeforeScroll :0, //default behaviour when set to 0
-    cacheLength :10 //default
+    minChars: 2,
+    multiple: true,
+    selectFirst: true,
+    max: 20,
+    // Extensions for person and cource
+    resultsBeforeScroll: 0, //default behaviour when set to 0
+    cacheLength: 10 //default
   };
   if (params) {
     $.extend(p, params);

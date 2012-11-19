2ffad4b8692f00fb1a8dd5d29a78e1f75b5b5a0c
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-11-19 14:59:29 +0000

    VTK - a little larger textarea field in send to approval dialog

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -330,11 +330,11 @@ vrtxAdmin._$(document).ready(function () {
           _$("body").append("<div id='" + id + "'>" + _$(results).find("#contents").html() + "</div>");
           dialogManageCreate = $("#" + id);
           dialogManageCreate.hide();
-          vrtxSimpleDialogs.openHtmlDialog(dialogManageCreate.html(), link.title, 310, 415);
+          vrtxSimpleDialogs.openHtmlDialog(dialogManageCreate.html(), link.title, 410, 515);
         }
       });
     } else {
-      vrtxSimpleDialogs.openHtmlDialog(dialogManageCreate.html(), link.title, 310, 415);
+      vrtxSimpleDialogs.openHtmlDialog(dialogManageCreate.html(), link.title, 410, 515);
     }
     e.stopPropagation();
     e.preventDefault();

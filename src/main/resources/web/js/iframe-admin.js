307f80000d1ef281a512ca56683df90a29662662
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-11-23 09:54:19 +0000

    VTK-3099

--- src/main/resources/web/js/iframe-admin.js
+++ src/main/resources/web/js/iframe-admin.js
@@ -2,9 +2,19 @@
  *  Not essential functionality. Only works in browsers which support postMessage.
  *
  */
+ 
+(function ($) {
 
-var appContentHeight, appWrapperHeight, appFooterHeight, windowHeight, previewIframeMinHeight;
-var surplusAnimationSpeed = 200;
+var appContentHeight,
+    appWrapperHeight,
+    appFooterHeight,
+    windowHeight,
+    previewIframeMinHeight,
+    appContent,
+    main,
+    contents,
+    previewLoading,
+    surplusAnimationSpeed = 200;
 
 var crossDocComLink = new CrossDocComLink();
 crossDocComLink.setUpReceiveDataHandler(function(cmdParams, source) {
@@ -24,17 +34,23 @@ crossDocComLink.setUpReceiveDataHandler(function(cmdParams, source) {
         }
         var diff = newHeight - previewIframeMinHeight;
         var surplus = appFooterHeight + 20 + 12;
-        var h = $("#app-content").height();
+        var h = appContent.height();
         if(diff > surplus) {
-          $("#app-content").animate({height: (h + surplus) + "px"}, surplusAnimationSpeed);
-          $("#main, #contents, #preview-loading").animate({height: (newHeight + surplus) + "px"}, surplusAnimationSpeed, function() {
-            $("#preview-loading").remove();
-            $("#main, #contents, #app-content").removeAttr('style');
+          appContent.animate({height: (h + surplus) + "px"}, surplusAnimationSpeed);
+          contents.animate({height: (newHeight + surplus) + "px"}, surplusAnimationSpeed);
+          previewLoading.animate({height: (newHeight + surplus) + "px"}, surplusAnimationSpeed);
+          main.animate({height: (newHeight + surplus) + "px"}, surplusAnimationSpeed, function() {
+            previewLoading.remove();
+            appContent.removeAttr('style');
+            main.removeAttr('style');
+            contents.removeAttr('style');
             previewIframe.style.height = newHeight + "px";
           });  
         } else { // TODO
-          $("#preview-loading").remove();
-          $("#main, #contents, #app-content").removeAttr('style');
+          previewLoading.remove();
+          appContent.removeAttr('style');
+          main.removeAttr('style');
+          contents.removeAttr('style');
           previewIframe.style.height = newHeight + "px";
         }
       }
@@ -46,24 +62,26 @@ crossDocComLink.setUpReceiveDataHandler(function(cmdParams, source) {
 
 $(document).ready(function() {
   if($("#vrtx-preview").length) {
-    appContentHeight = $("#app-content").height();
-    appWrapperHeight = $("#app-head-wrapper").height();
-    appFooterHeight = $("#app-footer").outerHeight(true);
+    var body = $("body");
+    appContent = body.find("#app-content");
+    main = appContent.find("#main");
+    contents = main.find("#contents");
+    
+    appContentHeight = appContent.height();
+    appWrapperHeight = body.find("#app-head-wrapper").height();
+    appFooterHeight = body.find("#app-footer").outerHeight(true);
     windowHeight = $(window).height();
     previewIframeMinHeight = (windowHeight - (appContentHeight + appWrapperHeight + appFooterHeight)) + 150; //+ iframe default height
 
-    $("#main, #contents").css({ height: previewIframeMinHeight + "px" });
-        $("#app-content").css({ height: ((appContentHeight + previewIframeMinHeight) - 150 - 38 - 12 - 4) + "px" }); // TODO
-    $("#contents").append("<span id='preview-loading' />")
-                         .css({ position: "relative" });
-    $("#preview-loading").css({
-      position: "absolute",
-      top: 0,
-      left: 0,
-      backgroundColor: "#fff",
-      display: "block",
-      width: "100%",
-      height: previewIframeMinHeight + "px"
-    });
+    appContent.css({ height: ((appContentHeight + previewIframeMinHeight) - 150 - 38 - 12 - 4) + "px" }); // TODO
+          main.css({ height: previewIframeMinHeight + "px" });
+    contents.append("<span id='preview-loading' />")
+              .css({ position: "relative",
+                     height: previewIframeMinHeight + "px" });
+                     
+    previewLoading = contents.find("#preview-loading");
+    previewLoading.css({ height: previewIframeMinHeight + "px" });
   }
 });
+
+}(jQuery));

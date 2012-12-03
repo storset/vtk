d18b9ba29d523ab0966243e0be1bc68a4074e0bf
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-12-03 11:48:49 +0000

    VTK-3115 - add todo

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -359,6 +359,7 @@ vrtxAdmin._$(document).ready(function () {
           dialogManageCreate = $("#" + id);
           dialogManageCreate.hide();
           vrtxSimpleDialogs.openHtmlDialog("send-approval", dialogManageCreate.html(), link.title, 410, 525);
+          // TODO: need to save and send to view after approved
           if($(".ui-dialog #emailToField").val().length > 0) {
             $(".ui-dialog #yourCommentTxtArea")[0].focus();
           }

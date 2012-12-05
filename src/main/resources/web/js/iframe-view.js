e84821e3dd747c94ceb8e846529d9f43faf12482
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-12-05 20:38:20 +0000

    VTK-3121 - preview popup (window === top)

--- src/main/resources/web/js/iframe-view.js
+++ src/main/resources/web/js/iframe-view.js
@@ -8,38 +8,40 @@
  *  - Resizing the outer iframe (served from the admin domain) only works on browsers which support postMessage
  */
  
-var crossDocComLink = new CrossDocComLink();
-crossDocComLink.setUpReceiveDataHandler(function(cmdParams, source) {
-  switch(cmdParams[0]) {
-    case "admin-min-height":
-      var setHeight = (cmdParams.length === 2) ? cmdParams[1] : 450;
+if(window !== top) {
+  var crossDocComLink = new CrossDocComLink();
+  crossDocComLink.setUpReceiveDataHandler(function(cmdParams, source) {
+    switch(cmdParams[0]) {
+      case "admin-min-height":
+        var setHeight = (cmdParams.length === 2) ? cmdParams[1] : 450;
       
-      var previewViewIframe = $("iframe#previewViewIframe");
-      var iframe = previewViewIframe[0];
-      try {
-        if(typeof iframe.contentWindow !== "undefined" && typeof iframe.contentWindow.document !== "undefined" && typeof iframe.contentWindow.document.body !== "undefined") {
-          var computedHeight = Math.ceil(iframe.contentWindow.document.body.offsetHeight) + 45;
-          if(computedHeight > setHeight) {
-            setHeight = computedHeight;
-            crossDocComLink.postCmdToParent("preview-height|" + setHeight);
-          } else { // Computed height is less than or below minimum height
+        var previewViewIframe = $("iframe#previewViewIframe");
+        var iframe = previewViewIframe[0];
+        try {
+          if(typeof iframe.contentWindow !== "undefined" && typeof iframe.contentWindow.document !== "undefined" && typeof iframe.contentWindow.document.body !== "undefined") {
+            var computedHeight = Math.ceil(iframe.contentWindow.document.body.offsetHeight) + 45;
+            if(computedHeight > setHeight) {
+              setHeight = computedHeight;
+              crossDocComLink.postCmdToParent("preview-height|" + setHeight);
+            } else { // Computed height is less than or below minimum height
+              crossDocComLink.postCmdToParent("preview-keep-min-height");
+            }
+          } else {
             crossDocComLink.postCmdToParent("preview-keep-min-height");
           }
-        } else {
+          iframe.style.height = setHeight + "px";
+        } catch(e) { // Error
+          if(typeof console !== "undefined" && console.log) {
+            console.log("Error finding preview height: " + e.message);
+          }
+          iframe.style.height = setHeight + "px";
           crossDocComLink.postCmdToParent("preview-keep-min-height");
         }
-        iframe.style.height = setHeight + "px";
-      } catch(e) { // Error
-        if(typeof console !== "undefined" && console.log) {
-          console.log("Error finding preview height: " + e.message);
-        }
-        iframe.style.height = setHeight + "px";
-        crossDocComLink.postCmdToParent("preview-keep-min-height");
-      }
-      break;
-    default:
-  }
-}); 
+        break;
+      default:
+    }
+  });
+}
 
 // Notify parent when loaded
 $(document).ready(function () {
@@ -56,7 +58,23 @@ $(document).ready(function () {
       previewViewIframeElm.src = iSource;
     } 
     previewViewIframe.load(function() {
-      crossDocComLink.postCmdToParent("preview-loaded");
+      if(window !== top) {
+        crossDocComLink.postCmdToParent("preview-loaded");
+      } else {
+        var previewViewIframeElm = previewViewIframe[0];
+        try {
+          if(typeof previewViewIframeElm.contentWindow !== "undefined" && typeof previewViewIframeElm.contentWindow.document !== "undefined" && typeof previewViewIframeElm.contentWindow.document.body !== "undefined") {
+            previewViewIframeElm.style.height = (Math.ceil(previewViewIframeElm.contentWindow.document.body.offsetHeight) + 45) + "px";
+          } else {
+            previewViewIframeElm.style.height = ($(window).outerHeight(true) - $("h1").outerHeight(true) - 40) + "px";
+          }
+        } catch(e) { // Error
+          if(typeof console !== "undefined" && console.log) {
+            console.log("Error finding preview height: " + e.message);
+          }
+          previewViewIframeElm.style.height = ($(window).outerHeight(true) - $("h1").outerHeight(true) - 40) + "px";
+        }
+      }
     });
   } else {
     crossDocComLink.postCmdToParent("preview-keep-min-height");

6d0004c09608f9162f0fff50ba53fabf80d31b61
Øyvind Hatland <oyvihatl@usit.uio.no>
2012-12-10 14:29:02 +0000

    VTK - refactor/optimize JS for study enhancements alittle and fix markup on view for semester page (initial)

--- src/main/resources/web/js/study-admin-enhancements.js
+++ src/main/resources/web/js/study-admin-enhancements.js
@@ -11,58 +11,53 @@ $(document).ready(function () {
   // When ui-helper-hidden class is added => we need to add 'first'-class to next element (if it is not last and first of these)
   $(".ui-helper-hidden").filter(":not(:last)").filter(":first").next().addClass("first");
   // TODO: make sure these are NOT first so that we can use pure CSS
+  
+  var editor = $("#editor");
+  var docType = editor[0].className;
 
-  // 'How to search'-document
-  var typeToDisplay = $("#typeToDisplay"); 
-  if(typeToDisplay.length) { 
-    hideShowStudy(typeToDisplay);
+  if(docType === "vrtx-hvordan-soke") {
+    hideShowStudy(editor, $("#typeToDisplay"));
     $(document).on("change", "#typeToDisplay", function () {
-      hideShowStudy($(this));
-      $(".ui-accordion > .vrtx-string.last").removeClass("last");
-      $(".ui-accordion > .vrtx-string:visible:last").addClass("last");
+      hideShowStudy(editor, $(this));
+      editor.find(".ui-accordion > .vrtx-string.last").removeClass("last");
+      editor.find(".ui-accordion > .vrtx-string:visible:last").addClass("last");
     });    
-    
-     // Because accordion needs one content wrapper
-    for(var grouped = $(".vrtx-grouped"), i = grouped.length; i--;) { 
+     
+    // Because accordion needs one content wrapper
+    for(var grouped = editor.find(".vrtx-grouped"), i = grouped.length; i--;) { 
       $(grouped[i]).find("> *:not(.header)").wrapAll("<div />");
     }
-    $("#editor .properties").accordion({ header: "> div > .header",
-                             autoHeight: false,
-                             collapsible: true,
-                             active: false
-                           });
-    $(".ui-accordion > .vrtx-string:visible:last").addClass("last");
-  }
-  
-  // Course description - hide/show semesters
-  for(var semesters = ["teaching", "exam"], i = semesters.length, semesterId, semesterType; i--;) {
-    semesterId = "#" + semesters[i] + "semester";
-    semesterType = $(semesterId);
-    if(semesterType.length) {
-      hideShowSemester(semesterType);
-      $(document).on("change", semesterId, function () {
-        hideShowSemester($(this));
-      });
+    editor.find(".properties").accordion({ header: "> div > .header",
+                               autoHeight: false,
+                               collapsible: true,
+                               active: false
+                             });
+    editor.find(".ui-accordion > .vrtx-string:visible:last").addClass("last");
+    
+  } else if(docType === "vrtx-course-description") { 
+    for(var semesters = ["teaching", "exam"], i = semesters.length, semesterId, semesterType; i--;) {
+      semesterId = "#" + semesters[i] + "semester";
+      semesterType = $(semesterId);
+      if(semesterType.length) {
+        hideShowSemester(editor, semesterType);
+        $(document).on("change", semesterId, function () {
+          hideShowSemester(editor, $(this));
+        });
+      }
     }
-  }
-  setShowHide('course-fee', ["course-fee-amount"], false);
-  
-  // Semester page
-  if($("#editor.vrtx-semester-page").length) {
-    for(var grouped = $(".vrtx-grouped[class*=link-box]"), i = grouped.length; i--;) { 
+    setShowHide('course-fee', ["course-fee-amount"], false);
+  } else if(docType === "vrtx-semester-page") {
+    for(var grouped = editor.find(".vrtx-grouped[class*=link-box]"), i = grouped.length; i--;) { 
       $(grouped[i]).find("> *:not(.header)").wrapAll("<div />");
     }
     grouped.wrapAll("<div id='link-boxes' />");
-    $("#editor #link-boxes").accordion({ header: "> div[class*=link-box] > .header",
-                             autoHeight: false,
-                             collapsible: true,
-                             active: false
-                           });
-  }
-  
-  // 'Samlet program'-document
-  var samletElm = $(".samlet-element");
-  if(samletElm.length) {
+    editor.find("#link-boxes").accordion({ header: "> div > .header",
+                                     autoHeight: false,
+                                     collapsible: true,
+                                     active: false
+                                   });
+  } else if(docType === "vrtx-samlet-program") {
+    var samletElm = editor.find(".samlet-element");
     replaceTag(samletElm, "h6", "strong");
     replaceTag(samletElm, "h5", "h6");  
     replaceTag(samletElm, "h4", "h5");
@@ -72,8 +67,7 @@ $(document).ready(function () {
   }
 });
 
-function hideShowStudy(typeToDisplayElem) {
-  var container = $("#editor");
+function hideShowStudy(container, typeToDisplayElem) {
   switch (typeToDisplayElem.val()) {
     case "so":
       container.removeClass("nm").removeClass("em").addClass("so");
@@ -90,8 +84,7 @@ function hideShowStudy(typeToDisplayElem) {
   }
 }
 
-function hideShowSemester(typeSemesterElem) {
-  var container = $("#editor");
+function hideShowSemester(container, typeSemesterElem) {
   var prefix = typeSemesterElem.attr("id") + "-selected";
   switch (typeSemesterElem.val()) {
     case "particular-semester":

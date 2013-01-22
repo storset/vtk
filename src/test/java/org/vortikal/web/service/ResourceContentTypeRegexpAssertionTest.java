76f00ec6ad11848b768705ffa8c43d938def0159
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-01-22 15:23:17 +0000

    VTK-3070 - test for all MSOffice 2007+ formats + old (ms-word, ms-excel and ms-powerpoint)

--- src/test/java/org/vortikal/web/service/ResourceContentTypeRegexpAssertionTest.java
+++ src/test/java/org/vortikal/web/service/ResourceContentTypeRegexpAssertionTest.java
@@ -20,25 +20,57 @@ public class ResourceContentTypeRegexpAssertionTest extends TestCase {
         p = new MockPrincipalImpl("user", Type.USER);
     }
 
+    /* MSOffice 2007+ http://filext.com/faq/office_mime_types.php */
+    
     @Test
     public void testResourceWordMacro() {
-        String testContentType = "application/vnd.ms-word.document.macroEnabled.12";
-        String pattern = "application/(msword|vnd\\.ms-word(\\.(document|(document|template)\\.macroEnabled\\.12))|vnd\\.openxmlformats-officedocument\\.wordprocessingml\\.(document|template))";
-        assertTrue(matchTestContentTypeWithPattern(testContentType, pattern));
+        String[] testContentTypes = {"application/msword",
+                                     "application/vnd.ms-word",
+                                     "application/vnd.ms-word.document",
+                                     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                                     "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+                                     "application/vnd.ms-word.document.macroEnabled.12",
+                                     "application/vnd.ms-word.template.macroEnabled.12"};
+        
+        String pattern = "application/(msword|vnd\\.ms-word(|\\.(document|(document|template)\\.macroEnabled\\.12))|vnd\\.openxmlformats-officedocument\\.wordprocessingml\\.(document|template))";
+        assertTrue(matchTestContentTypeWithPattern(testContentTypes, pattern));
+    }
+
+    @Test
+    public void testResourceExcelMacro() {
+        String[] testContentTypes = {"application/vnd.ms-excel",
+                                     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                     "application/vnd.openxmlformats-officedocument.spreadsheetml.template",
+                                     "application/vnd.ms-excel.sheet.macroEnabled.12",
+                                     "application/vnd.ms-excel.template.macroEnabled.12",
+                                     "application/vnd.ms-excel.addin.macroEnabled.12",
+                                     "application/vnd.ms-excel.sheet.binary.macroEnabled.12"};
+
+        String pattern = "application/(vnd\\.ms-excel(|\\.(sheet(|\\.binary)|template|addin|)\\.macroEnabled\\.12)|x-msexcel|vnd\\.openxmlformats-officedocument\\.spreadsheetml\\.(sheet|template))";
+        assertTrue(matchTestContentTypeWithPattern(testContentTypes, pattern));
     }
     
     @Test
     public void testResourcePowerpointMacro() {
-        String testContentType = "application/vnd.ms-powerpoint.presentation.macroEnabled.12";
-        String pattern = "application/(ms-ppt|vnd\\.ms-powerpoint(\\.(addin|presentation|template|slideshow)\\.macroEnabled\\.12)|vnd\\.openxmlformats-officedocument\\.presentationml\\.(presentation|template|slideshow))";
-        assertTrue(matchTestContentTypeWithPattern(testContentType, pattern));
+        String[] testContentTypes = {"application/ms-ppt",
+                                     "application/vnd.ms-powerpoint",
+                                     "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                                     "application/vnd.openxmlformats-officedocument.presentationml.template",
+                                     "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+                                     "application/vnd.ms-powerpoint.addin.macroEnabled.12",
+                                     "application/vnd.ms-powerpoint.presentation.macroEnabled.12",
+                                     "application/vnd.ms-powerpoint.template.macroEnabled.12",
+                                     "application/vnd.ms-powerpoint.slideshow.macroEnabled.12"};
+        
+        String pattern = "application/(ms-ppt|vnd\\.ms-powerpoint(|\\.(addin|presentation|template|slideshow)\\.macroEnabled\\.12)|vnd\\.openxmlformats-officedocument\\.presentationml\\.(presentation|template|slideshow))";
+        assertTrue(matchTestContentTypeWithPattern(testContentTypes, pattern));
     }
     
-    @Test
-    public void testResourceExcelMacro() {
-        String testContentType = "application/vnd.ms-excel.sheet.macroEnabled.12";
-        String pattern = "application/(vnd\\.ms-excel(\\.(sheet(|\\.binary)|template|addin|)\\.macroEnabled\\.12)|x-msexcel|vnd\\.openxmlformats-officedocument\\.spreadsheetml\\.(sheet|template))";
-        assertTrue(matchTestContentTypeWithPattern(testContentType, pattern));
+    private boolean matchTestContentTypeWithPattern(String[] testContentTypes, String pattern) {
+        for(String testContentType : testContentTypes) {
+            if(!matchTestContentTypeWithPattern(testContentType, pattern)) return false;
+        }
+        return true;
     }
     
     private boolean matchTestContentTypeWithPattern(String testContentType, String pattern) {

3bff5ab0e929274487602f906934c1f87deffcf2
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-01-22 14:48:43 +0000

    VTK-3070 - refactor test

--- src/test/java/org/vortikal/web/service/ResourceContentTypeRegexpAssertionTest.java
+++ src/test/java/org/vortikal/web/service/ResourceContentTypeRegexpAssertionTest.java
@@ -6,45 +6,48 @@ import org.junit.Test;
 import org.vortikal.repository.Path;
 import org.vortikal.repository.ResourceImpl;
 import org.vortikal.security.InvalidPrincipalException;
+import org.vortikal.security.Principal;
 import org.vortikal.security.Principal.Type;
 import org.vortikal.security.PrincipalImpl;
 
 public class ResourceContentTypeRegexpAssertionTest extends TestCase {
+    
+    private ResourceContentTypeRegexpAssertion r;
+    private Principal p;
+    
+    public ResourceContentTypeRegexpAssertionTest() {
+        r = new ResourceContentTypeRegexpAssertion();
+        p = new MockPrincipalImpl("user", Type.USER);
+    }
 
     @Test
     public void testResourceWordMacro() {
         String testContentType = "application/vnd.ms-word.document.macroEnabled.12";
         String pattern = "application/(msword|vnd\\.ms-word(\\.(document|(document|template)\\.macroEnabled\\.12))|vnd\\.openxmlformats-officedocument\\.wordprocessingml\\.(document|template))";
-        
-        ResourceContentTypeRegexpAssertion r = new ResourceContentTypeRegexpAssertion();
-        r.setPattern(pattern);
-        MockResource mr = new MockResource(testContentType);
-        assertTrue(r.matches(mr, new MockPrincipalImpl("user", Type.USER)));
+        assertTrue(matchTestContentTypeWithPattern(testContentType, pattern));
     }
     
     @Test
     public void testResourcePowerpointMacro() {
         String testContentType = "application/vnd.ms-powerpoint.presentation.macroEnabled.12";
         String pattern = "application/(ms-ppt|vnd\\.ms-powerpoint(\\.(addin|presentation|template|slideshow)\\.macroEnabled\\.12)|vnd\\.openxmlformats-officedocument\\.presentationml\\.(presentation|template|slideshow))";
-        
-        ResourceContentTypeRegexpAssertion r = new ResourceContentTypeRegexpAssertion();
-        r.setPattern(pattern);
-        MockResource mr = new MockResource(testContentType);
-        assertTrue(r.matches(mr, new MockPrincipalImpl("user", Type.USER)));
+        assertTrue(matchTestContentTypeWithPattern(testContentType, pattern));
     }
     
     @Test
     public void testResourceExcelMacro() {
         String testContentType = "application/vnd.ms-excel.sheet.macroEnabled.12";
         String pattern = "application/(vnd\\.ms-excel(\\.(sheet(|\\.binary)|template|addin|)\\.macroEnabled\\.12)|x-msexcel|vnd\\.openxmlformats-officedocument\\.spreadsheetml\\.(sheet|template))";
-        
-        ResourceContentTypeRegexpAssertion r = new ResourceContentTypeRegexpAssertion();
+        assertTrue(matchTestContentTypeWithPattern(testContentType, pattern));
+    }
+    
+    private boolean matchTestContentTypeWithPattern(String testContentType, String pattern) {
         r.setPattern(pattern);
-        MockResource mr = new MockResource(testContentType);
-        assertTrue(r.matches(mr, new MockPrincipalImpl("user", Type.USER)));
+        return r.matches(new MockResource(testContentType), p);
     }
 
 }
+
 
 @SuppressWarnings("serial")
 class MockPrincipalImpl extends PrincipalImpl {

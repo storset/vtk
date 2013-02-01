9ee0a95420087ce594adcee402dde7dc15f4a0be
Øyvind Stegard <oyviste@usit.uio.no>
2013-02-01 15:12:08 +0000

    Document and some small clean-ups. Will move and re-use this class for other things besides decorating config.

--- src/main/java/org/vortikal/web/decorating/PathMappingConfig.java
+++ src/main/java/org/vortikal/web/decorating/PathMappingConfig.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008, University of Oslo, Norway
+/* Copyright (c) 2008-2013, University of Oslo, Norway
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -41,21 +41,84 @@ import java.util.Map;
 
 import org.vortikal.repository.Path;
 
-
+/**
+ * Configuration parser for URI/path mapping configuration style files.
+ *
+ * <p>
+ * Parses a line-based configuration file on the format:
+ * <pre>
+ *    / = VALUE1                   # A comment ..
+ *    /a[p1=x,p2=y] = VALUE2
+ *    /a/b = VALUE3
+ *    /a/b/c/ = VALUE4             # exact match hint for path
+ *
+ *    # another comment
+ *    /a/b/c[p1=x]/ = VALUE5       # exact match hint AND predicate
+ * </pre> where paths are mapped to a list of predicates and a single VALUE.
+ * Empty lines and lines not beginning with "/" are ignored.
+ *
+ * <p>
+ * After parsing, the configuration will be organized into a hierarchy
+ * corresponding to the paths present, and it can be queried with
+ * {@link #get(org.vortikal.repository.Path)}.
+ * <p>
+ * When matching an input path against configuration, it will return the config
+ * entries for the most specific configured path that either exactly matches the
+ * input path or is an ancestor path.
+ * <p>
+ * The same path may occur in configuration multiple times, but with different
+ * set of predicates and values. A single path can map to zero or more
+ * {@link ConfigEntry} instances, each with list of predicate name-value-pairs,
+ * a single VALUE and a flag indicating that exact path matching is desired.
+ * <p>
+ * It is up to client code how to interpret the configuration entries that
+ * matchfor a given path - this class is merely a parser that understands path
+ * hierarchies and the syntax described.
+ */
 public class PathMappingConfig {
-    
+
     private Node root = new Node();
     private int maxLineSize = 300;
     private int maxLines = 10000;
-    
+
+    /**
+     * Construct a configuration instance from an input stream.
+     * 
+     * @param source the input stream to read configuration lines from.
+     * @throws Exception in case of parsing errors.
+     */
     public PathMappingConfig(InputStream source) throws Exception {
         load(source);
     }
-    
+
+    /**
+     * Construct a configuration instance from an input stream.
+     * 
+     * @param source the input stream to read configuration lines from. The stream
+     *               will be closed after parsing.
+     * @param maxLineSize max size of lines in configuration source.
+     * @param maxLines max number of lines read from configuration source.
+     * @throws Exception in case of parsing errors.
+     */
+    public PathMappingConfig(InputStream source, int maxLineSize, int maxLines) throws Exception {
+        if (maxLineSize <= 0) throw new IllegalArgumentException("maxLineSize must be > 0");
+        if (maxLines <= 0) throw new IllegalArgumentException("maxLines must be > 0");
+        
+        this.maxLineSize = maxLineSize;
+        this.maxLines = maxLines;
+        
+        load(source);
+    }
+
+    /**
+     * Returns all config entries that apply for the given input path.
+     */
     public List<ConfigEntry> get(Path path) {
         Node n = root;
-        for (String name: path.getElements()) {
-            if (name.equals("/")) continue;
+        for (String name : path.getElements()) {
+            if (name.equals("/")) {
+                continue;
+            }
             if (n.children != null) {
                 n = n.children.get(name);
             }
@@ -69,11 +132,11 @@ public class PathMappingConfig {
         return null;
     }
 
-    public void load(InputStream is) throws Exception {
+    private void load(InputStream is) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         try {
-            String line = null;
-            Node root = new Node();
+            String line;
+            Node r = new Node();
             int lineNum = 0;
             while ((line = reader.readLine()) != null) {
                 if (++lineNum > this.maxLines) {
@@ -89,10 +152,10 @@ public class PathMappingConfig {
                     continue;
                 }
                 if (line.startsWith("/")) {
-                    parseLine(line, root);
-                } 
+                    parseLine(line, r);
+                }
             }
-            this.root = root;
+            this.root = r;
         } finally {
             reader.close();
         }
@@ -113,7 +176,7 @@ public class PathMappingConfig {
         }
         buildNode(lhs, rhs, root);
     }
-    
+
     private void buildNode(String lhs, String rhs, Node root) {
         boolean exact = false;
         if (lhs.endsWith("/") && !lhs.equals("/")) {
@@ -133,16 +196,16 @@ public class PathMappingConfig {
         }
         String pathStr = lhs.substring(0, pathEndPos);
         pathStr = pathStr.replaceAll("/+", "/");
-        
-        String qualifierStr = leftBracketPos == -1 ? null :
-            lhs.substring(leftBracketPos + 1, rightBracketPos);
+
+        String qualifierStr = leftBracketPos == -1 ? null
+                : lhs.substring(leftBracketPos + 1, rightBracketPos);
         List<Predicate> qualifiers = new ArrayList<Predicate>();
         if (qualifierStr != null) {
             String[] splitQualifiers = qualifierStr.split(",");
-            for (String qualifier: splitQualifiers) {
+            for (String qualifier : splitQualifiers) {
                 qualifier = qualifier.trim();
                 int colonIdx = qualifier.indexOf(":");
-                if (colonIdx == 0 || colonIdx == -1 
+                if (colonIdx == 0 || colonIdx == -1
                         || colonIdx == qualifier.length() - 1) {
                     return;
                 }
@@ -154,7 +217,7 @@ public class PathMappingConfig {
 
         Path path = Path.fromString(pathStr);
         Node n = root;
-        for (String name: path.getElements()) {
+        for (String name : path.getElements()) {
             if (name.equals("/")) {
                 continue;
             }
@@ -171,24 +234,24 @@ public class PathMappingConfig {
         n.entries.add(new ConfigEntry(qualifiers, rhs, exact));
     }
 
-        
     private static class Node {
+
         private Map<String, Node> children = new HashMap<String, Node>();
         private List<ConfigEntry> entries = null;
-        
+
+        @Override
         public String toString() {
-           StringBuilder sb = new StringBuilder();
-           sb.append("(");
-           for (String s: this.children.keySet()) {
-               sb.append(s).append("=").append(this.children.get(s));
-               sb.append(",");
-           }
-           sb.append(")");
-           return sb.toString();
+            StringBuilder sb = new StringBuilder();
+            sb.append("(");
+            for (String s : this.children.keySet()) {
+                sb.append(s).append("=").append(this.children.get(s));
+                sb.append(",");
+            }
+            sb.append(")");
+            return sb.toString();
         }
     }
-    
-    
+
     private String stripComments(String line) {
         if (line.startsWith("#")) {
             return "";
@@ -204,7 +267,14 @@ public class PathMappingConfig {
         return result.toString();
     }
 
+    /**
+     * A config entry represents a single line in the configuration file. Note
+     * multiple lines may have the same path mapping, and thus there can be
+     * multiple instances of this class for a single path as returned by
+     * {@link #get(org.vortikal.repository.Path) }.
+     */
     public static class ConfigEntry {
+
         private List<Predicate> predicates;
         private String value;
         private boolean exact;
@@ -214,19 +284,20 @@ public class PathMappingConfig {
             this.value = value;
             this.exact = exact;
         }
-        
+
         public List<Predicate> getPredicates() {
             return Collections.unmodifiableList(this.predicates);
         }
-        
+
         public String getValue() {
             return this.value;
         }
-        
+
         public boolean isExact() {
             return this.exact;
         }
-        
+
+        @Override
         public String toString() {
             StringBuilder sb = new StringBuilder(this.getClass().getName());
             sb.append(": predicates=").append(this.predicates);
@@ -235,8 +306,12 @@ public class PathMappingConfig {
             return sb.toString();
         }
     }
-    
+
+    /**
+     * A predicate is simply a generic name-value pair of strings.
+     */
     public static class Predicate {
+
         private String name;
         private String value;
 
@@ -244,15 +319,16 @@ public class PathMappingConfig {
             this.name = name;
             this.value = value;
         }
-        
+
         public String getName() {
             return this.name;
         }
-        
+
         public String getValue() {
             return this.value;
         }
-        
+
+        @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
             sb.append(this.name).append("=").append(this.value);

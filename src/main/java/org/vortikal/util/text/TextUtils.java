6b6bb55bb8c19b95a175a0cc7d308e4b2cbb74af
Øyvind Stegard <oyviste@usit.uio.no>
2013-02-05 02:15:49 +0000

    Add parsing functions for character-separated values and character-separated key-value pairs. Both support escaping and customization of escape handling through parsing flags. Trim support and selectable handling of empty values.

--- src/main/java/org/vortikal/util/text/TextUtils.java
+++ src/main/java/org/vortikal/util/text/TextUtils.java
@@ -30,12 +30,17 @@
  */
 package org.vortikal.util.text;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 import org.apache.commons.lang.WordUtils;
 
+/**
+ * Various text parsing utility functions.
+ */
 public class TextUtils {
 
     private static final char[] HEX = {
@@ -51,6 +56,29 @@ public class TextUtils {
         }
         return result;
     }
+    
+    /**
+     * Parsing flag indicating that values should be trimmed.
+     */
+    public static final int TRIM = 0x01;
+    
+    /**
+     * Parsing flag indicating that empty string values should be discarded
+     * from results.
+     */
+    public static final int DISCARD = 0x02;
+    
+    /**
+     * Parsing flag indicating that illegal escape sequences should be ignored.
+     */
+    public static final int IGNORE_ILLEGAL_ESCAPE = 0x04;
+    
+    /**
+     * Parsing flag indicating that unescaped separators in value of key-value
+     * pair should not cause an exception to be thrown.
+     * @see #parseKeyValue(java.lang.String, char, int) 
+     */
+    public static final int IGNORE_UNESCAPED_SEP_IN_VALUE = 0x08;
     
     /**
      * Extracts a field from a string using the character <code>,</code> as field delimiter.
@@ -175,5 +203,197 @@ public class TextUtils {
             }
         }
         return noDupes.toString();
+    }
+
+    /**
+     * Parse a string of character-separated values, with no parsing flags set.
+     * @see #parseCsv(java.lang.String, char, int) 
+     */
+    public static String[] parseCsv(String input, char sep) {
+        return parseCsv(input, sep, 0);
+    }
+    
+    /**
+     * Parse a string of character-separated values, optionally trimming each
+     * value. The chosen separator character may be escaped using backslash '\\',
+     * and any backslashes need to be escaped themselves.
+     * 
+     * Empty strings between consecutive separator characters are included
+     * in results unless <code>discard</code> is <code>true</code>.
+     * 
+     * @param input the input string to parse.
+     * @param sep the character to use as value separator.
+     * @param flags Parsing flags.
+     *        <ul><li>If {@link #TRIM} is set, then each value to be trimmed.
+     *            <li>If {@link #DISCARD} is set, then empty string values
+     *        will not to be included in result. When combined with {@link #TRIM}, empty
+     *        values after trimming are discarded.
+     *            <li>If {@link #IGNORE_ILLEGAL_ESCAPE} is set, then illegal
+     *                backslash escape sequences are ignored.
+     *                The backslash character will be included in output
+     *                in such cases, preceding the non-special character.
+     *        </ul>
+     * @return an array of strings containing the values.
+     */
+    public static String[] parseCsv(String input, char sep, int flags) {
+        if (input == null) throw new IllegalArgumentException("null input");
+        final boolean trim = hasFlag(TRIM, flags);
+        final boolean discard = hasFlag(DISCARD, flags);
+        final boolean ignoreIllegalEscape = hasFlag(IGNORE_ILLEGAL_ESCAPE, flags);
+        
+        if (trim ? input.trim().isEmpty() : input.isEmpty()) {
+            if (discard) return new String[0];
+            else return new String[] {""};
+        }
+        
+        List<String> values = new ArrayList<String>();
+        boolean escape = false;
+        StringBuilder buf = new StringBuilder();
+        for (int i=0; i<input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '\\' && escape) {
+                buf.append('\\');
+                escape = false;
+                continue;
+            }
+            if (c == '\\') {
+                escape = true;
+                continue;
+            }
+            if (c == sep && escape) {
+                buf.append(c);
+                escape = false;
+                continue;
+            }
+            if (escape) {
+                if (ignoreIllegalEscape) {
+                    buf.append('\\');
+                    escape = false;
+                } else {
+                    throw new IllegalArgumentException("Illegal escape char: '\\" 
+                        + c + "' in value: " + input);
+                }
+            }
+            if (c == sep) {
+                String v = trim ? buf.toString().trim() : buf.toString();
+                buf.setLength(0);
+                if (!v.isEmpty() || !discard) {
+                    values.add(v);
+                }
+                if (i == input.length()-1) {
+                    // Input ended with separator, be consistent
+                    if (!discard) values.add("");
+                }
+                continue;
+            }
+            buf.append(c);
+        }
+        if (escape && !ignoreIllegalEscape) {
+            throw new IllegalArgumentException(
+                    "Illegal escape sequence '\\' at end of key-value: '" + input + "'");
+        }
+        if (buf.length() > 0) {
+            String v = trim ? buf.toString().trim() : buf.toString();
+            if (!v.isEmpty() || !discard) {
+                values.add(v);
+            }
+        }
+        
+        return values.toArray(new String[values.size()]);
+    }
+    
+    /**
+     * Parses key-value with no parsing flags set.
+     * @see #parseKeyValue(java.lang.String, char, int) 
+     */
+    public static String[] parseKeyValue(String input, char sep) {
+        return parseKeyValue(input, sep, 0);
+    }
+    
+    /**
+     * Parses a string into a key and a value using a single character
+     * as separator. The chosen separator character may be escaped using backslash
+     * character '\\'. If no separator is found in input, then the returned string
+     * array will have the entire input string as key part and <code>null</code>
+     * as value part.
+     * 
+     * @param input The input string to parse.
+     * @param sep The character to use as separator between key and value.
+     * @param flags Set parsing flags. Valid flags:
+     *        <ul><li>If {@link #TRIM} is set, then each value to be trimmed.
+     *            <li>If {@link #IGNORE_ILLEGAL_ESCAPE} is set, then
+     *                then illegal backslash escape sequences are ignored.
+     *                The backslash character will be included in output
+     *                in such cases, preceding the non-special character.
+     *            <li>If {@link #IGNORE_UNESCAPED_SEP_IN_VALUE} is set, then
+     *             do not throw exception if unescaped separator characters
+     *             are encountered in the value part.
+     *        </ul>
+
+     * @return A string array with position 0 containing the key and position
+     *         1 containing the value.
+     */
+    public static String[] parseKeyValue(String input, char sep, int flags) {
+        boolean trim = hasFlag(TRIM, flags);
+        boolean ignoreIllegalEscape = hasFlag(IGNORE_ILLEGAL_ESCAPE, flags);
+        StringBuilder key = new StringBuilder();
+        StringBuilder value = null;
+        StringBuilder cur = key;
+        boolean escape = false;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (cur == value && value == null) {
+                value = new StringBuilder();
+                cur = value;
+            }
+            if (c == '\\' && escape) {
+                cur.append('\\');
+                escape = false;
+                continue;
+            }
+            if (c == '\\') {
+                escape = true;
+                continue;
+            }
+            if (c == sep && escape) {
+                cur.append(c);
+                escape = false;
+                continue;
+            }
+            if (c == sep && cur == value) {
+                if (!hasFlag(IGNORE_UNESCAPED_SEP_IN_VALUE, flags)) {
+                    throw new IllegalArgumentException(
+                        "Unescaped separator '" + sep + "' in  value: \"" + input + "\"");
+                }
+                cur.append(c);
+                continue;
+            }
+            if (escape) {
+                if (ignoreIllegalEscape) {
+                    escape = false;
+                    cur.append('\\');
+                } else {
+                    throw new IllegalArgumentException("Illegal escape char: '\\" 
+                        + c + "' in key-value: " + input);
+                }
+            }
+            if (c == sep) {
+                value = new StringBuilder();
+                cur = value;
+                continue;
+            }
+            cur.append(c);
+        }
+        if (escape && !ignoreIllegalEscape) {
+            throw new IllegalArgumentException(
+                    "Illegal escape sequence '\\' at end of key-value: '" + input + "'");
+        }
+        
+        return new String[] { trim ? key.toString().trim() : key.toString(), 
+            value == null ? null : trim ? value.toString().trim() : value.toString() };
+    }
+
+    private static boolean hasFlag(int flag, int flags) {
+        return (flag & flags) != 0;
     }
 }
---
+++ src/test/java/org/vortikal/util/text/TextUtilsTest.java
@@ -0,0 +1,261 @@
+/* Copyright (c) 2009, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.vortikal.util.text;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.fail;
+import junit.framework.TestCase;
+
+public class TextUtilsTest extends TestCase {
+
+    public void testRemoveDuplicatesIgnoreCase() {
+
+        // OLD: String testData = "Forskning, Røed Ødegård, forskning, FoRsKning, forskNING";
+        String testData = "oFFentlig helseVesen, jon blund, Offentlig helsevesen, Jon Blund";
+
+        String expectedData = "oFFentlig helseVesen, jon blund";
+        String expectedDataNoSpaces = "oFFentlig helseVesen,jon blund";
+        String expectedDataNoDelimiter = "oFFentlig helseVesen jon blund";
+        String expectedDataNoSpacesAndDelimiter = "oFFentlig helseVesenjon blund";
+        String expectedDataCapitalized = "OFFentlig HelseVesen, Jon Blund";
+
+        // Test noSpaces, noDelimiter boolean configurations
+        String test = TextUtils.removeDuplicatesIgnoreCase(testData, ",", false, false, false);
+        assertEquals(expectedData, test);
+
+        test = TextUtils.removeDuplicatesIgnoreCase(testData, ",", true, false, false);
+        assertEquals(expectedDataNoSpaces, test);
+
+        test = TextUtils.removeDuplicatesIgnoreCase(testData, ",", false, true, false);
+        assertEquals(expectedDataNoDelimiter, test);
+
+        test = TextUtils.removeDuplicatesIgnoreCase(testData, ",", true, true, false);
+        assertEquals(expectedDataNoSpacesAndDelimiter, test);
+
+        // Overload Methods
+        test = TextUtils.removeDuplicatesIgnoreCase(testData, ",");
+        assertEquals(expectedData, test);
+
+        test = TextUtils.removeDuplicatesIgnoreCaseCapitalized(testData, ",");
+        assertEquals(expectedDataCapitalized, test);
+
+    }
+    
+    public void testParseCsv() {
+        
+        String[] values = TextUtils.parseCsv("a,b,c", ',');
+        assertEquals(3, values.length);
+        assertEquals("a", values[0]);
+        assertEquals("b", values[1]);
+        assertEquals("c", values[2]);
+        
+        values = TextUtils.parseCsv(" First ; Second ; Third ;", ';');
+        assertEquals(4, values.length);
+        assertEquals(" First ", values[0]);
+        assertEquals(" Second ", values[1]);
+        assertEquals(" Third ", values[2]);
+        assertEquals("", values[3]);
+        
+        values = TextUtils.parseCsv(" First ; Second ; Third ;", ';', TextUtils.DISCARD);
+        assertEquals(3, values.length);
+        assertEquals(" First ", values[0]);
+        assertEquals(" Second ", values[1]);
+        assertEquals(" Third ", values[2]);
+
+        values = TextUtils.parseCsv(" First ; Second ; Third ;", ';', TextUtils.TRIM|TextUtils.DISCARD);
+        assertEquals(3, values.length);
+        assertEquals("First", values[0]);
+        assertEquals("Second", values[1]);
+        assertEquals("Third", values[2]);
+        
+        values = TextUtils.parseCsv("", ';');
+        assertEquals(1, values.length);
+        assertEquals("", values[0]);
+
+        values = TextUtils.parseCsv("", ';', TextUtils.DISCARD);
+        assertEquals(0, values.length);
+        
+        values = TextUtils.parseCsv(";;;;a value;;;;", ';', TextUtils.DISCARD);
+        assertEquals(1, values.length);
+        assertEquals("a value", values[0]);
+
+        values = TextUtils.parseCsv("  ;a value;  ", ';', TextUtils.TRIM);
+        assertEquals(3, values.length);
+        assertEquals("", values[0]);
+        assertEquals("a value", values[1]);
+        assertEquals("", values[2]);
+        
+        values = TextUtils.parseCsv("value\\;a;value\\;b;value\\\\c", ';');
+        assertEquals(3, values.length);
+        assertEquals("value;a", values[0]);
+        assertEquals("value;b", values[1]);
+        assertEquals("value\\c", values[2]);
+        
+        values = TextUtils.parseCsv("1 \t2 \t3 ", '\t');
+        assertEquals(3, values.length);
+        assertEquals("1 ", values[0]);
+        assertEquals("2 ", values[1]);
+        assertEquals("3 ", values[2]);
+        
+        values = TextUtils.parseCsv("1 \t2 \t3 ", '\t', TextUtils.TRIM);
+        assertEquals(3, values.length);
+        assertEquals("1", values[0]);
+        assertEquals("2", values[1]);
+        assertEquals("3", values[2]);
+        
+        try {
+            TextUtils.parseCsv("value\\a;value\\b", ';');
+            fail("Expected illegal escape exception");
+        } catch (IllegalArgumentException ia) {
+            // ok
+        }
+        
+        try {
+            values = TextUtils.parseCsv("\\\\value\\a;\\\\value\\b", ';', TextUtils.IGNORE_ILLEGAL_ESCAPE);
+            assertEquals(2, values.length);
+            assertEquals("\\value\\a", values[0]);
+            assertEquals("\\value\\b", values[1]);
+        } catch (IllegalArgumentException ia) {
+            fail("Unexpected illegal escape exception");
+        }
+        
+        try {
+            TextUtils.parseCsv("value a;value b\\", ';');
+            fail("Expected illegal escape exception");
+        } catch (IllegalArgumentException ia) {
+            // ok
+        }
+        
+        try {
+            values = TextUtils.parseCsv("value a;value b\\", ';', TextUtils.IGNORE_ILLEGAL_ESCAPE);
+            assertEquals(2, values.length);
+            assertEquals("value a", values[0]);
+            assertEquals("value b", values[1]);
+        } catch (IllegalArgumentException ia) {
+            fail("Unexpected illegal escape exception");
+        }
+    }
+    
+    public void testParseKeyValue() {
+        String[] kv = TextUtils.parseKeyValue("foo:bar", ':');
+        assertEquals(2, kv.length);
+        assertEquals("foo", kv[0]);
+        assertEquals("bar", kv[1]);
+        
+        kv = TextUtils.parseKeyValue("foobar:", ':');
+        assertEquals(2, kv.length);
+        assertEquals("foobar", kv[0]);
+        assertEquals("", kv[1]);
+        
+        kv = TextUtils.parseKeyValue(":foobar", ':');
+        assertEquals(2, kv.length);
+        assertEquals("", kv[0]);
+        assertEquals("foobar", kv[1]);
+        
+        kv = TextUtils.parseKeyValue(":", ':');
+        assertEquals(2, kv.length);
+        assertEquals("", kv[0]);
+        assertEquals("", kv[1]);
+        
+        kv = TextUtils.parseKeyValue("", ':');
+        assertEquals(2, kv.length);
+        assertEquals("", kv[0]);
+        assertNull(kv[1]);
+        
+        kv = TextUtils.parseKeyValue("foobar", ':');
+        assertEquals(2, kv.length);
+        assertEquals("foobar", kv[0]);
+        assertNull(kv[1]);
+        
+        kv = TextUtils.parseKeyValue("foo\\:bar:baz\\\\Z", ':');
+        assertEquals(2, kv.length);
+        assertEquals("foo:bar", kv[0]);
+        assertEquals("baz\\Z", kv[1]);
+        
+        kv = TextUtils.parseKeyValue("  a key  \t=  val\t", '=', TextUtils.TRIM);
+        assertEquals(2, kv.length);
+        assertEquals("a key", kv[0]);
+        assertEquals("val", kv[1]);
+
+        kv = TextUtils.parseKeyValue("  a key  \t=  val\t", '=');
+        assertEquals(2, kv.length);
+        assertEquals("  a key  \t", kv[0]);
+        assertEquals("  val\t", kv[1]);
+        
+        kv = TextUtils.parseKeyValue("  a key  \t=  \t", '=', TextUtils.TRIM);
+        assertEquals(2, kv.length);
+        assertEquals("a key", kv[0]);
+        assertEquals("", kv[1]);
+        
+        try {
+            TextUtils.parseKeyValue("\\a=b", '=');
+            fail("Expected illegal escape exception");
+        } catch (IllegalArgumentException ia) {
+            // ok
+        }
+        
+        try {
+            kv = TextUtils.parseKeyValue("\\a=b", '=', TextUtils.IGNORE_ILLEGAL_ESCAPE);
+            assertEquals(2, kv.length);
+            assertEquals("\\a", kv[0]);
+            assertEquals("b", kv[1]);
+        } catch (IllegalArgumentException ia) {
+            fail("Unexpected illegal escape exception");
+        }
+        
+        try {
+            TextUtils.parseKeyValue("a=b\\", '=', TextUtils.TRIM);
+            fail("Expected illegal escape exception");
+        } catch (IllegalArgumentException ia) {
+            // ok
+        }
+        
+        try {
+            kv = TextUtils.parseKeyValue("a= \\b\\ ", '=', TextUtils.TRIM | TextUtils.IGNORE_ILLEGAL_ESCAPE);
+            assertEquals(2, kv.length);
+            assertEquals("a", kv[0]);
+            assertEquals("\\b\\", kv[1]);
+        } catch (IllegalArgumentException ia) {
+            fail("Unexpected illegal escape exception");
+        }
+        
+        try {
+            kv = TextUtils.parseKeyValue("a = b = c = d", '=', TextUtils.IGNORE_UNESCAPED_SEP_IN_VALUE | TextUtils.TRIM);
+            assertEquals(2, kv.length);
+            assertEquals("a", kv[0]);
+            assertEquals("b = c = d", kv[1]);
+        } catch (IllegalArgumentException ia) {
+            fail("Unexpected illegal argument exception: " + ia.getMessage());
+        }
+        
+    }
+}

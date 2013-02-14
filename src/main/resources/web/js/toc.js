2ef15b64ff5ca74042a3123eff19543a41ef34c2
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-02-14 15:18:53 +0000

    VTK - cleanup toc and get rid of warnings (move function for gettings tags for <= IE7 at the bottom; can probably be removed (soon))

--- src/main/resources/web/js/toc.js
+++ src/main/resources/web/js/toc.js
@@ -5,41 +5,8 @@
 
 // Possible to override with a target for toc defined typical in document.ready() on a site / sites
 var tocTargetId = "";
-
-// This function is stolen (legally) from quirksmode.org (and modified by USIT)
-function getElementsByTagNames(list, obj) {
-  if (!obj) var obj = document;
-  var tagNames = list.split(',');
-  var resultArray = new Array();
-  var tagNamesLength = tagNames.length;
-  for (var i = 0; i < tagNames.length; i++) {
-    if (tocTargetId != "") {
-      var tags = obj.getElementById(tocTargetId).getElementsByTagName(tagNames[i]);
-    } else {
-      var tags = obj.getElementsByTagName(tagNames[i]);
-    }
-    var tagsLength = tags.length;
-    for (var j = 0; j < tagsLength; j++) {
-      resultArray.push(tags[j]);
-    }
-  }
-  var testNode = resultArray[0];
-  if (!testNode) return [];
-  if (testNode.sourceIndex) {
-    resultArray.sort(function (a, b) {
-      return a.sourceIndex - b.sourceIndex;
-    });
-  } else if (testNode.compareDocumentPosition) {
-    resultArray.sort(function (a, b) {
-      return 3 - (a.compareDocumentPosition(b) & 6);
-    });
-  }
-  return resultArray;
-}
-
-
 window.onload = function () {
-  new tocGen('toc')
+  new tocGen('toc');
 };
 
 //This script was originally written By Brady Mulhollem - WebTech101.com
@@ -50,25 +17,23 @@ function tocGen(writeTo) {
   this.writeOut = '';
   this.previous = 0;
   if (document.getElementById) {
-    //current requirements;
-    this.parentOb = document.getElementById(writeTo);
-
-    if (document.querySelectorAll) {
-      if (tocTargetId != "") {
-        var headers = document.getElementById(tocTargetId).querySelectorAll('h2,h3');
+    this.parentOb = document.getElementById(writeTo); // current requirements;
+    var headers = null;
+    if (document.querySelectorAll) { /* >= IE8 */
+      if (tocTargetId !== "") {
+        headers = document.getElementById(tocTargetId).querySelectorAll('h2,h3');
       } else {
-        var headers = document.querySelectorAll('h2,h3');
+        headers = document.querySelectorAll('h2,h3');
       }
     } else if (typeof (document.compareDocumentPosition) != 'undefined' || typeof (this.parentOb.sourceIndex) != 'undefined') {
-      var headers = getElementsByTagNames('h2,h3');
+      headers = getElementsByTagNames('h2,h3');
     } else {
-      var headers = getElementsByTagNames('h2');
+      headers = getElementsByTagNames('h2');
     }
 
-    if (headers.length > 0) {
-      var headerNr;
-      var headersLength = headers.length; //performance
-      for (var i = 0; i < headersLength; i++) {
+    if (headers !== null && headers.length > 0) {
+      var headerNr = null;
+      for (var i = 0, headersLength = headers.length; i < headersLength; i++) {
         headerNr = headers[i].nodeName.substr(1);
         if (headerNr > this.previous) {
           this.writeOut += '<ul>';
@@ -99,7 +64,7 @@ tocGen.prototype.addLink = function (ob) {
   var id = this.getId(ob);
   var link = '<li><a href="#' + id + '">' + ob.innerHTML + '<\/a>';
   this.writeOut += link;
-}
+};
 
 tocGen.prototype.getId = function (ob) {
   if (!ob.id) {
@@ -107,6 +72,37 @@ tocGen.prototype.getId = function (ob) {
     this.num++;
   }
   return ob.id;
+};
+
+// This function is stolen (legally) from quirksmode.org (and modified by USIT)
+// For >= IE7 (can be removed soon)
+function getElementsByTagNames(list, obj) {
+  if (!obj) obj = document;
+  var tagNames = list.split(',');
+  var resultArray = [];
+  for (var i = 0, tagNamesLength = tagNames.length; i < tagNamesLength; i++) {
+	var tags = null;
+    if (tocTargetId !== "") {
+      tags = obj.getElementById(tocTargetId).getElementsByTagName(tagNames[i]);
+    } else {
+      tags = obj.getElementsByTagName(tagNames[i]);
+    }
+    for (var j = 0, tagsLength = tags.length; j < tagsLength; j++) {
+      resultArray.push(tags[j]);
+    }
+  }
+  var testNode = resultArray[0];
+  if (!testNode) return [];
+  if (testNode.sourceIndex) {
+    resultArray.sort(function (a, b) {
+      return a.sourceIndex - b.sourceIndex;
+    });
+  } else if (testNode.compareDocumentPosition) {
+    resultArray.sort(function (a, b) {
+      return 3 - (a.compareDocumentPosition(b) & 6);
+    });
+  }
+  return resultArray;
 }
 
 /* ^ ToC (Table of Content) */

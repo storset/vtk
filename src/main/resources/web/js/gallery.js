81cdf1d76a7c9fd0d30c34ca450b30b656035b6a
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-02-14 17:15:58 +0000

    VTK - simplify, refactor and optimize gallery (still more can be done to keep it light and maintainable)

--- src/main/resources/web/js/gallery.js
+++ src/main/resources/web/js/gallery.js
@@ -28,139 +28,120 @@
       fadedThumbsOutOpacity: 0.6,
       fadeNavInOutTime: 250
     }, options || {});
-
-    // Unobtrusive JavaScript
-    $(container + "-pure-css").addClass(container.substring(1));
-    $(container + "-nav-pure-css").addClass(container.substring(1) + "-nav");
-    $(wrapper + "-thumbs-pure-css").addClass(wrapper.substring(1) + "-thumbs");
-
-    wrapperContainer = wrapper + " " + container;
-    wrapperContainerLink = wrapperContainer + " a" + container + "-link";
-    wrapperThumbsLinks = wrapper + " ul li a";
     
-    // Cache image HTML with src as hash
-    var images = []; 
-    var imagesWidth = [];
-    var imagesHeight = [];
+    var wrp = $(wrapper);
 
-    // Performance: Function pointers to use inside loops
-    var centerThumbnailImageFunc = centerThumbnailImage;
-    var calculateImageFunc = calculateImage;
-    var generateLinkImageFunc = generateLinkImage;
+    // Unobtrusive
+    wrp.find(container + "-pure-css").addClass(container.substring(1));
+    wrp.find(container + "-nav-pure-css").addClass(container.substring(1) + "-nav");
+    wrp.find(wrapper + "-thumbs-pure-css").addClass(wrapper.substring(1) + "-thumbs");
+
+    var wrapperContainer = wrapper + " " + container;
+    var wrapperContainerLink = wrapperContainer + " a" + container + "-link";
+    var wrapperThumbsLinks = wrapper + " li a";
+    
+    // Cache containers and image HTML with src as hash
+    var wrpContainer = $(wrapperContainer);
+    var wrpContainerLink = $(wrapperContainer + " a" + container + "-link");
+    var wrpThumbsLinks = $(wrapperThumbsLinks);
+    var images = []; 
+    var imagesWidth = {};
+    var imagesHeight = {};
     
     // Init first active image
-    calculateImageFunc($(wrapperThumbsLinks + ".active img.vrtx-thumbnail-image"), 
-                       $(wrapperThumbsLinks + ".active img.vrtx-full-image"), true);
-
-    initPagingEvents("prev");
-    initPagingEvents("next");
+    var firstImage = wrpThumbsLinks.filter(".active");
+    calculateImage(firstImage.find("img.vrtx-thumbnail-image"), firstImage.find("img.vrtx-full-image"), true);
+    
+    wrp.find("a.prev, a.prev span, a.next, a.next span").fadeTo(0, 0);
     
     // Event-handlers
     $(document).keydown(function (e) {
       if (e.keyCode == 37) {
-        $(wrapper + " a.prev").click();
+        wrp.find("a.prev").click();
       } else if (e.keyCode == 39) {
-        $(wrapper + " a.next").click();
+        wrp.find("a.next").click();
       }
     });
 
-    $(wrapper).on("mouseover mouseout click", "ul li a", function (e) {
-      var h = $(this);      
+    wrp.on("mouseover mouseout click", "li a", function (e) {
+      var elm = $(this);      
       if (e.type == "mouseover") {
-        if (!h.hasClass("active")) {
-          h.find("img").stop().fadeTo(settings.fadeThumbsInOutTime, 1);
-        }
+        elm.filter(":not(.active)").find("img").stop().fadeTo(settings.fadeThumbsInOutTime, 1);
       } else if (e.type == "mouseout") {
-        if (!h.hasClass("active")) {
-          h.find("img").stop().fadeTo(settings.fadeThumbsInOutTime, settings.fadedThumbsOutOpacity);
-        }
+        elm.filter(":not(.active)").find("img").stop().fadeTo(settings.fadeThumbsInOutTime, settings.fadedThumbsOutOpacity);
       } else {
-        var img = h.find("img.vrtx-thumbnail-image");
-        var fullImage = h.find("img.vrtx-full-image");
-        calculateImageFunc(img, fullImage, false);
-        h.addClass("active");
+        var img = elm.find("img.vrtx-thumbnail-image");
+        var fullImage = elm.find("img.vrtx-full-image");
+        calculateImage(img, fullImage, false);
+        elm.addClass("active");
         img.stop().fadeTo(0, 1);
-        e.preventDefault()
-      }
-    });
-
-    $(wrapper).on("click mouseover mouseout", "a.next, " + container + "-link", function (e) {
-      if (e.type == "mouseover") {
-        fadeMultiple([wrapper + " a.next span",
-                      wrapper + " a.prev span"], settings.fadeNavInOutTime, 0.2);
-        $(wrapper + " a.next").stop().fadeTo(settings.fadeNavInOutTime, 1);
-        $(wrapper + " a.prev").stop().fadeTo(settings.fadeNavInOutTime, 0.5);
-      } else if (e.type == "mouseout") {
-        fadeMultiple([wrapper + " a.next", wrapper + " a.next span",
-                      wrapper + " a.prev", wrapper + " a.prev span"], settings.fadeNavInOutTime, 0);
-      } else {
-        var activeThumb = $(wrapperThumbsLinks + ".active");
-        if (activeThumb.parent().next().length != 0) {
-          activeThumb.parent().next().find("a").click();
-        } else {
-          $(wrapper + " ul li:first a").click();
-        }
         e.preventDefault();
       }
     });
 
-    $(wrapper).on("click mouseover mouseout", "a.prev", function (e) {
-      if (e.type == "mouseover") {
-        fadeMultiple([wrapper + " a.next span",
-                                wrapper + " a.prev span"], settings.fadeNavInOutTime, 0.2);
-        $(wrapper + " a.prev").stop().fadeTo(settings.fadeNavInOutTime, 1);
-        $(wrapper + " a.next").stop().fadeTo(settings.fadeNavInOutTime, 0.5);
-      } else if (e.type == "mouseout") {
-        fadeMultiple([wrapper + " a.next", wrapper + " a.next span",
-                      wrapper + " a.prev", wrapper + " a.prev span"], settings.fadeNavInOutTime, 0);
-      } else {
-        var activeThumb = $(wrapperThumbsLinks + ".active");
-        if (activeThumb.parent().prev().length != 0) {
-          activeThumb.parent().prev().find("a").click();
-        } else {
-          $(wrapper + " ul li:last a").click();
-        }
-        e.preventDefault();
-      }
+    wrp.on("click mouseover mouseout", "a.next, " + container + "-link", function (e) {
+      nextPrevNavigate(e, 1);
     });
 
-    // TODO: use for- or async loop
-    // Center thumbnails and cache images with link
-    return this.each(function () {
-      var link = $(this);
+    wrp.on("click mouseover mouseout", "a.prev", function (e) {
+      nextPrevNavigate(e, -1);
+    });
+
+    var imgs = this, centerThumbnailImageFunc = centerThumbnailImage, generateLinkImageFunc = generateLinkImage;
+    for(var i = 0, len = imgs.length; i < len; i++) {
+      var link = $(imgs[i]);
       var img = link.find("img.vrtx-full-image");
       var src = img.attr("src");
-      imagesWidth[src] = parseInt(link.find("span.hiddenWidth").text());
-      imagesHeight[src] = parseInt(link.find("span.hiddenHeight").text());
+      imagesWidth[src] = parseInt(link.find("span.hiddenWidth").text(), 10);
+      imagesHeight[src] = parseInt(link.find("span.hiddenHeight").text(), 10);
       images[src] = generateLinkImageFunc(img, link, false);
+      
       centerThumbnailImageFunc(link.find("img.vrtx-thumbnail-image"), link);
-    });
-
+    }
+    return imgs; /* Make chainable */
+    
+    function nextPrevNavigate(e, dir) {
+      var isNext = dir > 0;	
+      if (e.type == "mouseover") {
+        wrp.find("a.next span, a.prev span").stop().fadeTo(settings.fadeNavInOutTime, 0.2);   /* XXX: some filtering instead below */
+        wrp.find("a." + (isNext ? "next" : "prev")).stop().fadeTo(settings.fadeNavInOutTime, 1);
+        wrp.find("a." + (isNext ? "prev" : "next")).stop().fadeTo(settings.fadeNavInOutTime, 0.5);
+      } else if (e.type == "mouseout") {
+        wrp.find("a.next span, a.prev span").stop().fadeTo(settings.fadeNavInOutTime, 0);
+      } else {
+        var activeThumb = wrpThumbsLinks.filter(".active").parent();
+        var elm = isNext ? activeThumb.next() : activeThumb.prev();
+        var roundAboutElm = isNext ? "first" : "last";
+        if (elm.length) {
+          elm.find("a").click();
+        } else {
+          wrp.find("li:" + roundAboutElm + " a").click();
+        }
+        e.preventDefault();
+      }
+    }
+    
     function calculateImage(image, fullImage, init) {
       if (settings.fadeInOutTime > 0 && !init) {
-        $(wrapperContainer).append("<div class='over'>" + $(wrapperContainerLink).html() + "</div>");
+        wrpContainer.append("<div class='over'>" + $(wrapperContainerLink).html() + "</div>");
         $(wrapperContainerLink).remove();
-        $(wrapperContainer).append(images[fullImage.attr("src")]);
-        scaleAndCalculatePosition(fullImage);
+        wrpContainer.append(images[fullImage.attr("src")]);
         $(".over").fadeTo(settings.fadeInOutTime, settings.fadedOutOpacity, function () {
           $(this).remove();
         });
       } else {
         if (init) {
-          $(wrapperContainer).append(generateLinkImageFunc($(fullImage), $(image).parent(), true));
+          wrpContainer.append(generateLinkImage(fullImage, image.parent(), true));
         } else {
           $(wrapperContainerLink).remove();
-          $(wrapperContainer).append(images[fullImage.attr("src")]);
+          wrpContainer.append(images[fullImage.attr("src")]);
         }
-        scaleAndCalculatePosition(fullImage);
       }
+      scaleAndCalculatePosition(fullImage);
 
-      var thumbs = $(wrapperThumbsLinks);
-      var thumbsLength = thumbs.length,
-          i = 0,
-          thumb;
-      for (; i < thumbsLength; i++) {
-        thumb = $(thumbs[i]);
+      var thumbs = wrpThumbsLinks;
+      for (var thumbsLength = thumbs.length, i = 0; i < thumbsLength; i++) {
+        var thumb = $(thumbs[i]);
         if (thumb.hasClass("active")) {
           if (!init) {
             thumb.removeClass("active");
@@ -172,58 +153,32 @@
       }
     }
 
-    function initPagingEvents(navClass) {
-      fadeMultiple(new Array(wrapper + " a." + navClass, wrapper + " a." + navClass + " span"), 0, 0);
-    }
-
-    function scaleAndCalculatePosition(image, init) {
+    function scaleAndCalculatePosition(image) {
       var src = image.attr("src").split("?")[0];
+      var imgWidth =  Math.max(parseInt( imagesWidth[src], 10), 150) + "px";
+      var imgHeight = Math.max(parseInt(imagesHeight[src], 10), 100) + "px";
 
-      var imgWidth = imagesWidth[src];
-      var imgHeight = imagesHeight[src];
-
-      var minContainerHeight = 100;
-      var minContainerWidth = 150;
-
-      if (parseInt(imgHeight) < minContainerHeight) {
-        imgHeight = minContainerHeight + "px";
-      } else {
-        imgHeight = imgHeight + "px";
-      }
-      if (parseInt(imgWidth) < minContainerWidth) {
-        imgWidth = minContainerWidth + "px";
-      } else {
-        imgWidth = imgWidth + "px";
-      }
-
-      // For Debugging: galleryLog(src + " [" + imgWidth + ", " + imgHeight + "]");
-
-      setMultipleCSS([wrapperContainer + "-nav a", wrapperContainer + "-nav span",
-                               wrapperContainerLink], "height", imgHeight);
-
-      setMultipleCSS([wrapperContainer, wrapperContainer + "-nav"], "width", imgWidth);
-
+      $(wrapperContainer + "-nav a, " + wrapperContainer + "-nav span, " + wrapperContainerLink).css("height", imgHeight);
+      $(wrapperContainer + ", " + wrapperContainer + "-nav").css("width", imgWidth);
       $(wrapperContainer + "-description").remove();
+      
+      var title = image.attr("title");
+      var alt = image.attr("alt");
+      var hasTitle = title && title.length;
+      var hasAlt = alt && alt.length;
 
       var html = "<div class='" + container.substring(1) + "-description'>";
-      if ($(image).attr("title") && $(image).attr("title") != "") {
-        html += "<p class='" + container.substring(1) + "-title'>" + $(image).attr("title") + "</p>";
+      if (hasTitle) {
+        html += "<p class='" + container.substring(1) + "-title'>" + title + "</p>";
       }
-      if ($(image).attr("alt") && $(image).attr("alt") != "") {
-        html += $(image).attr("alt");
+      if (hasAlt) {
+        html += alt;
       }
       html += "</div>";
       $(html).insertAfter(wrapperContainer);
 
-      if (($(image).attr("alt") && $(image).attr("alt") != "") 
-       || ($(image).attr("title") && $(image).attr("title") != "")) {
+      if (hasTitle || hasAlt) {
         $(wrapperContainer + "-description").css("width", imgWidth);
-      }
-    }
-
-    function galleryLog(msg) {
-      if (typeof console != "undefined" && console.log) {
-        console.log(msg);
       }
     }
 
@@ -233,48 +188,35 @@
     }
 
     function centerDimension(thumb, thumbDimension, thumbContainerDimension, cssProperty) {
+      var adjust = 0;
       if (thumbDimension > thumbContainerDimension) {
-        var adjust = (thumbDimension - thumbContainerDimension) / 2;
-        $(thumb).css(cssProperty, -adjust + "px");
+        adjust = ((thumbDimension - thumbContainerDimension) / 2) * -1;
       } else if (thumbDimension < thumbContainerDimension) {
-        var adjust = (thumbContainerDimension - thumbDimension) / 2;
-        $(thumb).css(cssProperty, adjust + "px");
+        adjust = (thumbContainerDimension - thumbDimension) / 2;
       }
+      thumb.css(cssProperty, adjust + "px");
     }
 
-    function generateLinkImage(theimage, thelink, init) {
-      var src = theimage.attr("src").split("?")[0];
-      var alt = theimage.attr("alt");
+    function generateLinkImage(image, link, init) {
+      var src = image.attr("src").split("?")[0];
+      var alt = image.attr("alt");
+      var width = 0;
+      var height = 0;
       if (!init) {
-        var width = imagesWidth[src];
-        var height = imagesHeight[src];
+        width = imagesWidth[src];
+        height = imagesHeight[src];
       } else {
-        var width = parseInt(thelink.find("span.hiddenWidth").text());
-        var height = parseInt(thelink.find("span.hiddenHeight").text());
+        width = parseInt(link.find("span.hiddenWidth").text(), 10);
+        height = parseInt(link.find("span.hiddenHeight").text(), 10);
         imagesWidth[src] = width;
         imagesHeight[src] = height;
       }
-
-      return "<a href='" + $(thelink).attr("href") + "'" 
-           + " class='" + container.substring(1) + "-link'>"
-           + "<img src='" + src + "' alt='" + alt + "' style='width: "
-           + width + "px; height: " + height + "px;' />" + "</a>";
-    }
-
-    function setMultipleCSS(elements, cssProperty, value) {
-      var elementsLength = elements.length;
-      for (var i = 0; i < elementsLength; i++) {
-        $(elements[i]).css(cssProperty, value);
-      }
-    }
-
-    function fadeMultiple(elements, time, opacity) {
-      var elementsLength = elements.length;
-      for (var i = 0; i < elementsLength; i++) {
-        $(elements[i]).stop().fadeTo(time, opacity);
-      }
+      return "<a href='" + link.attr("href") + "'" +
+             " class='" + container.substring(1) + "-link'>" +
+             "<img src='" + src + "' alt='" + alt + "' style='width: " +
+             width + "px; height: " + height + "px;' />" + "</a>";
     }
   };
-})(jQuery)
+})(jQuery);
 
 /* ^ Vortex Simple Gallery jQuery plugin */

34dded8379b416f30702ecc93b46ae275c5ab1c0
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-02-20 14:47:24 +0000

    VTK-3153 - formatting toggler

--- src/main/resources/web/js/toggle.js
+++ src/main/resources/web/js/toggle.js
@@ -6,71 +6,70 @@
  * XXX: JSDoc
  * 
  */
-
-(function(){
-  if(typeof Toggler !== "function") {
+(function () {
+  if (typeof Toggler !== "function") {
     function Toggler() {
       this.configs = {
-    	/* name 
+        /* name 
          * hideLinkText
-    	 * showLinkText
-    	 * isAnimated (optional)
-    	 */
+         * showLinkText
+         * isAnimated (optional)
+         */
       };
     }
     toggler = new Toggler(); /* Global accessible object - XXX: proper singleton */
-    
-    $(document).ready(function() {
+
+    $(document).ready(function () {
       toggler.init();
     });
-  
-    Toggler.prototype.add = function(config) {
+
+    Toggler.prototype.add = function (config) {
       this.configs["vrtx-" + config.name + "-toggle"] = config;
     };
-  	  
-    Toggler.prototype.init = function() {
-	  var self = this;
 
-      for(var key in self.configs) {
-    	var config = self.configs[key];
+    Toggler.prototype.init = function () {
+      var self = this;
+
+      for (var key in self.configs) {
+        var config = self.configs[key];
         var container = $("#vrtx-" + config.name);
-	    var link = $("#" + key);
-	    if(container.length && link.length) {
-	      container.hide();
-	      link.addClass("togglable");
-	      link.parent().show();
-	      config.container = container;
-	      config.link = link;
-	    }
+        var link = $("#" + key);
+        if (container.length && link.length) {
+          container.hide();
+          link.addClass("togglable");
+          link.parent().show();
+          config.container = container;
+          config.link = link;
+        }
       }
-      
-      $(document).on("click", "a.togglable", function(e) {
-	    self.toggle(this);
-	    e.stopPropagation();
-	    e.preventDefault();
+
+      $(document).on("click", "a.togglable", function (e) {
+        self.toggle(this);
+        e.stopPropagation();
+        e.preventDefault();
       });
     };
-  
-    Toggler.prototype.toggle = function(link) {
+
+    Toggler.prototype.toggle = function (link) {
       var self = this;
-    	
-	  var config = self.configs[link.id];
-	  if(config.isAnimated) {
-	    config.container.slideToggle("fast", function() {  /* XXX: proper easing requires jQuery UI */ 
-	      self.toggleLinkText(config);	 
-	    });
-	  } else {
-	    config.container.toggle();
-	    self.toggleLinkText(config);
-	  } 
+
+      var config = self.configs[link.id];
+      if (config.isAnimated) {
+        config.container.slideToggle("fast", function () { /* XXX: proper easing requires jQuery UI */
+          self.toggleLinkText(config);
+        });
+      } else {
+        config.container.toggle();
+        self.toggleLinkText(config);
+      }
     };
-    
-    Toggler.prototype.toggleLinkText = function(config) {
-    	if(config.container.filter(":visible").length) {
-   	     config.link.text(config.hideLinkText);
-   	   } else {
-   	     config.link.text(config.showLinkText);
-   	   }	
+
+    Toggler.prototype.toggleLinkText = function (config) {
+      if (config.container.filter(":visible").length) {
+        config.link.text(config.hideLinkText);
+      } else {
+        config.link.text(config.showLinkText);
+      }
     };
   }
 })();

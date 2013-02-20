4902fad35ed7907236906e2d1e9d83bf4c292b48
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-02-20 11:44:09 +0000

    VTK - refactor out JS for toggling completed/previous list of links

---
+++ src/main/resources/web/js/toggle.js
@@ -0,0 +1,16 @@
+/*
+ * Plain JS toggle 
+ * XXX: Should probably use jQuery as need document.ready for hiding list and showing toggle link (making the JS unobtrusive)
+ */
+
+function toggleCompleted(name, hideLinkText, showLinkText) {
+  var elm = document.getElementById("vrtx-" + name + "-semesters");
+  var toggleLink = document.getElementById("vrtx-" + name + "-semesters-toggle");
+  if(elm.style.display == "none") {
+    elm.style.display = "block";
+    toggleLink.innerHTML = hideLinkText;
+  } else {
+    elm.style.display = "none";
+    toggleLink.innerHTML = showLinkText;
+  }
+}

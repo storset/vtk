b869ed8085020bc58118b1b7530d676ef5cff7b1
Øyvind Stegard <oyviste@usit.uio.no>
2013-03-15 00:27:54 +0000

    Merge from trunk.

--- src/main/java/org/vortikal/web/decorating/components/menu/BreadcrumbMenuComponent.java
+++ src/main/java/org/vortikal/web/decorating/components/menu/BreadcrumbMenuComponent.java
@@ -102,6 +102,7 @@ public class BreadcrumbMenuComponent extends ListMenuComponent {
         // and must respect plainServiceMode.
         if (requestContext.isViewUnauthenticated()) {
             token = null;
+            principal = null;
         }
 
         if (!currentResource.isCollection()) {

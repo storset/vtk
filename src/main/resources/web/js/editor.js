2ece6deeec1caab71e068f4cc5917edd6db88e9f
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-05-03 15:22:33 +0000

    VTK-3277 - fix magicline jump (caused by magicline becoming first-child and adding margin removed from next first-child)

--- src/main/resources/web/js/editor.js
+++ src/main/resources/web/js/editor.js
@@ -585,7 +585,6 @@ maxHeight, minHeight, toolbar, complete, resizable, baseDocumentUrl, simple) {
     config.filebrowserImageBrowseUrl = imageBrowseUrl;
     config.filebrowserFlashBrowseUrl = flashBrowseUrl;
     config.extraPlugins = 'mediaembed,studyreferencecomponent,htmlbuttons';
-    config.removePlugins = 'magicline';
     config.stylesSet = vrtxEdit.CKEditorDivContainerStylesSet;
     if (name == "resource.content" && simple) { // XHTML
       config.format_tags = 'p;h1;h2;h3;h4;h5;h6;pre;div';

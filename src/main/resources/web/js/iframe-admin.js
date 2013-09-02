a8c6ad49b459454bfad8c0e261644975a82da9ec
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-09-02 17:29:56 +0000

    VTK-3321 - this=>vrtxAdmin

--- src/main/resources/web/js/iframe-admin.js
+++ src/main/resources/web/js/iframe-admin.js
@@ -87,7 +87,7 @@
       body = $("body");
       
       // As we can't check on matchMedia and Modernizr is not included in admin yet - hide if <= IE8
-      if(vrtxAdmin.isIE8 || this.isMobileWebkitDevice) {
+      if(vrtxAdmin.isIE8 || vrtxAdmin.isMobileWebkitDevice) {
         $("#preview-mode").hide();
       } else {
         $(document).on("click", "#preview-mode a", function(e) {

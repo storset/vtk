feddb488b8c64f6ac8ebccbb89e957918569e7d4
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-10-08 10:20:36 +0000

    VTK - seperate options and dialog options (a little quirky that interpreter need to look oustide the closures for __opts on open, ok and cancel actions)

--- src/main/resources/web/js/vrtx-simple-dialogs.js
+++ src/main/resources/web/js/vrtx-simple-dialogs.js
@@ -17,57 +17,60 @@ var AbstractVrtxSimpleDialog = dejavu.AbstractClass.declare({
   $name: "AbstractVortexSimpleDialog",      // Meta-attribute useful for debugging
   $implements: [VrtxSimpleDialogInterface],
   initialize: function(opts) {              // Constructor
-      this.__addDOM(opts)
-      var dialogOpts =     { selector: opts.selector,
-                             modal: true,                        
+      this.__opts = opts;
+      this.__addDOM();
+      var dialogOpts =     { modal: true,                        
                              autoOpen: false,
                              resizable: false,
-                             buttons: this.__generateOkCancel(opts) };
+                             buttons: this.__generateOkCancel() };
       if (opts.width)      { dialogOpts.width = opts.width; }
       if (opts.height)     { dialogOpts.height = opts.height; }
       if (opts.unclosable) { dialogOpts.closeOnEscape = false; }
+                             var dialog = this;
                              dialogOpts.open = function(e, ui) {
-                               if (opts.unclosable) {
-                                 var ctx = $(this).parent();
+                               var ctx = $(this).parent();
+                               if (dialog.__opts.unclosable) {
                                  ctx.find(".ui-dialog-titlebar-close").hide();
                                  ctx.find(".ui-dialog-titlebar").addClass("closable");
                                }
-                               if(opts.funcOnOpen) opts.funcOnOpen();
-                               if(opts.cancelIsNotAButton) {
-                                 var ctx = $(this).parent();
+                               if(dialog.__opts.funcOnOpen) ialog .__opts.funcOnOpen();
+                               if(dialog.__opts.cancelIsNotAButton) {
                                  ctx.find(".ui-dialog-buttonpane button:last-child span").unwrap().addClass("cancel-is-not-a-button");
                                }
                              };
-      this.opts = dialogOpts;
+      this.__dialogOpts = dialogOpts;
   },
-  opts: {},
-  __generateOkCancel: function(opts) {
+  __opts: {},
+  __dialogOpts: {},
+  __generateOkCancel: function() {
     var buttons = {};
-    if (opts.hasOk) {
-      var ok = opts.btnTextOk || "Ok";
+    if (this.__opts.hasOk) {
+      var ok = this.__opts.btnTextOk || "Ok";
+      var dialog = this;
       buttons[ok] = function() {
         $(this).dialog("close");
-        if(opts.funcOkComplete) opts.funcOkComplete(opts.funcOkCompleteOpts);
+        if(dialog.__opts.funcOkComplete) dialog.__opts.funcOkComplete(dialog.__opts.funcOkCompleteOpts);
       };
     }
-    if (opts.hasCancel) {
-      var cancel = opts.btnTextCancel || ((typeof cancelI18n != "undefined") ? cancelI18n : "Cancel");
+    if (this.__opts.hasCancel) {
+      var cancel = this.__opts.btnTextCancel || ((typeof cancelI18n != "undefined") ? cancelI18n : "Cancel");
       if(/^\(/.test(cancel)) {
-        opts.cancelIsNotAButton = true;
+        this.__opts.cancelIsNotAButton = true;
       }
+      var dialog = this;
       buttons[cancel] = function() {
         $(this).dialog("close");
-        if(opts.funcCancelComplete) opts.funcCancelComplete();
+        if(dialog.__opts.funcCancelComplete) dialog.__opts.funcCancelComplete();
       };
     }
     return buttons;
   },
-  __addDOM: function(opts) {
+  __addDOM: function() {
     $(".vrtx-dialog").remove();
-    if (opts.title) {
-      $("body").append("<div class='vrtx-dialog' id='" + opts.selector.substring(1) + "' title='" + opts.title + "'><div id='" + opts.selector.substring(1) + "-content'>" + (!opts.hasHtml ? "<p>" + opts.msg + "</p>" : opts.msg) + "</div></div>");
+    if (this.__opts.title) {
+      $("body").append("<div class='vrtx-dialog' id='" + this.__opts.selector.substring(1) + "' title='" + this.__opts.title + "'><div id='" + this.__opts.selector.substring(1) + "-content'>" + (!this.__opts.hasHtml ? "<p>" + this.__opts.msg + "</p>" : this.__opts.msg) + "</div></div>");
     }
-    $("body").append("<div class='vrtx-dialog' id='" + opts.selector.substring(1) + "'><div id='" + opts.selector.substring(1) + "-content'>" + (!opts.hasHtml ? "<p>" + opts.msg + "</p>" : opts.msg) + "</div></div>");
+    $("body").append("<div class='vrtx-dialog' id='" + this.__opts.selector.substring(1) + "'><div id='" + this.__opts.selector.substring(1) + "-content'>" + (!this.__opts.hasHtml ? "<p>" + this.__opts.msg + "</p>" : this.__opts.msg) + "</div></div>");
     $(".vrtx-dialog").hide();
   },
   open: function () {
@@ -75,17 +78,17 @@ var AbstractVrtxSimpleDialog = dejavu.AbstractClass.declare({
     var futureUiUrl = "/vrtx/__vrtx/static-resources/jquery/plugins/ui/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.min.js";
     var futureUi = (typeof $.ui === "undefined") ? $.getScript(futureUiUrl) : $.Deferred().resolve();
     $.when(futureUi).done(function() {
-      dialog.opts.elm = $(dialog.opts.selector);
-      dialog.opts.elm.dialog(dialog.opts);
-      dialog.opts.elm.dialog("open");
+      dialog.__opts.elm = $(dialog.__opts.selector);
+      dialog.__opts.elm.dialog(dialog.__dialogOpts);
+      dialog.__opts.elm.dialog("open");
     });
   },
   close: function () {
-    this.opts.elm.dialog("close");
+    this.__opts.elm.dialog("close");
   },
   destroy: function () {
-    this.opts.elm.dialog("destroy");
-    this.opts.elm.remove();
+    this.__opts.elm.dialog("destroy");
+    this.__opts.elm.remove();
   }        
 });
 

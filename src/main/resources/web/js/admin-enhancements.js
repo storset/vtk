272bdc8ac3bd79d74e4e8a251d087857d9b509b6
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-10-17 19:12:32 +0000

    VTK-3410 - remove unused function

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -3705,30 +3705,6 @@ function unique(array) {
   return r;
 }
 
-if (!Date.prototype.toISOString) {
-  (function() {
-    function pad(number) {
-      var r = String(number);
-      if (r.length === 1) {
-        r = '0' + r;
-      }
-      return r;
-    }
- 
-    Date.prototype.toISOString = function() {
-      return this.getUTCFullYear()
-        + '-' + pad(this.getUTCMonth() + 1)
-        + '-' + pad(this.getUTCDate())
-        + 'T' + pad(this.getUTCHours())
-        + ':' + pad(this.getUTCMinutes())
-        + ':' + pad(this.getUTCSeconds())
-        + '.' + String((this.getUTCMilliseconds()/1000).toFixed(3).slice(2, 5))
-        + 'Z';
-    };
-  }());
-}
-
-
 /*-------------------------------------------------------------------*\
     14. Override JavaScript / jQuery
 \*-------------------------------------------------------------------*/

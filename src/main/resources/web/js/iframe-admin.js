438479afdfb53d36018afcebc483f72d0f22aa5d
Øyvind Hatland <oyvihatl@usit.uio.no>
2013-10-22 13:48:17 +0000

    VTK-3389 - remove '#' when no hash params in admin preview if HTML5 history is supported

--- src/main/resources/web/js/iframe-admin.js
+++ src/main/resources/web/js/iframe-admin.js
@@ -100,6 +100,9 @@
   function updateHashShareLink(link) {
     if(location.hash.length <= 1) {
       var hash = "";
+      if(window.history && window.history.pushState) { 
+        window.history.pushState("", "", window.location.pathname + window.location.search) 
+      }
     } else {
       var hash = location.hash;
     }

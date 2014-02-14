63ba5f7988c3e32537a6d3a997895cddc9b1f6cf
Øyvind Hatland <oyvihatl@usit.uio.no>
2014-02-14 14:33:57 +0000

    VTK-3485 - you cant focus a hidden element with tab - programmatically show and set focus on next element

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -1918,7 +1918,13 @@ function ajaxUploadPerform(opts) {
   var uploadingD = new VrtxLoadingDialog({title: uploading.inprogress});
   uploadingD.open();
   _$("#dialog-loading-content").append("<div id='dialog-uploading-bar' /><div id='dialog-uploading-percent'>&nbsp;</div><a id='dialog-uploading-abort' href='javascript:void(0);'>Avbryt</a>");
-  _$("<span style='outline: none;' tabindex='-1' />").insertBefore("#dialog-uploading-abort")[0].focus();
+  _$("<span id="dialog-uploading-focus" style='outline: none;' tabindex='-1' />").insertBefore("#dialog-uploading-abort")[0].focus();
+  _$("#dialog-uploading-focus").keydown(function(e) {
+    if (isKey(e, 9)) { 
+      $(this).next().show()[0].focus();
+      return false;
+    }
+  })
 
   var uploadXhr = null;
   

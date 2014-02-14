ace3533aac6412e887edad3a8ee441bda7f962ff
Øyvind Hatland <oyvihatl@usit.uio.no>
2014-02-14 14:12:52 +0000

    VTK-3485

--- src/main/resources/web/js/vrtx-simple-dialogs.js
+++ src/main/resources/web/js/vrtx-simple-dialogs.js
@@ -51,7 +51,7 @@ var AbstractVrtxSimpleDialog = dejavu.AbstractClass.declare({
                                  if(input.length) {
                                    $("<span style='outline: none;' tabindex='-1' />").insertBefore(input)[0].focus();
                                  } else {
-                                   input = ctx.find("a");
+                                   input = ctx.find("a").filter(":first");
                                    if(input.length) {
                                      $("<span style='outline: none;' tabindex='-1' />").insertBefore(input)[0].focus();
                                    }

be2c940e0c3e8b96474080319c47d17d1db1862b
Øyvind Hatland <oyvihatl@usit.uio.no>
2014-03-06 14:22:05 +0000

    VTK - +1

--- src/main/resources/web/js/editor.js
+++ src/main/resources/web/js/editor.js
@@ -929,7 +929,7 @@ function showImagePreviewCaption(input) {
     captionWrp.find(".hidePicture").fadeIn("fast");
     captionWrp.find(".pictureAlt").fadeIn("fast");
     captionWrp.animate({
-      height: "223px"
+      height: "224px"
     }, "fast");
   }
 }

529ace7cad8f961543f79778f50bdfb03cc84bbb
Øyvind Hatland <oyvihatl@usit.uio.no>
2014-03-20 10:38:15 +0000

    VTK - ie8 partly fix and give resize some slack after 2 debounced resize on 1s

--- src/main/resources/web/js/admin-enhancements.js
+++ src/main/resources/web/js/admin-enhancements.js
@@ -4170,11 +4170,16 @@ jQuery.fn.extend({
  */
 (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
 
-
-vrtxAdmin._$(window).resize(vrtxAdmin._$.debounce(0, function () {
-  if (vrtxAdmin.runReadyLoad) {
+var count = 0;
+vrtxAdmin._$(window).resize(vrtxAdmin._$.debounce(20, function () {
+  if (vrtxAdmin.runReadyLoad && count < 3) {
+    count++;
     vrtxAdmin.scrollBreadcrumbsRight();
     vrtxAdmin.adjustResourceTitle();
+  } else {
+    var waitResize = setTimeout(function() {
+      count = 0;
+    }, 1000);
   }
 }));
 

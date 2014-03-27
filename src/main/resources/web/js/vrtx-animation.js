36e1a651495d7c8fedd696bcab6413dbce708df4
Øyvind Hatland <oyvihatl@usit.uio.no>
2014-03-27 14:24:49 +0000

    VTK - simplify more and prepare for CSS3 animation progressively enhanced

--- src/main/resources/web/js/vrtx-animation.js
+++ src/main/resources/web/js/vrtx-animation.js
@@ -29,45 +29,62 @@ var VrtxAnimation = dejavu.Class.declare({
   $constants: {
     animationSpeed: /(iphone|ipad|android)/.test(navigator.userAgent.toLowerCase()) ? 0 : 200,
     easeIn: !/msie (8|9.)/.test(navigator.userAgent.toLowerCase()) ? "easeInQuad" : "linear",
-    easeOut: !/msie (8|9.)/.test(navigator.userAgent.toLowerCase()) ? "easeOutQuad" : "linear"
+    easeOut: !/msie (8|9.)/.test(navigator.userAgent.toLowerCase()) ? "easeOutQuad" : "linear",
+    cssTransform: (function () {
+      var propArray = ['transform', 'MozTransform', 'WebkitTransform', 'msTransform', 'OTransform'];
+      var root = document.documentElement;
+      for (var i = 0, len = propArray.length; i < len; i++) {
+        if (propArray[i] in root.style){
+          return propArray[i];
+        }
+      }
+    })()
   },
   __opts: {},
   initialize: function(opts) {
     this.__opts = opts;
   },
-  __prepareHorizontalMove: function() {
+  __prepareMove: function() {
     if(this.__opts.outerWrapperElem && !this.__opts.outerWrapperElem.hasClass("overflow-hidden")) {
       this.__opts.outerWrapperElem.addClass("overflow-hidden");
     }
-    return this.__opts.elem.outerWidth(true);
+    return [this.__opts.elem.outerWidth(true), this.__opts.elem.outerHeight(true)];
   },
-  __horizontalMove: function(left, easing, afterSp) {
+  __afterMove: function(afterSp) {
+    if(this.__opts.outerWrapperElem) this.__opts.outerWrapperElem.removeClass("overflow-hidden");
+    if(this.__opts.after) this.__opts.after(this);
+    if(this.__opts[afterSp]) this.__opts[afterSp](this);
+  },
+  __horizontalMove: function(fn, easing, afterSp) {
+    var width = this.__prepareMove()[0];
+    var left = (fn === "slideIn") ? 0 : -width;
+    if(fn === "slideIn") {
+      this.__opts.elem.css("marginLeft", -width);
+    }
+    
     var animation = this;
     animation.__opts.elem.animate({
       "marginLeft": left + "px"
-    }, animation.__opts.animationSpeed || animation.$static.animationSpeed, this.__opts[easing] || this.$static[easing], function() {
-      if(animation.__opts.outerWrapperElem) animation.__opts.outerWrapperElem.removeClass("overflow-hidden");
-      if(animation.__opts.after) animation.__opts.after(animation);
-      if(animation.__opts[afterSp]) animation.__opts[afterSp](animation);
+    }, animation.__opts.animationSpeed || animation.$static.animationSpeed,
+      animation.__opts[easing] || animation.$static[easing], function() {
+      animation.__afterMove(afterSp);
     });
   },
   __verticalMove: function(fn, easing, afterSp) {
+    var height = this.__prepareMove()[1];
+    
     var animation = this;
     animation.__opts.elem[fn](
         animation.__opts.animationSpeed || animation.$static.animationSpeed, 
         animation.__opts[easing] || animation.$static[easing], function() {
-      if(animation.__opts.after) animation.__opts.after(animation);
-      if(animation.__opts[afterSp]) animation.__opts[afterSp](animation);
+      animation.__afterMove(afterSp);
     });
   },
   rightIn: function() {
-    var width = this.__prepareHorizontalMove();
-    this.__opts.elem.css("marginLeft", -width);
-    this.__horizontalMove(0, "easeIn", "afterIn");
+    this.__horizontalMove("slideIn", "easeIn", "afterIn");
   },
   leftOut: function() {
-    var width = this.__prepareHorizontalMove();
-    this.__horizontalMove(-width, "easeOut", "afterOut");
+    this.__horizontalMove("slideOut", "easeOut", "afterOut");
   },
   topDown: function() {
     this.__verticalMove("slideDown", "easeIn", "afterIn");

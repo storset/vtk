f6ed5dd1c45508db41d702b23b85def634599026
Øyvind Hatland <oyvihatl@usit.uio.no>
2014-03-28 09:56:09 +0000

    VTK - teqhniques for animatronics

--- src/main/resources/web/js/vrtx-animation.js
+++ src/main/resources/web/js/vrtx-animation.js
@@ -5,8 +5,9 @@
  *  
  *  * Requires Dejavu OOP library
  *
- *  TODO: Progressively support CSS3 animations
- *
+ *  Horizontal - hides content and marginLeft it left (show) and then right (hide)
+ *  Vertical - height and padding down (show) and then up (hide)
+ *             TODO: Same strategy as horizontal will make vertical smoother
  */
  
 var VrtxAnimationInterface = dejavu.Interface.declare({

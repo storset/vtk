beb858059d8a037f7152883e68931d0f27c32342
Øyvind Hatland <oyvind.hatland@usit.uio.no>
2014-05-23 12:33:37 +0200

    VTK-3604 - seem to be working from this timezone with days in different DST's (hopefully JS Date handles this well)

--- src/test/resources/web/js/tests.js
+++ src/test/resources/web/js/tests.js
@@ -19,5 +19,25 @@ test("DateTime parsing", function () {
     "d4": "Torsdag",
     "d5": "Fredag",
     "d6": "Lørdag"
-  }), "Mandag",  "Time");
+  }), "Mandag",  "Day +0200 - 12:15=>14:00");
+  var datetime = utils.getDateTime("2014-11-03T23:15:00.000+01:00", "2014-11-03T23:59:00.000+01:00");
+  equal(utils.getDayFormatted(datetime.startDateTime, datetime.endDateTime, {
+    "d0": "Søndag",
+    "d1": "Mandag",
+    "d2": "Tirsdag",
+    "d3": "Onsdag",
+    "d4": "Torsdag",
+    "d5": "Fredag",
+    "d6": "Lørdag"
+  }), "Mandag",  "Day +0100 - 23:15=>23:59");
+    var datetime = utils.getDateTime("2014-11-03T00:15:00.000+01:00", "2014-11-03T01:59:00.000+01:00");
+  equal(utils.getDayFormatted(datetime.startDateTime, datetime.endDateTime, {
+    "d0": "Søndag",
+    "d1": "Mandag",
+    "d2": "Tirsdag",
+    "d3": "Onsdag",
+    "d4": "Torsdag",
+    "d5": "Fredag",
+    "d6": "Lørdag"
+  }), "Mandag",  "Day +0100 - 00:15=>01:59");
 });

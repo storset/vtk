e20d3dacb98570a6d206753a43a7d9718cd4eb20
Øyvind Hatland <oyvind.hatland@usit.uio.no>
2014-05-23 13:26:27 +0200

    VTK-3604 - add QUnit tests for generating of HTML from JSON for place, staff and resources

--- src/test/resources/web/js/tests.js
+++ src/test/resources/web/js/tests.js
@@ -1,9 +1,40 @@
 var utils = null;
+var session = null;
 module("Schedule.js", {
   setup: function() {
     utils = new scheduleUtils();
+    session = {
+      "id": "14H-EXPHIL03-1-1-2-12-1/34",
+      "dtStart": "2014-08-21T16:30:00.000+02:00",
+      "dtEnd": "2014-08-21T18:15:00.000+02:00",
+      "weekNr": 34,
+      "status": "active",
+      "title": "Seminargruppe 12 (MED)",
+      "rooms": [{
+        "buildingId": "BL16",
+        "buildingName": "Georg Morgenstiernes hus",
+        "buildingAcronym": "GM",
+        "roomId": "205",
+        "roomName": "Seminarrom 205",
+        "roomUrl": "http://www.med.uio.no/om/finn-fram/kart/vis/#bl1602,300,253"
+      }],
+      "vrtxTitle": "Åpningsforelesning",
+      "vrtxStaff": [
+        { "id": "rezam" },
+        { "id": "oyvihatl" }
+      ],
+      "vrtxStaffExternal": [
+        { "name": "Gunnar Flaksnes", "url": "http://www.nrk.no/" },
+        { "name": "Roger Rabbit", "url": "http://www.aftenposten.no/" }
+      ],
+      "vrtxResources": [
+        { "title": "Pensumlitteratur (PDF)", "url": "http://www.vg.no/" }
+      ],
+      "vrtxResourcesText": "<ul><li>listepunkt #1</li><li>listepunkt #2</li></ul>"
+    };
   }, teardown: function() {
     utils = null;
+    session = null;
   }
 });
 
@@ -40,4 +71,13 @@ test("DateTime parsing", function () {
     "d5": "Fredag",
     "d6": "Lørdag"
   }), "Mandag",  "Day +0100 - 00:15=>01:59");
+});
+test("Generating HTML from JSON for Place", function () {
+  equal(utils.getPlace(session), "<abbr title='Georg Morgenstiernes hus'>GM</abbr> <a title='Seminarrom 205' href='http://www.med.uio.no/om/finn-fram/kart/vis/#bl1602,300,253'>205</a>", "List or single element of: Link, Link with title, abbr with title or just text");
+});
+test("Generating HTML from JSON for Staff", function () {
+  equal(utils.getStaff(session), "<ul><li>rezam</li><li>oyvihatl</li><li><a href='http://www.nrk.no/'>G. Flaksnes</a></li><li><a href='http://www.aftenposten.no/'>R. Rabbit</a></li></ul>", "List or single element of: Link, Link with title, abbr with title or just text");
+});
+test("Generating HTML from JSON for Resources", function () {
+  equal(utils.getResources(session), "<a href='http://www.vg.no/'>Pensumlitteratur (PDF)</a><ul><li>listepunkt #1</li><li>listepunkt #2</li></ul>", "List or single element of: Link, Link with title, abbr with title or just text");
 });

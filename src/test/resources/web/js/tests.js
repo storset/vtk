15059b93c4026ad56a76997a50c929c22f70231e
Øyvind Hatland <oyvind.hatland@usit.uio.no>
2014-05-23 19:20:56 +0200

    VTK-3604 - simplify day test

--- src/test/resources/web/js/tests.js
+++ src/test/resources/web/js/tests.js
@@ -1,8 +1,19 @@
 var utils = null;
+var i18n = null;
 var session = null;
+
 module("Schedule.js", {
   setup: function() {
     utils = new scheduleUtils();
+    i18n = {
+      "d0": "Søndag",
+      "d1": "Mandag",
+      "d2": "Tirsdag",
+      "d3": "Onsdag",
+      "d4": "Torsdag",
+      "d5": "Fredag",
+      "d6": "Lørdag"
+    };
     session = {
       /* TP */
       "id": "14H-EXPHIL03-1-1-2-12-1/34",
@@ -36,6 +47,7 @@ module("Schedule.js", {
     };
   }, teardown: function() {
     utils = null;
+    i18n = null;
     session = null;
   }
 });
@@ -44,35 +56,11 @@ test("DateTime parsing", function () {
   var datetime = utils.getDateTime("2014-08-18T12:15:00.000+02:00", "2014-08-18T14:00:00.000+02:00");
   equal(utils.getDateFormatted(datetime.start, datetime.end), "18.08.14", "Date");
   equal(utils.getTimeFormatted(datetime.start, datetime.end), "12:15&ndash;14:00",  "Time");
-  equal(utils.getDayFormatted(datetime.start, datetime.end, {
-    "d0": "Søndag",
-    "d1": "Mandag",
-    "d2": "Tirsdag",
-    "d3": "Onsdag",
-    "d4": "Torsdag",
-    "d5": "Fredag",
-    "d6": "Lørdag"
-  }), "Mandag",  "Day +0200 - 12:15=>14:00");
+  equal(utils.getDayFormatted(datetime.start, datetime.end, i18n), "Mandag",  "Day +0200 - 12:15=>14:00");
   var datetime2 = utils.getDateTime("2014-11-03T23:15:00.000+01:00", "2014-11-03T23:59:00.000+01:00");
-  equal(utils.getDayFormatted(datetime2.start, datetime2.end, {
-    "d0": "Søndag",
-    "d1": "Mandag",
-    "d2": "Tirsdag",
-    "d3": "Onsdag",
-    "d4": "Torsdag",
-    "d5": "Fredag",
-    "d6": "Lørdag"
-  }), "Mandag",  "Day +0100 - 23:15=>23:59");
+  equal(utils.getDayFormatted(datetime2.start, datetime2.end, i18n), "Mandag",  "Day +0100 - 23:15=>23:59");
   var datetime3 = utils.getDateTime("2014-11-03T00:15:00.000+01:00", "2014-11-03T01:59:00.000+01:00");
-  equal(utils.getDayFormatted(datetime3.start, datetime3.end, {
-    "d0": "Søndag",
-    "d1": "Mandag",
-    "d2": "Tirsdag",
-    "d3": "Onsdag",
-    "d4": "Torsdag",
-    "d5": "Fredag",
-    "d6": "Lørdag"
-  }), "Mandag",  "Day +0100 - 00:15=>01:59");
+  equal(utils.getDayFormatted(datetime3.start, datetime3.end, i18n), "Mandag",  "Day +0100 - 00:15=>01:59");
 });
 test("Generating HTML from JSON for Title", function () {
   equal(utils.getTitle(session), "Åpningsforelesning", "Title from Vortex");
